7615727d91cef67e10768429e4fecdb40377db0f
Benjamin E <ben@elwyn.co>
2020-02-10 17:50:59 +0000

    start next config

---
+++ next.config.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv'
+// Extract environment variables from local .env file
+dotenv.config()
+
+module.exports = {
+  // Save environment variables
+  env: {
+    firebase_api_key: process.env.FIREBASE_API_KEY,
+    firebase_auth_domain: process.env.FIREBASE_AUTH_DOMAIN,
+    firebase_database_url: process.env.FIREBASE_DATABASE_URL,
+    firebase_project_id: process.env.FIREBASE_PROJECT_ID,
+    firebase_storage_bucket: process.env.FIREBASE_STORAGE_BUCKET,
+    firebase_messaging_sender_id: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    firebase_app_id: process.env.FIREBASE_APP_ID,
+  },
+}
