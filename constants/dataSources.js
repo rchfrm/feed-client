94818d81e4b65fad3ed371b5286d06e2f7525597
Benjamin E <ben@elwyn.co>
2020-02-10 17:51:39 +0000

    create constants

---
+++ constants/dataSources.js
@@ -0,0 +1,177 @@
+// https://coolors.co/7a9fff-e85a84-ffc249-aeefa2-1b2f33
+
+const dataSourceDetails = {
+
+  apple: {
+    colour: "#1B2F33",
+    textColour: "white",
+  },
+  apple_music_streams_7d: {
+    colour: "#1B2F33",
+    textColour: "white",
+  },
+
+  bandcamp: {
+    colour: "#477987",
+    textColour: "white",
+  },
+
+  facebook: {
+    colour: "#7A9FFF",
+    textColour: "white",
+  },
+  facebook_likes: {
+    colour: "#86A7FF",
+    textColour: "white",
+  },
+  facebook_page_fans_country: {
+    colour: "#86A7FF",
+    textColour: "white",
+  },
+  facebook_page_fans_city: {
+    colour: "#86A7FF",
+    textColour: "white",
+  },
+  facebook_page_fans_gender_age: {
+    colour: "#86A7FF",
+    textColour: "white",
+  },
+  facebook_engaged_1y: {
+    colour: "#92B0FF",
+    textColour: "white",
+  },
+  facebook_engaged_28d: {
+    colour: "#92B0FF",
+    textColour: "white",
+  },
+  facebook_engaged_7d: {
+    colour: "#92B0FF",
+    textColour: "white",
+  },
+  facebook_ad_spend: {
+    colour: "#6483D1",
+    textColour: "white",
+  },
+  facebook_ad_spend_feed: {
+    colour: "#6483D1",
+    textColour: "white",
+  },
+
+  instagram: {
+    colour: "#E85A84",
+    textColour: "white",
+  },
+  instagram_follower_count: {
+    colour: "#E85A84",
+    textColour: "white",
+  },
+  instagram_audience_country: {
+    colour: "#EA698F",
+    textColour: "white",
+  },
+  instagram_audience_city: {
+    colour: "#EA698F",
+    textColour: "white",
+  },
+  instagram_audience_gender_age: {
+    colour: "#EA698F",
+    textColour: "white",
+  },
+  instagram_reach: {
+    colour: "#EC789A",
+    textColour: "white",
+  },
+  instagram_reach_7d: {
+    colour: "#EE87A5",
+    textColour: "white",
+  },
+  instagram_profile_views: {
+    colour: "#F096B0",
+    textColour: "white",
+  },
+  instagram_profile_views_7d: {
+    colour: "#F2A5BB",
+    textColour: "white",
+  },
+  instagram_engaged_1y: {
+    colour: "#F4B4C7",
+    textColour: "black",
+  },
+  instagram_engaged_28d: {
+    colour: "#F4B4C7",
+    textColour: "black",
+  },
+  instagram_engaged_7d: {
+    colour: "#F4B4C7",
+    textColour: "black",
+  },
+
+  soundcloud: {
+    colour: "#FFC249",
+    textColour: "white",
+  },
+  soundcloud_follower_count: {
+    colour: "#FFC249",
+    textColour: "white",
+  },
+  soundcloud_listeners_7d: {
+    colour: "#FFCD6A",
+    textColour: "white",
+  },
+
+  spotify: {
+    colour: "#AEEFA2",
+    textColour: "black",
+  },
+  spotify_follower_count: {
+    colour: "#AEEFA2",
+    textColour: "black",
+  },
+  spotify_streams_7d: {
+    colour: "#BCF1B2",
+    textColour: "black",
+  },
+  spotify_listeners_7d: {
+    colour: "#CBF4C3",
+    textColour: "black",
+  },
+
+  spotify_listeners_30d: {
+    colour: "#DAF7D4",
+    textColour: "black",
+  },
+
+  twitter: {
+    colour: "#A5D8FF",
+    textColour: "black",
+  },
+  twitter_follower_count: {
+    colour: "#A5D8FF",
+    textColour: "black",
+  },
+
+  website: {
+    colour: "black",
+    textColour: "white",
+  },
+
+  youtube: {
+    colour: "#DB4C56",
+    textColour: "white",
+  },
+  youtube_subscriber_count: {
+    colour: "#DB4C56",
+    textColour: "white",
+  },
+  youtube_views: {
+    colour: "#E16C74",
+    textColour: "white",
+  },
+  youtube_views_7d: {
+    colour: "#E88D93",
+    textColour: "white",
+  },
+
+};
+
+export default dataSourceDetails;
---
+++ constants/routes.js
@@ -0,0 +1,13 @@
+export const SIGN_UP = "/sign-up";
+export const CONNECT_ARTIST = "/connect-artist";
+export const THANK_YOU = "/thank-you";
+export const LOG_IN = "/";
+export const PASSWORD_FORGET = "/forgot-password";
+export const HOME = "/home";
+export const POSTS = "/posts";
+export const ACCOUNT = "/account";
+export const SUBSCRIBE = "/subscribe";
+export const RESULTS = "/results";
+export const FAQ = "/faq";
+export const PRICES = "/pricing";
+export const TERMS = "/terms";
