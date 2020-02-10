9022a12be5c5b8b74bbdcaa114f97d01c2fdc733
Benjamin E <ben@elwyn.co>
2020-02-10 15:08:37 +0000

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'import',
+    'react',
+  ],
+
+  rules: {
+    'no-shadow': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'max-len': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-param-reassign': 'off',
+    'no-empty-pattern': 'off',
+    'arrow-parens': 'off',
+    'consistent-return': 'off',
+    'arrow-body-style': 'off',
+    'object-curly-newline': 'off',
+    'implicit-arrow-linebreak': 'off',
+    'no-underscore-dangle': 'off',
+    'semi': [ 2, 'never' ],
+    'unicorn/number-literal-case': 'off',
+    "react/react-in-jsx-scope": "off"
+  },
+};
