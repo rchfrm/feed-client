7314d6cfe90186d3d46e27cfa0a3a42ef6156ad2
Benjamin E <ben@elwyn.co>
2020-02-13 12:06:09 +0000

    don't use manifest plugin

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 // Next plugins
 const withPlugins = require('next-compose-plugins')
 const withPWA = require('next-pwa')
-const withManifest = require('next-manifest')
 
 // Extract environment variables from local .env file
 const dotenv = require('dotenv')
@@ -35,34 +34,4 @@ module.exports = withPlugins([
       dest: 'public',
     },
   }],
-
-  [withManifest, {
-    manifest: {
-      output: './public/',
-      short_name: 'Feed',
-      name: 'Feed',
-      icons: [
-        {
-          src: 'favicon.ico',
-          sizes: '64x64 32x32 24x24 16x16',
-          type: 'image/x-icon',
-        },
-        {
-          src: '/icons/icon-192x192.png',
-          sizes: '192x192',
-          type: 'image/png',
-        },
-        {
-          src: '/icons/icon-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-        },
-      ],
-      start_url: '/',
-      display: 'standalone',
-      theme_color: '#000000',
-      background_color: '#ffffff',
-    },
-  }],
-
 ], nextConfig)
