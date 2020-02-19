827b89b95794a36ccdd73c2f5b6b58c8e47d6b8c
Benjamin E <ben@elwyn.co>
2020-02-19 13:53:35 +0000

    simplify route guarding

--- components/RouteGuarding.jsx
+++ components/RouteGuarding.jsx
@@ -1,32 +1,28 @@
 
 import React from 'react'
-import Router, { useRouter } from 'next/router'
+import Router from 'next/router'
 import { AuthContext } from './contexts/Auth'
 import { UserContext } from './contexts/User'
 import { ArtistContext } from './contexts/Artist'
 import * as ROUTES from '../constants/routes'
 
-const RouteGuarding = () => {
-  const router = useRouter()
-  const { noAuth } = React.useContext(AuthContext)
-  const { noUser } = React.useContext(UserContext)
-  const { noArtist } = React.useContext(ArtistContext)
+import firebase from './helpers/firebase'
 
-  const handleNoAuthUser = React.useCallback(async () => {
-    // Check if the user is on an auth only page,
-    // if they are push to log in page
-    const { pathname } = router
-    if (pathname !== '/' || pathname !== '/sign-up') {
+const RouteGuarding = ({ children }) => {
+  const { auth: { token } } = React.useContext(AuthContext)
+  const [isLoggedIn, setLoggedIn] = React.useState(false)
+
+  React.useEffect(() => {
+    if (!token) {
+      console.log('nononono')
       Router.push(ROUTES.LOG_IN)
+      return
     }
+    setLoggedIn(true)
+  })
 
-    // Reset all contexts
-    noAuth()
-    noUser()
-    noArtist()
-  }, [noArtist, noAuth, noUser])
-  
-  
+  if (isLoggedIn) return children
+  return <></>
 }
 
 export default RouteGuarding
