580d4e64ebbc87de902040447efb7798f30818f4
Benjamin E <ben@elwyn.co>
2020-02-19 13:53:35 +0000

    simplify route guarding

--- components/RouteGuarding.jsx
+++ components/RouteGuarding.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Router, { useRouter } from 'next/router'
+import Router from 'next/router'
 import { AuthContext } from './contexts/Auth'
 import { UserContext } from './contexts/User'
 import { ArtistContext } from './contexts/Artist'
