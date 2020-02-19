90e97d2eb10a41ccf68a76dee73bbc962562ab45
Benjamin E <ben@elwyn.co>
2020-02-19 15:44:47 +0000

    refactor

--- pages/faq/index.jsx
+++ pages/faq/index.jsx
@@ -1,10 +1,8 @@
 import FaqPage from '../../components/page/FaqPage'
 import RouteGuarding from '../../components/RouteGuarding'
 
-export default () => {
-  return (
-    <RouteGuarding>
-      <FaqPage />
-    </RouteGuarding>
-  )
-}
+export default (
+  <RouteGuarding>
+    <FaqPage />
+  </RouteGuarding>
+)
