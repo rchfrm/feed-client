887b6abbb26ffca6a3422c5a02f6bb6301b3304a
Benjamin E <ben@elwyn.co>
2020-02-20 17:33:16 +0000

    refactor nav and style active links

---
+++ components/ActiveLink.jsx
@@ -0,0 +1,18 @@
+
+import { withRouter } from 'next/router'
+import Link from 'next/link'
+import React, { Children } from 'react'
+
+const ActiveLink = ({ router, children, ...props }) => {
+  const child = Children.only(children)
+  const { href, activeClassName = '-active' } = props
+
+  let className = child.props.className || ''
+  if (router.pathname === href) {
+    className = `${className} ${activeClassName}`.trim()
+  }
+
+  return <Link {...props}>{React.cloneElement(child, { className })}</Link>
+}
+
+export default withRouter(ActiveLink)
