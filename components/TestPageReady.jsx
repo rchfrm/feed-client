e3f5ffee1a325182780c8da5723e69ec9a1c4696
Benjamin E <ben@elwyn.co>
2020-03-02 10:28:22 +0000

    fetch new token if expired on test page ready

--- components/TestPageReady.jsx
+++ components/TestPageReady.jsx
@@ -1,5 +1,6 @@
-import { useContext, useEffect } from 'react'
+import { useContext, useState } from 'react'
 import Router, { useRouter } from 'next/router'
+import useAsyncEffect from 'use-async-effect'
 
 import * as ROUTES from '../constants/routes'
 import { AuthContext } from './contexts/Auth'
@@ -9,11 +10,18 @@ const kickToLogin = () => Router.push(ROUTES.HOME)
 const TestPageReady = (Component) => {
   return (props) => {
     const { pathname: currentPath } = useRouter()
-    const { auth: { token } } = useContext(AuthContext)
-    // If token has gone, kick to login
-    useEffect(() => {
+    const { auth: { token: initialToken }, getToken } = useContext(AuthContext)
+    const [token, setToken] = useState(initialToken)
+    useAsyncEffect(async (isMounted) => {
       if (!token && !currentPath === ROUTES.LOGIN) {
-        kickToLogin()
+        // Check for new token
+        const newToken = await getToken()
+        // Stop here if not mounted
+        if (!isMounted()) return
+        // If new token can't be generated...
+        if (!newToken) return kickToLogin()
+        // Set new token
+        setToken(newToken)
       }
     }, [token])
     // Stop flash of content if landing on page while not logged in
