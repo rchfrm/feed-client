22de311565533c291422751988575a03f4106e05
Benjamin E <ben@elwyn.co>
2020-03-05 10:47:32 +0000

    create and use host helper

--- components/helpers/server.jsx
+++ components/helpers/server.jsx
@@ -1,9 +1,7 @@
+import host from './host'
 import helper from './helper'
 import artistHelpers from './artistHelpers'
 
-const host = process.env.build_env === 'development'
-  ? process.env.react_app_api_url_local || 'http://localhost:5000/'
-  : process.env.react_app_api_url || 'http://localhost:5000/'
 
 export default {
 
