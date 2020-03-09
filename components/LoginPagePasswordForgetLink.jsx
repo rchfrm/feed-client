3736fd0d9ff46102ffdd364365abed34b914605c
Benjamin E <ben@elwyn.co>
2020-03-09 11:10:12 +0000

    improve and style login page

--- components/LoginPagePasswordForgetLink.jsx
+++ components/LoginPagePasswordForgetLink.jsx
@@ -1,9 +1,10 @@
 import Link from 'next/link'
 import * as ROUTES from '../constants/routes'
+import styles from './LoginPage.module.css'
 
 export default () => {
   return (
-    <p>
+    <p className={styles.forgotPasswordLink}>
       <Link href={ROUTES.PASSWORD_FORGET}><a>Forgot Password?</a></Link>
     </p>
   )
