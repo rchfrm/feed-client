c9e1869a5f07a19984e31b85222ddc4e1058c32d
Benjamin E <ben@elwyn.co>
2020-03-16 15:44:47 +0000

    put things back to where they were

--- components/PostsAll.jsx
+++ components/PostsAll.jsx
@@ -22,6 +22,7 @@ function Posts({
   posts,
   setPosts,
   visiblePost,
+  setVisiblePost,
   togglePromotion,
 }) {
   // DEFINE POST DETAILS
@@ -35,6 +36,23 @@ function Posts({
   }
   const postTrackerReducer = (postTrackerState, postTrackerAction) => {
     switch (postTrackerAction.type) {
+      case 'set-scroll-position':
+        return {
+          ...postTrackerState,
+          timerRunning: true,
+          updateTime: moment().format('x'),
+          scroll: postTrackerAction.payload.scroll,
+        }
+      case 'toggle-timer':
+        return {
+          ...postTrackerState,
+          timerRunning: postTrackerAction.payload,
+        }
+      case 'time-now':
+        return {
+          ...postTrackerState,
+          timeNow: moment().format('x'),
+        }
       case 'posts-added':
         return {
           ...postTrackerState,
@@ -46,8 +64,36 @@ function Posts({
         throw new Error(`Unable to find ${postTrackerAction.type} in postTrackerReducer`)
     }
   }
-  const [, setPostTracker] = React.useReducer(postTrackerReducer, initialPostTrackerState)
+  const [postTracker, setPostTracker] = React.useReducer(postTrackerReducer, initialPostTrackerState)
   // END DEFINE TILE DETAILS
+
+  // TRACK SCROLL POSITION AND UPDATE TILE DETAILS STATE ACCORDINGLY
+  React.useEffect(() => {
+    const currentTile = Math.floor((postTracker.scroll + postTracker.width / 2) / postTracker.width) + 1
+    if (currentTile !== visiblePost) {
+      setVisiblePost(currentTile)
+    }
+
+    const postsList = document.getElementsByClassName('posts')[0]
+    if (postTracker.timeNow - postTracker.updateTime > 500 && postTracker.timerRunning) {
+      postsList.scrollLeft = postsList.scrollWidth * (visiblePost - 1) * postTracker.width
+      setPostTracker({
+        type: 'toggle-timer',
+        payload: false,
+      })
+    }
+  }, [postTracker, visiblePost, setVisiblePost])
+  // END TRACK SCROLL POSITION AND UPDATE TILE DETAILS STATE ACCORDINGLY
+
+  // CLOCK TO REFERENCE WHEN USER STOPS SCROLLING
+  React.useEffect(() => {
+    if (postTracker.timerRunning) {
+      setPostTracker({
+        type: 'time-now',
+      })
+    }
+  })
+  // END CLOCK TO REFERENCE WHEN USER STOPS SCROLLING
 
 
   // KEEP POST TRACKER IN SYNC WITH NUMBER OF POSTS SAVED TO STATE
@@ -61,6 +107,22 @@ function Posts({
   }, [numberOfPosts])
   // END KEEP POST TRACKER IN SYNC WITH NUMBER OF POSTS SAVED TO STATE
 
+  // HANDLE SCROLL
+  const handleScroll = () => {
+    const frame = document.getElementsByClassName('frame')[0]
+    const frameWidth = frame.scrollWidth
+    const scrollPosition = frame.scrollLeft
+    const scrollPercentage = scrollPosition / frameWidth
+    setPostTracker({
+      type: 'set-scroll-position',
+      payload: {
+        timerRunning: true,
+        scroll: scrollPercentage,
+      },
+    })
+  }
+  // END HANDLE SCROLL
+
   if (posts.length === 0) {
     return <PostsNone />
   }
@@ -71,7 +133,8 @@ function Posts({
       <PageHeader heading="review your posts" punctuation="," />
 
       <h4 className={styles.h4}>
-        Below are your most recent posts.&nbsp;
+        Below are your most recent posts.
+        {' '}
         <Feed />
         {' '}
         works best if they're all selected, but if there are any you'd rather not promote just untick them.
@@ -81,6 +144,7 @@ function Posts({
         posts={posts}
         setPosts={setPosts}
         togglePromotion={togglePromotion}
+        handleScroll={handleScroll}
       />
 
     </div>
@@ -89,8 +153,7 @@ function Posts({
 
 export default Posts
 
-function PostList({ posts, setPosts, togglePromotion }) {
-
+function PostList({ posts, setPosts, togglePromotion, handleScroll }) {
   // CREATE ARRAY OF POSTS
   const postList = posts.map((post, index) => {
     return (
@@ -109,7 +172,7 @@ function PostList({ posts, setPosts, togglePromotion }) {
   // END CREATE ARRAY OF POSTS
 
   return (
-    <ul className={`frame posts ${styles.posts}`}>
+    <ul className={`frame posts ${styles.posts}`} onScroll={handleScroll}>
       {postList}
     </ul>
   )
