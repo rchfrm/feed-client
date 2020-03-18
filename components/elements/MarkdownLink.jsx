40d37dacccf7370ccd85b38127a621860eb66729
Benjamin E <ben@elwyn.co>
2020-03-18 15:10:38 +0000

    improve link type testing

--- components/elements/MarkdownLink.jsx
+++ components/elements/MarkdownLink.jsx
@@ -2,20 +2,29 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'next/link'
 
+const getLinkType = (href) => {
+  if (href.startsWith('/')) return 'internal'
+  if (href.startsWith('mailto:')) return 'email'
+  return 'external'
+}
+
 const MarkdownLink = ({ href, children }) => {
   const { props: { value: linkText } } = children[0]
+  const linkType = getLinkType(href)
   // INTERNAL LINK
-  if (href[0] === '/') {
+  if (linkType === 'internal') {
     return (
       <Link href={href}>
         <a>{ linkText }</a>
       </Link>
     )
   }
+  // EMAIL LINK
+  if (linkType === 'email') {
+    return <a href={href}>{ linkText }</a>
+  }
   // EXTERNAL LINK
-  return (
-    <a href={href} target="_blank" rel="noopener noreferrer">{ linkText }</a>
-  )
+  return <a href={href} target="_blank" rel="noopener noreferrer">{ linkText }</a>
 }
 
 MarkdownLink.propTypes = {
