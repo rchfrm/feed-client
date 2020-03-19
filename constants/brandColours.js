2d1865b59188905529a3b725f2c3b87b2ac3960b
Benjamin E <ben@elwyn.co>
2020-03-19 11:44:08 +0000

    update colour vars

--- constants/brandColours.js
+++ constants/brandColours.js
@@ -3,9 +3,24 @@ const brandColours = {
   black: '#0D1311',
   green: '#03D8B2',
   red: '#FA5450',
-  lightGrey: '#E7E9E8',
+  greyLight: '#E7E9E8',
   grey: '#C8CBCA',
-  darkGrey: '#7F8382',
+  greyDark: '#7F8382',
+  // SM colors
+  colorFbButton: '#4267B2',
+  colorFb: '#26547C',
+  colorInsta: '#E75A7C',
+  colorTwitter: '#5BC0EB',
+  colorSc: '#FDE74C',
 }
+
+// APP COLORS
+brandColours.bgColor = brandColours.white
+brandColours.textColor = brandColours.black
+brandColours.successColor = brandColours.green
+brandColours.loaderColor = brandColours.green
+brandColours.errorColor = brandColours.red
+brandColours.disabledColorBg = brandColours.greyLight
+brandColours.disabledColorText = brandColours.greyDark
 
 export default brandColours
