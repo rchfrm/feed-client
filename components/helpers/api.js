82d6aac4d4dbe2863cbe8f457256d06a7dca44a6
Andrew Mackrodt <andrew@denimsoft.com>
2020-03-20 06:28:11 +0000

    Implements an api helper to reduce request duplication and use axios

---
+++ components/helpers/api.js
@@ -0,0 +1,121 @@
+import app from 'firebase/app'
+import axios from 'axios'
+import host from './host'
+
+export default new class {
+  /**
+   * @param {string} method
+   * @param {string} path
+   * @param {object|string|false} [options]
+   * @param {string|false} [token]
+   * @returns {Promise<any>}
+   */
+  async request(method, path, options, token) {
+    if (typeof options === 'string' || options === false) {
+      if (typeof token !== 'undefined') {
+        throw new Error('token argument must not be set if passed in options argument')
+      }
+      token = options
+      options = undefined
+    }
+
+    const query = options ? options.query : undefined
+
+    if (typeof query !== 'undefined' && typeof query !== 'object') {
+      throw new Error('query must be empty or an object')
+    }
+
+    if (token) {
+      if (typeof token !== 'string') {
+        throw new Error('token must be a string')
+      }
+    } else if (token !== false) {
+      const { currentUser } = app.auth()
+      if (currentUser) {
+        token = await currentUser.getIdToken()
+      }
+      if (!token) {
+        throw new Error('no login session found')
+      }
+    }
+
+    let url = path.match(/^(?:https?:)?\/\//)
+      ? path
+      : `${host.replace(/\/$/, '')}/${path.replace(/^\//, '')}`
+
+    if (query) {
+      url += `?${Object.keys(query).map(key => `${key}=${query[key]}`).join('&')}`
+    }
+
+    const req = {
+      method: method.toUpperCase(),
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+
+    if (token !== false) {
+      req.headers['Authorization'] = `Bearer ${token}`
+    }
+
+    if (options && options.data) {
+      req.data = options.data
+    }
+
+    const res = await axios.request(req)
+
+    return res.data
+  }
+
+  /**
+   * @param {string} path
+   * @param {object|string|false} [query]
+   * @param {string|false} [token]
+   * @returns {Promise<any>}
+   */
+  get(path, query, token) {
+    const options = {}
+    if (typeof query !== 'object') {
+      token = query
+      query = undefined
+    } else {
+      options.query = query
+    }
+    return this.request('GET', path, options, token)
+  }
+
+  /**
+   * @param {string} path
+   * @param {object} data
+   * @param {string|false} [token]
+   * @returns {Promise<any>}
+   */
+  patch(path, data, token) {
+    const options = {}
+    if (typeof data !== 'object') {
+      token = data
+      data = undefined
+    } else {
+      options.data = data
+    }
+    return this.request('PATCH', path, options, token)
+  }
+
+  /**
+   * @param {string} path
+   * @param {object} data
+   * @param {string|false} [token]
+   * @returns {Promise<any>}
+   */
+  post(path, data, token) {
+    const options = {}
+    if (typeof data !== 'object') {
+      token = data
+      data = undefined
+    } else {
+      options.data = data
+    }
+    return this.request('POST', path, options, token)
+  }
+}()
--- components/helpers/server.jsx
+++ components/helpers/server.jsx
@@ -1,190 +1,144 @@
 import host from './host'
 import helper from './helper'
 import artistHelpers from './artistHelpers'
+import {default as api} from './api'
 
 
 export default {
 
   // USER
-  createUser: async (first_name, last_name, verify_id_token) => {
-    const endpoint = `${host}accounts/register`
-    const res = await fetch(endpoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        first_name,
-        last_name,
-        token: verify_id_token,
-      }),
-    }).catch((err) => {
-      throw (err)
-    })
-
-    if (res.ok) {
-      const jsonResponse = await res.json()
-      jsonResponse.artists = artistHelpers.sortArtistsAlphabetically(jsonResponse.artists)
-      return jsonResponse
-    }
+  /**
+   * @param {string} first_name
+   * @param {string} last_name
+   * @param {string} verify_id_token
+   * @returns {Promise<any>}
+   */
+  createUser: (first_name, last_name, verify_id_token) => {
+    return api
+        .post('/accounts/register', {
+          first_name,
+          last_name,
+          token: verify_id_token,
+        }, false)
+        .then(res => {
+          res.artists = artistHelpers.sortArtistsAlphabetically(res.artists)
+          return res
+        })
   },
 
+  /**
+   * @param {string} [verifyIdToken]
+   * @returns {Promise<any>}
+   */
   getUser: async (verifyIdToken) => {
-    const endpoint = `${host}users/me`
-    const res = await fetch(endpoint, {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${verifyIdToken}`,
-      },
-    }).catch((err) => {
-      console.error('cant get user')
-      throw (err)
-    })
-
-    if (res.ok) {
-      const jsonResponse = await res.json()
-      jsonResponse.artists = artistHelpers.sortArtistsAlphabetically(jsonResponse.artists)
-      return jsonResponse
-    }
+    return api.get('/users/me', verifyIdToken)
+        .then(res => {
+          res.artists = artistHelpers.sortArtistsAlphabetically(res.artists)
+          return res
+        })
   },
 
+  /**
+   * @param {string} firstName
+   * @param {string} lastName
+   * @param {string} email
+   * @param {string} [verifyIdToken]
+   * @returns {Promise<any>}
+   */
   updateUser: async (firstName, lastName, email, verifyIdToken) => {
-    const endpoint = `${host}users/me`
-    const res = await fetch(endpoint, {
-      method: 'PATCH',
-      headers: {
-        Authorization: `Bearer ${verifyIdToken}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        first_name: firstName,
-        last_name: lastName,
-        email,
-      }),
-    })
-    if (res.ok) {
-      return res.json()
-    }
-    throw new Error(res.statusText)
+    return api
+        .patch('/users/me', {
+          first_name: firstName,
+          last_name: lastName,
+          email,
+        }, verifyIdToken)
   },
 
 
   // UPDATE ARTIST
+  /**
+   * @param {string} artistId
+   * @param {number} dailyBudget
+   * @param {string} [verifyIdToken]
+   * @returns {Promise<any>}
+   */
   updateDailyBudget: async (artistId, dailyBudget, verifyIdToken) => {
-    const endpoint = `${host}artists/${artistId}`
-    const res = await fetch(endpoint, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${verifyIdToken}`,
-      },
-      body: JSON.stringify({
-        daily_budget: dailyBudget,
-      }),
-    })
-    if (res.ok) {
-      return res.json()
-    }
-    throw new Error(res.statusText)
+    return api.patch(`/artists/${artistId}`, { daily_budget: dailyBudget }, verifyIdToken)
   },
 
+  /**
+   * @param {string} artistId
+   * @param {string} link
+   * @param {string} linkLabel
+   * @param {string} [verifyIdToken]
+   * @returns {Promise<any>}
+   */
   saveLink: async (artistId, link, linkLabel, verifyIdToken) => {
-    const endpoint = `${host}artists/${artistId}`
-    const res = await fetch(endpoint, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${verifyIdToken}`,
-      },
-      body: JSON.stringify({
-        [linkLabel]: link,
-      }),
-    })
-    if (res.ok) {
-      return res.json()
-    }
-    throw new Error(res.statusText)
+    return api.patch(`/artists/${artistId}`, { [linkLabel]: link }, verifyIdToken)
   },
 
+  /**
+   * @param {string} artistId
+   * @param {string} priorityDSP
+   * @param {string} [verifyIdToken]
+   * @returns {Promise<any>}
+   */
   updatePriorityDSP: async (artistId, priorityDSP, verifyIdToken) => {
-    const endpoint = `${host}artists/${artistId}`
-    const res = await fetch(endpoint, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${verifyIdToken}`,
-      },
-      body: JSON.stringify({
-        priority_dsp: priorityDSP,
-      }),
-    })
-    if (res.ok) {
-      return res.json()
-    }
-    throw new Error(res.statusText)
+    return api.patch(`/artists/${artistId}`, { priority_dsp: priorityDSP }, verifyIdToken)
   },
 
   // DATA SOURCES
+  /**
+   * @param {string} endpoint
+   * @param {string} [verifyIdToken]
+   * @returns {Promise<any>}
+   */
   getEndpoint: async (endpoint, verifyIdToken) => {
-    const res = await fetch(endpoint, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${verifyIdToken}`,
-      },
-    })
-    if (res.ok) {
-      return res.json()
-    }
-    throw new Error(res.statusText)
+    return api.get(endpoint, verifyIdToken)
   },
 
+  /**
+   * @param {string} path
+   * @param {string} [verifyIdToken]
+   * @returns {Promise<any>}
+   */
   getPath: async (path, verifyIdToken) => {
-    const endpoint = `${host}${path}`
-
-    const res = await fetch(endpoint, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${verifyIdToken}`,
-      },
-    })
-
-    if (res.ok) {
-      return res.json()
-    }
-
-    throw new Error(res.statusText)
+    return api.get(path, verifyIdToken)
   },
 
+  /**
+   * @param {string[]} dataSources
+   * @param {string} artistId
+   * @param {string} [verifyIdToken]
+   * @returns {Promise<any>}
+   */
   getDataSourceValue: async (dataSources, artistId, verifyIdToken) => {
-    const dataSourceString = dataSources.join(',')
-    const endpoint = `${host}artists/${artistId}/data_sources?metrics=${dataSourceString}`
-    const res = await fetch(endpoint, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${verifyIdToken}`,
-      },
-    })
-    if (res.ok) {
-      return res.json()
-    }
-    throw new Error(res.statusText)
+    return api
+        .get(`/artists/${artistId}/data_sources`, {
+          query: {
+            metrics: dataSources.join(','),
+          }
+        }, verifyIdToken)
   },
 
   // ASSETS
+  /**
+   * @param {number} offset
+   * @param {number} limit
+   * @param {string} artistId
+   * @param {string} [verifyIdToken]
+   * @returns {Promise<any>}
+   */
   getUnpromotedPosts: async (offset, limit, artistId, verifyIdToken) => {
-    const endpoint = `${host}artists/${artistId}/assets?promotion_status=inactive&offset=${offset}&limit=${limit}`
-    const res = await fetch(endpoint, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${verifyIdToken}`,
-      },
-    })
-    if (res.ok) {
-      const jsonResponse = await res.json()
+    return api
+        .get(`/artists/${artistId}/assets`, {
+          query: {
+            promotion_status: 'inactive',
+            offset,
+            limit,
+          },
+        }, verifyIdToken)
+    .then(jsonResponse => {
       // Process certain parts of the response to make it easier to handle
       jsonResponse.forEach(post => {
         // Abbreviate text to <100 characters long
@@ -242,100 +196,67 @@ export default {
       })
 
       return jsonResponse
-    }
-    throw new Error(res.statusText)
+    })
   },
 
+  /**
+   * @param {string} artistId
+   * @param {string} [promotionStatus]
+   * @param {string} [verifyIdToken]
+   * @returns {Promise<any>}
+   */
   getAssets: async (artistId, promotionStatus, verifyIdToken) => {
-    const query = promotionStatus ? `?promotion_status=${promotionStatus}` : ''
-    const endpoint = `${host}artists/${artistId}/assets${query}`
+    const query = promotionStatus ? { promotion_status: promotionStatus } : {}
 
-    const res = await fetch(endpoint, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${verifyIdToken}`,
-      },
-    })
-
-    if (res.ok) {
-      return res.json()
-    }
-
-    throw new Error(res.statusText)
+    return api.get(`/artists/${artistId}/assets`, query, verifyIdToken)
   },
 
+  /**
+   * @param {string} artistId
+   * @param {string} postId
+   * @param {boolean} promotionEnabled
+   * @param {string} [verifyIdToken]
+   * @returns {Promise<any>}
+   */
   togglePromotionEnabled: async (artistId, postId, promotionEnabled, verifyIdToken) => {
-    const endpoint = `${host}artists/${artistId}/assets/${postId}`
-
-    const res = await fetch(endpoint, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${verifyIdToken}`,
-      },
-      body: JSON.stringify({
-        promotion_enabled: promotionEnabled,
-      }),
-    })
-
-    if (res.ok) {
-      return res.json()
-    }
-
-    throw new Error(res.statusText)
+    return api.patch(`/artists/${artistId}/assets/${postId}`, { promotion_enabled: promotionEnabled }, verifyIdToken)
   },
 
+  /**
+   * @param {string} artistId
+   * @param {string} postId
+   * @param {string} link
+   * @param {string} [verifyIdToken]
+   * @returns {Promise<any>}
+   */
   updateAssetLink: async (artistId, postId, link, verifyIdToken) => {
-    const endpoint = `${host}artists/${artistId}/assets/${postId}`
-
-    const res = await fetch(endpoint, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${verifyIdToken}`,
-      },
-      body: JSON.stringify({
-        priority_dsp: link,
-      }),
-    })
-
-    if (res.ok) {
-      return res.json()
-    }
-
-    throw new Error(res.statusText)
+    return api.patch(`/artists/${artistId}/assets/${postId}`, { priority_dsp: link }, verifyIdToken)
   },
 
+  /**
+   * @param {string[]} artistIds
+   * @param {string} accessToken
+   * @param {string} [verifyIdToken]
+   * @returns {Promise<any>}
+   */
   updateAccessToken: async (artistIds, accessToken, verifyIdToken) => {
     const response = []
     for (let i = 0; i < artistIds.length; i += 1) {
       const artistId = artistIds[i]
       if (
-        artistId === 'Y8uCfxBZkAVcpokW4S4b'
-        || artistId === '4FwK6p6y9xhpxZSGW2fR'
-        || artistId === 'vpdEYAT65K8gVcIuLpvO'
-        || artistId === 'z86bIwfwlIXwEtmKIML6'
+          artistId === 'Y8uCfxBZkAVcpokW4S4b'
+          || artistId === '4FwK6p6y9xhpxZSGW2fR'
+          || artistId === 'vpdEYAT65K8gVcIuLpvO'
+          || artistId === 'z86bIwfwlIXwEtmKIML6'
       ) {
-        const endpoint = `${this.url}artists/${artistId}`
-        const res = await fetch(endpoint, {
-          method: 'PATCH',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${verifyIdToken}`,
-          },
-          body: JSON.stringify({
-            integrations: {
-              facebook: {
-                access_token: accessToken,
-              },
+        const res = await api.patch(`/artists/${artistId}`, {
+          integrations: {
+            facebook: {
+              access_token: accessToken,
             },
-          }),
-        })
-        if (res.ok) {
-          const res = await res.json()
-          response.push(res)
-        }
+          },
+        }, verifyIdToken)
+        response.push(res)
       } else {
         response.push(`Not updating access token for ${artistId}`)
       }
