aa0154c77c7ac8567d2c5d4e714759287f0a8894
Benjamin E <ben@elwyn.co>
2020-03-26 16:22:03 +0000

    fix redirect test

--- components/InitUser.jsx
+++ components/InitUser.jsx
@@ -86,7 +86,7 @@ const InitUser = ({ children, setAuthSuccess = () => {} }) => {
     // Check if the user is on an auth only page,
     // if they are push to log in page
     const { pathname } = router
-    if (pathname !== ROUTES.LOGIN || pathname !== ROUTES.SIGN_UP) {
+    if (pathname !== ROUTES.LOGIN && pathname !== ROUTES.SIGN_UP) {
       Router.push(ROUTES.LOGIN)
     }
 
