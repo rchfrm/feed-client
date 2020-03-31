6dd2f014d077a65a193444f5fea788c2dabc06fa
Benjamin E <ben@elwyn.co>
2020-03-31 16:33:29 +0100

    don't show error if artist not owned by user

--- components/IntegrationErrorHandler.jsx
+++ components/IntegrationErrorHandler.jsx
@@ -4,9 +4,12 @@ import { useAsync } from 'react-async'
 import * as integrationErrorsHelpers from './helpers/integrationErrorsHelpers'
 import server from './helpers/server'
 import { ArtistContext } from './contexts/Artist'
+import { UserContext } from './contexts/User'
 
 import IntegrationErrorContent from './IntegrationErrorContent'
 
+
+// RUN THIS TO FETCH ERRORS
 const fetchError = async ({ artist, artistId }) => {
   if (!artist || !artistId) return
   console.log('artistId', artistId)
@@ -20,20 +23,29 @@ const fetchError = async ({ artist, artistId }) => {
   return errorResponse
 }
 
-
+// THE COMPONENT
 const IntegrationErrorHandler = () => {
   // Import artist context
   const {
     artist,
     artistId,
   } = React.useContext(ArtistContext)
-
+  // Import user
+  const { user } = React.useContext(UserContext)
+  // Does user own artist?
+  const { artists: userArtists } = user
+  const { role: artistRole } = userArtists.find(({ id }) => id === artistId)
+  const artistOwned = artistRole === 'owner' || artistRole === 'sysadmin'
+  if (!artistOwned) return null
+  // Run async request for errors
   const { data: integrationError, error, isPending } = useAsync({
     promiseFn: fetchError,
     watch: artistId,
     artist,
     artistId,
   })
+
+  console.log('user', user)
 
   console.log('integrationError', integrationError)
   console.log('isPending', isPending)
