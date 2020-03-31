159a2a4023fce40aaf74a3888e5b822300fab82e
Benjamin E <ben@elwyn.co>
2020-03-31 11:22:17 +0100

    add prop validation to alert

--- components/elements/Alert.jsx
+++ components/elements/Alert.jsx
@@ -1,8 +1,16 @@
 // IMPORT PACKAGES
 import React from 'react'
+import PropTypes from 'prop-types'
+
 import AlertButtons from './AlertButtons'
 
-function Alert({ contents, responseExpected, confirmationText, rejectionText, setAlert }) {
+function Alert({
+  contents,
+  responseExpected,
+  confirmationText,
+  rejectionText,
+  setAlert,
+}) {
   // If there are no contents, display nothing
   if (!contents) {
     return null
@@ -40,6 +48,26 @@ function Alert({ contents, responseExpected, confirmationText, rejectionText, se
       </div>
     </div>
   )
+}
+
+Alert.propTypes = {
+  contents: PropTypes.node,
+  responseExpected: PropTypes.bool,
+  confirmationText: PropTypes.string,
+  rejectionText: PropTypes.string,
+  setAlert(props, propName, componentName) {
+    if ((props.contents && (props[propName] === undefined || typeof (props[propName]) !== 'function'))) {
+      return new Error(`Please provide a ${propName} function to the ${componentName} component!`)
+    }
+  },
+}
+
+Alert.defaultProps = {
+  contents: null,
+  responseExpected: false,
+  confirmationText: 'Ok',
+  rejectionText: 'Cancel',
+  setAlert: () => {},
 }
 
 export default Alert
