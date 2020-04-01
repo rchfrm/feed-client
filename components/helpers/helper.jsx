695c51fae406712a779f9c14640d0dfd20a0864a
Benjamin E <ben@elwyn.co>
2020-04-01 11:04:29 +0100

    tidy docblock

--- components/helpers/helper.jsx
+++ components/helpers/helper.jsx
@@ -492,8 +492,8 @@ export default {
   },
 
   /**
-   * @param {string} url // To pass, url must include a protocol (ie, https?://)
-   * @returns {Boolean}
+   * @param {string} url To pass, url must include a protocol (ie, https?://)
+   * @returns {boolean}
    */
   testValidUrl: (url) => {
     const expression = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi
