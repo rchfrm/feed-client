1820fd0471eaf52cd97650595cca9cc03455dcb8
Benjamin E <ben@elwyn.co>
2020-04-02 18:09:43 +0100

    remove scrolling logic on posts all

--- components/PostsAll.jsx
+++ components/PostsAll.jsx
@@ -26,72 +26,17 @@ const resetScroll = () => {
 
 // Render list of posts and track the one that's currently visible
 function PostsAll({
-  numberOfPosts,
   posts,
   updateLink,
-  visiblePost,
-  setVisiblePost,
   togglePromotion,
+  loadMorePosts,
 }) {
-  // DEFINE POST DETAILS
-  const initialPostTrackerState = {
-    number: numberOfPosts,
-    scroll: 0,
-    width: 1 / numberOfPosts,
-  }
-
-  const postTrackerReducer = (postTrackerState, postTrackerAction) => {
-    switch (postTrackerAction.type) {
-      case 'set-scroll-position':
-        return {
-          ...postTrackerState,
-          scroll: postTrackerAction.payload.scroll,
-        }
-      case 'posts-added':
-        return {
-          ...postTrackerState,
-          number: postTrackerAction.payload.number,
-          scroll: visiblePost * (1 / postTrackerAction.payload.number),
-          width: 1 / postTrackerAction.payload.number,
-        }
-      default:
-        throw new Error(`Unable to find ${postTrackerAction.type} in postTrackerReducer`)
-    }
-  }
-
-  const [postTracker, setPostTracker] = React.useReducer(postTrackerReducer, initialPostTrackerState)
-
-  // TRACK SCROLL POSITION AND UPDATE TILE DETAILS STATE ACCORDINGLY
-  React.useEffect(() => {
-    const currentTile = Math.floor((postTracker.scroll + postTracker.width / 2) / postTracker.width) + 1
-    if (currentTile !== visiblePost) {
-      setVisiblePost(currentTile)
-    }
-  }, [postTracker, visiblePost, setVisiblePost])
-
-
-  // KEEP POST TRACKER IN SYNC WITH NUMBER OF POSTS SAVED TO STATE
-  React.useEffect(() => {
-    setPostTracker({
-      type: 'posts-added',
-      payload: {
-        number: numberOfPosts,
-      },
-    })
-  }, [numberOfPosts])
-
   // HANDLE SCROLL
   const handleScroll = debounce(() => {
     const scroller = document.getElementById('PostsAll__scroller')
     const scrollerWidth = scroller.scrollWidth
     const scrollPosition = scroller.scrollLeft
     const scrollPercentage = scrollPosition / scrollerWidth
-    setPostTracker({
-      type: 'set-scroll-position',
-      payload: {
-        scroll: scrollPercentage,
-      },
-    })
   }, 100)
 
   if (posts.length === 0) {
@@ -127,7 +72,6 @@ function PostsAll({
       <ul
         id="PostsAll__scroller"
         className={`frame posts ${styles.posts}`}
-        onScroll={handleScroll}
       >
         {postList}
       </ul>
