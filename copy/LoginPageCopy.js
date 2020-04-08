4c021dff695ff9c43175e6b4ab80a211e6b9e0d3
jay-see-jay <6587212+jay-see-jay@users.noreply.github.com>
2020-04-08 09:44:40 +0100

    Add space in between the words 'Privacy' and 'Policy' on /login

--- copy/LoginPageCopy.js
+++ copy/LoginPageCopy.js
@@ -1,5 +1,5 @@
 /* eslint-disable quotes */
 
 export default {
-  intro: `By clicking log in, you agree to our [Terms of Service](https://archform.ltd/terms) and [PrivacyPolicy](https://archform.ltd/privacy).`,
+  intro: `By clicking log in, you agree to our [Terms of Service](https://archform.ltd/terms) and [Privacy Policy](https://archform.ltd/privacy).`,
 }
