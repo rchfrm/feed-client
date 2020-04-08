2f0e3073c18c27b4cd7440ce5a3d6fbc58fc0b14
Benjamin E <ben@elwyn.co>
2020-04-08 16:38:49 +0100

    start signup email form

---
+++ components/SignupEmailForm.jsx
@@ -0,0 +1,124 @@
+import React from 'react'
+import Router from 'next/router'
+
+import { useImmerReducer } from 'use-immer'
+
+import { AuthContext } from './contexts/Auth'
+import { UserContext } from './contexts/User'
+
+import Input from './elements/Input'
+import Button from './elements/Button'
+import Error from './elements/Error'
+
+import * as ROUTES from '../constants/routes'
+import helpers from './helpers/helper'
+import styles from './LoginPage.module.css'
+
+const getInputType = (key) => {
+  if (key === 'email') return 'email'
+  if (key === 'passwordOne' || key === 'passwordTwo') return 'password'
+  return 'text'
+}
+
+const getInputLabel = (key) => {
+  if (key === 'firstName') return 'First name'
+  if (key === 'lastName') return 'Last name'
+  if (key === 'passwordOne') return 'Password'
+  if (key === 'passwordTwo') return 'Confirm password'
+  return helpers.capitalise(key)
+}
+
+
+const reducer = (draftState, action) => {
+  const { key, value } = action
+  draftState[key] = value
+}
+
+const SignupEmailForm = ({ setLoading }) => {
+  // Define component state
+  const [error, setError] = React.useState(null)
+  const [validForm, setValidForm] = React.useState(false)
+  // Get contexts
+  const { signUp } = React.useContext(AuthContext)
+  const { createUser } = React.useContext(UserContext)
+  // Define form state
+  const initialSignupState = {
+    email: '',
+    firstName: '',
+    lastName: '',
+    passwordOne: '',
+    passwordTwo: '',
+  }
+  const [signupDetails, setSignupDetails] = useImmerReducer(reducer, initialSignupState)
+  // Handle input changes
+  const onInputChange = (e) => {
+    console.log(e)
+    // setSignupDetails()
+  }
+  // Define array of form inputs
+  const formInputs = React.useMemo(() => {
+    Object.entries(initialSignupState).map(([key, value]) => {
+      const inputType = getInputType(key)
+      const label = getInputLabel(key)
+      const name = key
+      return {
+        inputType,
+        name,
+        value,
+        label,
+      }
+    })
+  }, [signupDetails])
+  // Handle form submit
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const { email, passwordOne, firstName, lastName } = signupDetails
+    setLoading(true)
+    const signupRes = await signUp(email, passwordOne)
+      .catch((error) => {
+        setError(error)
+        setLoading(false)
+      })
+    const userRes = await createUser(firstName, lastName)
+      .catch((error) => {
+        setError(error)
+        setLoading(false)
+      })
+    console.log('signupRes', signupRes)
+    console.log('userRes', userRes)
+    // Router.push(ROUTES.CONNECT_ACCOUNTS)
+  }
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+
+      <Error error={error} />
+
+      {/* All form inputs */}
+      {formInputs.map(({ inputType, name, value, label }) => {
+        return (
+          <Input
+            key={name}
+            className={styles.input}
+            handleChange={onInputChange}
+            name={name}
+            value={value}
+            type={inputType}
+            label={label}
+          />
+        )
+      })}
+
+      <Button
+        className={styles.signupButton}
+        version="black  wide"
+        disabled={!validForm}
+        type="sumbit"
+      >
+        sign up
+      </Button>
+    </form>
+  )
+}
+
+export default SignupEmailForm
