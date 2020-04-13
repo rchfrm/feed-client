1d4ee866b5310f2e27f553fbc07e7c4f8008ae25
Benjamin E <ben@elwyn.co>
2020-04-13 16:53:37 +0100

    don't show main page contents if no connected artists

--- components/page/InsightsPage.jsx
+++ components/page/InsightsPage.jsx
@@ -3,10 +3,15 @@ import React from 'react'
 // IMPORT COMPONENTS
 // IMPORT CONTEXTS
 import { NavigationContext } from '../contexts/Navigation'
+import { UserContext } from '../contexts/User'
 // IMPORT ELEMENTS
 import PageHeader from '../PageHeader'
 // IMPORT PAGES
 import InsightsLoader from '../InsightsLoader'
+// IMPORT ASSETS
+import MarkdownText from '../elements/MarkdownText'
+import copy from '../../copy/global'
+// IMPORT HELPERS
 
 function InsightsPage() {
   // SHOW / HIDE NAVIGATION
@@ -16,11 +21,19 @@ function InsightsPage() {
     navDispatch({ type: 'hide' })
   }, [navDispatch])
 
+  // Get user context
+  const { user } = React.useContext(UserContext)
+
   return (
     <div className={`${className}`}>
 
       <PageHeader heading="Insights" />
-      <InsightsLoader />
+
+      {user.artists.length === 0 ? (
+        <MarkdownText className="ninety-wide  h4--text" markdown={copy.noArtists} />
+      ) : (
+        <InsightsLoader />
+      )}
 
     </div>
   )
