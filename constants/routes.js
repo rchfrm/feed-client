0a95ded4a47ea1783483ff4d2734cc2cfd86fccc
Benjamin E <ben@elwyn.co>
2020-04-13 11:49:25 +0100

    change to email login by changing path

--- constants/routes.js
+++ constants/routes.js
@@ -2,6 +2,7 @@ export const SIGN_UP = '/join'
 export const CONNECT_ACCOUNTS = '/connect-accounts'
 export const THANK_YOU = '/thank-you'
 export const LOGIN = '/login'
+export const LOGIN_EMAIL = '/login/email'
 export const PASSWORD_FORGET = '/forgot-password'
 export const POSTS = '/'
 export const INSIGHTS = '/insights'
@@ -16,6 +17,7 @@ export const NOTIFICATIONS = '/notifications'
 export const HOME = POSTS
 // Array of restricted pages
 export const restrictedPages = [
+  LOGIN_EMAIL,
   CONNECT_ACCOUNTS,
   THANK_YOU,
   POSTS,
