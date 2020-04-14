0407c00078d86815ff1e55091f5e3acd8c2e6cae
Benjamin E <ben@elwyn.co>
2020-04-14 12:20:05 +0100

    modify connect accounts page based on where it appears

--- components/page/ConnectAccountsPage.jsx
+++ components/page/ConnectAccountsPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 // IMPORT CONTEXTS
 import { NavigationContext } from '../contexts/Navigation'
@@ -6,7 +7,7 @@ import { NavigationContext } from '../contexts/Navigation'
 import ConnectAccountsLoader from '../ConnectAccountsLoader'
 import PageHeader from '../PageHeader'
 
-const ConnectAccountsPage = () => {
+const ConnectAccountsPage = ({ heading, punctuation, onSignUp }) => {
   // SHOW / HIDE NAVIGATION
   const { navState, navDispatch } = React.useContext(NavigationContext)
   const className = navState.visible ? 'hidden' : ''
@@ -18,16 +19,25 @@ const ConnectAccountsPage = () => {
   return (
     <div className={`page--container ${className}`}>
 
-      <PageHeader heading="connect accounts" />
+      <PageHeader heading={heading} punctuation={punctuation} />
 
-      <ConnectAccountsLoader />
+      <ConnectAccountsLoader onSignUp={onSignUp} />
 
     </div>
   )
 }
 
 ConnectAccountsPage.propTypes = {
-
+  heading: PropTypes.string,
+  punctuation: PropTypes.string,
+  onSignUp: PropTypes.bool,
 }
+
+ConnectAccountsPage.defaultProps = {
+  heading: 'connect accounts',
+  punctuation: '.',
+  onSignUp: false,
+}
+
 
 export default ConnectAccountsPage
