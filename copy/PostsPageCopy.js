0f07a9d97395902dcf8c1f82d745cec3651b1f53
jay-see-jay <6587212+jay-see-jay@users.noreply.github.com>
2020-04-14 11:18:00 +0100

    update posts page copy

--- copy/PostsPageCopy.js
+++ copy/PostsPageCopy.js
@@ -2,14 +2,11 @@
 import * as ROUTES from '../constants/routes'
 
 export default {
-  intro: `Below are the posts that **Feed** hasn’t promoted yet, starting with the most recent. **Feed** works better the more you select, but just untick any you'd rather not promote.
-
-  Change the link on each post to specify where people should go when they click it.   
-  `,
+  intro: `Below are the posts Feed hasn't yet made into ads. It works better the more that are selected, but untick any you'd rather not promote. Feed excludes any posts that are more than a month old.`,
   budgetIntro: `Enter the amount you'd like to spend each day:`,
-  budgetOutro: `A higher budget lets **Feed** reach more people, but even a smaller amount can have a big impact. We recommended starting with £3/day so **Feed** can reach both new people and those who have engaged with you before. 
+  budgetOutro: `A higher budget lets **Feed** reach more people, but even a smaller amount can have a big impact. We recommend at least £3/day, which means **Feed** can reach new people and those who have engaged with you before. 
 
-  Included in this budget is our service fee - you can find more details on the [account page](${ROUTES.ACCOUNT}).
+  To pause ads at any time, simply set your budget back to 0.
   `,
 
   noPostsCopy: (platforms) => `It looks like you haven't posted for a little while, and by default **Feed** only promotes posts from the last month.
