15ba6d4639ecbdbb7887f1cc4f65ea85cc0d972f
Benjamin E <ben@elwyn.co>
2020-04-16 12:10:45 +0100

    change connect accounts copy

--- copy/ConnectAccountsCopy.js
+++ copy/ConnectAccountsCopy.js
@@ -3,9 +3,11 @@
 export default {
   signupIntro: `Thank you for signing up to **Feed**!
 
-In order for Feed to promote your posts and to show you the results, we need permission to manage to your business pages.
+In order for Feed to promote your posts and show you the results, we need permission to manage to your business profiles.
+
+To continue click on the button below. We'll never promote your posts without your approval first.
   
-To continue click on the button below. We’ll never post anything without your approval.`,
+Please make sure you opt in to all permissions shown, as Feed needs these to work properly.`,
 
   smallLegalText: `This allows us to connect to Facebook so that we can show you your data and promote posts on your behalf, we'll never post anything without your approval.`,
 }
