8615a39bbc6077fd35069f5ba77b23d680f27b72
Benjamin E <ben@elwyn.co>
2020-04-21 12:04:17 +0100

    no h tags in results copy

--- copy/ResultsPageCopy.js
+++ copy/ResultsPageCopy.js
@@ -4,12 +4,12 @@ import * as ROUTES from '../constants/routes'
 export default {
   intro: `Below are the results from your promoted posts. **Feed** automatically keeps the best ones running and turns off those that don’t work as well, but to manually switch one off a post just hit the red ‘x’.`,
 
-  noResultsWithBudget: `### **Feed** is setting up your posts for promotion.
+  noResultsWithBudget: `**Feed** is setting up your posts for promotion.
   
   There may be a delay whilst posts await approval, once promotions have started you'll be able to see your results below.
   `,
 
-  noResultsNoBudget: `### **Feed** hasn't started promoting your posts yet, get started by entering a daily budget [here](${ROUTES.POSTS}).`,
+  noResultsNoBudget: `**Feed** hasn't started promoting your posts yet, get started by entering a daily budget [here](${ROUTES.POSTS}).`,
 
   resultToggleWarning: (promotion_enabled) => {
     if (promotion_enabled) {
