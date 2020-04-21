28edf2fba3f67971c4dd81d692d28a31545649c0
Benjamin E <ben@elwyn.co>
2020-04-21 13:29:14 +0100

    create post connections components out of old integration components

---
+++ components/PostConnectionsConnection.jsx
@@ -0,0 +1,185 @@
+// IMPORT PACKAGES
+import React from 'react'
+// IMPORT COMPONENTS
+import PostConnectionsLink from './PostConnectionsLink'
+import PostConnectionsIcons from './PostConnectionsIcons'
+// IMPORT CONTEXTS
+// IMPORT ELEMENTS
+import Button from './elements/Button'
+import Icon from './elements/Icon'
+import Spinner from './elements/Spinner'
+// IMPORT ASSETS
+// IMPORT CONSTANTS
+import brandColors from '../constants/brandColors'
+// IMPORT HELPERS
+import helper from './helpers/helper'
+import server from './helpers/server'
+// IMPORT STYLES
+import styles from './Integrations.module.css'
+
+
+const PostConnectionsConnection = ({
+  url = '',
+  platform,
+  artistId,
+  valid,
+  priorityDSP,
+  setPriorityDSP,
+  setConnections,
+}) => {
+  // DEFINE STATES
+  const [value, setValue] = React.useState(url)
+  const [loading, setLoading] = React.useState(false)
+  const [disabled, setDisabled] = React.useState()
+
+  // Set initial disabled state
+  React.useEffect(() => {
+    setDisabled(!!(value === ''))
+  }, [])
+
+  // Toggle the value for valid in the integrations state
+  const toggleValid = (state) => {
+    setConnections({
+      type: 'toggle-platform-validity',
+      payload: {
+        platform,
+        state,
+      },
+    })
+  }
+
+  // Send updated links to the server
+  const saveLink = async (link) => {
+    setLoading(true)
+
+    // Send a patch request to the server to update the artist
+    const urlType = helper.convertPlatformToPriorityDSP(platform)
+    // Make sure the value is a link
+    const updatedArtist = await server.saveLink(artistId, link, urlType)
+    setConnections({
+      type: 'set-platform',
+      payload: {
+        platform,
+        name: urlType,
+        url: updatedArtist[urlType],
+      },
+    })
+    setLoading(false)
+  }
+
+  const addProtocol = (link) => {
+    const linkWithProtocol = link
+      // Strip protocol
+      .replace(/(^\w+:|^)\/\//, '')
+      // Add protocol back in
+      .replace(/^/, 'http://')
+    return linkWithProtocol
+  }
+
+  // Handle clicks on the integrations edit button(s)
+  const handleClick = async (e) => {
+    e.preventDefault()
+
+    // Show an alert if the user tries to edit the Facebook or Instagram URLs
+    if (platform === 'facebook' || platform === 'instagram') {
+      // eslint-disable-next-line
+      window.alert(`To connect a ${helper.capitalise(platform)} page, please contact us at services@archform.ltd`)
+      return
+    }
+
+    const link = value === '' ? value : addProtocol(value)
+
+    // Don't allow invalid links
+    const linkValid = link === '' ? true : helper.testValidUrl(link)
+    if (!linkValid) {
+      toggleValid(false)
+      // eslint-disable-next-line
+      window.alert('Please include a valid link')
+      return
+    }
+
+    // If there is already a link, set toggle valid to false
+    // in order to show the input field
+    // If the value of the input field is the same as the pre-existing url,
+    // toggle valid to true, and hide the input field
+    if (valid || value === url) {
+      toggleValid()
+      return
+    }
+
+    // Otherwise, send the updated url to the server
+    await saveLink(link)
+      .catch(err => {
+        setLoading(false)
+        // TODO: Find a way to show errors well
+        console.log(err)
+      })
+
+    // Reset to disabled if empty
+    if (!link) {
+      setDisabled(true)
+    }
+  }
+
+  return (
+    <li className={styles.integrarionsListItem} key={platform}>
+
+      <PostConnectionsIcons
+        artistId={artistId}
+        platform={platform}
+        priorityDSP={priorityDSP}
+        setConnections={setConnections}
+        setPriorityDSP={setPriorityDSP}
+        valid={valid}
+      />
+
+      <div className={styles['integration-link']}>
+        <PostConnectionsLink
+          platform={platform}
+          setValue={setValue}
+          setDisabled={setDisabled}
+          url={url}
+          valid={valid}
+          value={value}
+        />
+      </div>
+
+      <div className={styles['integration-edit']}>
+        <Button version="text" onClick={handleClick} disabled={disabled} bgColor={brandColors.white}>
+          <ConnectionEdit disabled={disabled} loading={loading} valid={valid} />
+        </Button>
+      </div>
+
+    </li>
+  )
+}
+
+export default PostConnectionsConnection
+
+
+function ConnectionEdit({
+  disabled,
+  loading,
+  valid,
+}) {
+  if (loading) {
+    return <Spinner width={15} color={brandColors.grey} />
+  }
+
+  if (valid) {
+    return (
+      <Icon
+        version="pencil"
+        color="black"
+        width={15}
+      />
+    )
+  }
+  return (
+    <Icon
+      version="tick"
+      color={disabled ? [brandColors.disabledColorText] : [brandColors.textColor]}
+      width={15}
+    />
+  )
+}
---
+++ components/PostConnectionsIcons.jsx
@@ -0,0 +1,155 @@
+// IMPORT PACKAGES
+import React from 'react'
+// IMPORT COMPONENTS
+// IMPORT CONTEXTS
+// IMPORT ELEMENTS
+import Icon from './elements/Icon'
+import Spinner from './elements/Spinner'
+import Alert, { alertReducer, initialAlertState } from './elements/Alert'
+// IMPORT ASSETS
+import AsteriskIcon from './icons/AsterixIcon'
+// IMPORT CONSTANTS
+import dataSourceDetails from '../constants/dataSources'
+import brandColors from '../constants/brandColors'
+// IMPORT HELPERS
+import helper from './helpers/helper'
+import server from './helpers/server'
+// IMPORT STYLES
+import styles from './Integrations.module.css'
+
+
+function ConfirmPriorityDSPChange({ platform }) {
+  return (
+    <div>
+      <h1>Are you sure?</h1>
+      <p>
+        Clicking 'Yes' will change the default link in your promoted posts to your
+        {' '}
+        {helper.capitalise(platform)}
+        {' '}
+        page.
+      </p>
+    </div>
+  )
+}
+
+
+const PostConnectionsIcons = ({
+  artistId,
+  platform,
+  priorityDSP,
+  setPriorityDSP,
+  valid,
+}) => {
+  // DEFINE DEFAULT COLOUR
+  const priority = priorityDSP === platform
+  let defaultColor = brandColors.greyLight
+  if (valid && priority) {
+    defaultColor = brandColors.textColor
+  } else if (!valid) {
+    defaultColor = brandColors.bgColor
+  }
+
+  // DEFINE STATES
+  const [color, setColor] = React.useState(defaultColor)
+  const [loading, setLoading] = React.useState(false)
+
+  // DEFINE ALERT STATE
+  const [alert, setAlert] = React.useReducer(alertReducer, initialAlertState)
+
+  // DEFINE ALERT REPONSES
+  const resetAlert = () => setAlert({ type: 'reset-alert' })
+  const acceptAlert = () => setAlert({ type: 'set-positive-response' })
+
+  const handleMouseEnter = () => {
+    if (valid && !priority) {
+      setColor(brandColors.greyDark)
+    }
+  }
+
+  const handleMouseLeave = () => {
+    setColor(defaultColor)
+  }
+
+  const handleClick = e => {
+    e.preventDefault()
+    if (priority || !valid) {
+      return
+    }
+
+    setAlert({
+      type: 'show-alert',
+      payload: {
+        contents: <ConfirmPriorityDSPChange platform={platform} />,
+      },
+    })
+  }
+
+  const updatePriorityDSP = React.useCallback(async (artistId, platform) => {
+    const result = await server.updatePriorityDSP(artistId, platform)
+    return result
+  }, [])
+
+  React.useEffect(() => {
+    if (!alert.response) {
+      return
+    }
+
+    setLoading(true)
+    updatePriorityDSP(artistId, platform)
+      .then(res => {
+        setPriorityDSP(res.priority_dsp)
+        setLoading(false)
+      }).catch(err => {
+        // TODO: PROPERLY HANDLE THIS ERROR
+        console.log(err)
+        setLoading(false)
+      })
+  }, [alert.response, artistId, platform, setPriorityDSP, updatePriorityDSP])
+
+  React.useEffect(() => {
+    setColor(defaultColor)
+  }, [defaultColor, priorityDSP])
+
+  return (
+    <div className={styles['integration-icons']}>
+
+      <Alert
+        confirmationText={alert.confirmationText}
+        contents={alert.contents}
+        rejectionText={alert.rejectionText}
+        responseExpected={alert.responseExpected}
+        resetAlert={resetAlert}
+        acceptAlert={acceptAlert}
+      />
+
+      {loading
+        ? <Spinner width={15} color={brandColors.grey} />
+        : (
+          <div
+            role="button"
+            className={styles['default-integration-icon']}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            onClick={handleClick}
+            style={{ cursor: valid && !priority ? 'pointer' : 'initial' }}
+          >
+
+            <AsteriskIcon fill={color} width={15} />
+
+          </div>
+        )}
+
+      <div className={styles['integration-platform-icon']}>
+        <Icon
+          version={platform}
+          color={dataSourceDetails[platform] ? dataSourceDetails[platform].color : 'black'}
+          width={20}
+        />
+      </div>
+
+    </div>
+  )
+}
+
+export default PostConnectionsIcons
