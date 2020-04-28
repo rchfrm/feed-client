5f3e1eb0d670f5546e97a61811622d80ef9e9673
Benjamin E <ben@elwyn.co>
2020-04-28 15:48:09 +0100

    start data source object

---
+++ constants/insightDataSources.js
@@ -0,0 +1,322 @@
+export default {
+  apple_music_streams_7d: {
+    id: 'apple_music_streams_7d',
+    platform: 'apple',
+    visible: false,
+    breakdown: false,
+    period: '',
+    title: 'Streams',
+    subtitle: '',
+  },
+
+  facebook_ad_impressions_feed: {
+    id: 'facebook_ad_impressions_feed',
+    platform: 'facebook',
+    visible: true,
+    breakdown: false,
+    period: '',
+    title: 'Streams',
+    subtitle: '',
+  },
+
+  facebook_ad_spend: {
+    id: 'facebook_ad_spend',
+    platform: 'facebook',
+    visible: true,
+    breakdown: false,
+    period: '',
+    title: 'Streams',
+    subtitle: '',
+  },
+
+  facebook_ad_spend_feed: {
+    id: 'facebook_ad_spend_feed',
+    platform: 'facebook',
+    visible: true,
+    breakdown: false,
+    period: '',
+    title: 'Streams',
+    subtitle: '',
+  },
+
+  facebook_engaged_1y: {
+    id: 'facebook_engaged_1y',
+    platform: 'facebook',
+    visible: true,
+    breakdown: false,
+    period: '',
+    title: 'Streams',
+    subtitle: '',
+  },
+
+  facebook_engaged_28d: {
+    id: 'facebook_engaged_28d',
+    platform: 'facebook',
+    visible: true,
+    breakdown: false,
+    period: '',
+    title: 'Streams',
+    subtitle: '',
+  },
+
+  facebook_engaged_7d: {
+    id: 'facebook_engaged_7d',
+    platform: 'facebook',
+    visible: true,
+    breakdown: false,
+    period: '',
+    title: 'Streams',
+    subtitle: '',
+  },
+
+  facebook_likes: {
+    id: 'facebook_likes',
+    platform: 'facebook',
+    visible: true,
+    breakdown: false,
+    period: '',
+    title: 'Streams',
+    subtitle: '',
+  },
+
+  facebook_page_fans_city: {
+    id: 'facebook_page_fans_city',
+    platform: 'facebook',
+    visible: true,
+    breakdown: false,
+    period: '',
+    title: 'Streams',
+    subtitle: '',
+  },
+
+  facebook_page_fans_country: {
+    id: 'facebook_page_fans_country',
+    platform: 'facebook',
+    visible: true,
+    breakdown: false,
+    period: '',
+    title: 'Streams',
+    subtitle: '',
+  },
+
+  facebook_page_fans_gender_age: {
+    id: 'facebook_page_fans_gender_age',
+    platform: 'facebook',
+    visible: true,
+    breakdown: false,
+    period: '',
+    title: 'Streams',
+    subtitle: '',
+  },
+
+  instagram_audience_city: {
+    id: 'instagram_audience_city',
+    platform: 'instagram',
+    visible: true,
+    breakdown: false,
+    period: '',
+    title: 'Streams',
+    subtitle: '',
+  },
+
+  instagram_audience_country: {
+    id: 'instagram_audience_country',
+    platform: 'instagram',
+    visible: true,
+    breakdown: false,
+    period: '',
+    title: 'Streams',
+    subtitle: '',
+  },
+
+  instagram_audience_gender_age: {
+    id: 'instagram_audience_gender_age',
+    platform: 'instagram',
+    visible: true,
+    breakdown: false,
+    period: '',
+    title: 'Streams',
+    subtitle: '',
+  },
+
+  instagram_engaged_1y: {
+    id: 'instagram_engaged_1y',
+    platform: 'instagram',
+    visible: true,
+    breakdown: false,
+    period: '',
+    title: 'Streams',
+    subtitle: '',
+  },
+
+  instagram_engaged_28d: {
+    id: 'instagram_engaged_28d',
+    platform: 'instagram',
+    visible: true,
+    breakdown: false,
+    period: '',
+    title: 'Streams',
+    subtitle: '',
+  },
+
+  instagram_engaged_7d: {
+    id: 'instagram_engaged_7d',
+    platform: 'instagram',
+    visible: true,
+    breakdown: false,
+    period: '',
+    title: 'Streams',
+    subtitle: '',
+  },
+
+  instagram_follower_count: {
+    id: 'instagram_follower_count',
+    platform: 'instagram',
+    visible: true,
+    breakdown: false,
+    period: '',
+    title: 'Streams',
+    subtitle: '',
+  },
+
+  instagram_profile_views: {
+    id: 'instagram_profile_views',
+    platform: 'instagram',
+    visible: true,
+    breakdown: false,
+    period: '',
+    title: 'Streams',
+    subtitle: '',
+  },
+
+  instagram_profile_views_7d: {
+    id: 'instagram_profile_views_7d',
+    platform: 'instagram',
+    visible: true,
+    breakdown: false,
+    period: '',
+    title: 'Streams',
+    subtitle: '',
+  },
+
+  instagram_reach: {
+    id: 'instagram_reach',
+    platform: 'instagram',
+    visible: true,
+    breakdown: false,
+    period: '',
+    title: 'Streams',
+    subtitle: '',
+  },
+
+  instagram_reach_7d: {
+    id: 'instagram_reach_7d',
+    platform: 'instagram',
+    visible: true,
+    breakdown: false,
+    period: '',
+    title: 'Streams',
+    subtitle: '',
+  },
+
+  soundcloud_follower_count: {
+    id: 'soundcloud_follower_count',
+    platform: 'soundcloud',
+    visible: true,
+    breakdown: false,
+    period: '',
+    title: 'Streams',
+    subtitle: '',
+  },
+
+  soundcloud_listeners_7d: {
+    id: 'soundcloud_listeners_7d',
+    platform: 'soundcloud',
+    visible: true,
+    breakdown: false,
+    period: 'Weekly',
+    title: 'Listeners',
+    subtitle: '',
+  },
+
+  spotify_follower_count: {
+    id: 'spotify_follower_count',
+    platform: 'spotify',
+    visible: true,
+    breakdown: false,
+    period: '',
+    title: 'Followers',
+    subtitle: '',
+  },
+
+  spotify_listeners_30d: {
+    id: 'spotify_listeners_30d',
+    platform: 'spotify',
+    visible: true,
+    breakdown: false,
+    period: 'Monthly',
+    title: 'Listeners',
+    subtitle: '',
+  },
+
+  spotify_listeners_7d: {
+    id: 'spotify_listeners_7d',
+    platform: 'spotify',
+    visible: true,
+    breakdown: false,
+    period: 'Weekly',
+    title: 'Listeners',
+    subtitle: '',
+  },
+
+  spotify_streams_7d: {
+    id: 'spotify_streams_7d',
+    platform: 'spotify',
+    visible: true,
+    breakdown: false,
+    period: 'Weekly',
+    title: 'Streams',
+    subtitle: '',
+  },
+
+  twitter_follower_count: {
+    id: 'twitter_follower_count',
+    platform: 'twitter',
+    visible: true,
+    breakdown: false,
+    period: '',
+    title: 'Followers',
+    subtitle: '',
+  },
+
+  youtube_subscriber_count: {
+    id: 'youtube_subscriber_count',
+    platform: 'youtube',
+    visible: true,
+    breakdown: false,
+    period: '',
+    title: 'Subscribers',
+    subtitle: '',
+  },
+
+  youtube_views: {
+    id: 'youtube_views',
+    platform: 'youtube',
+    visible: true,
+    breakdown: false,
+    period: '',
+    title: 'Views',
+    subtitle: '',
+  },
+
+  youtube_views_7d: {
+    id: 'youtube_views_7d',
+    platform: 'youtube',
+    visible: true,
+    breakdown: true,
+    period: 'weekly',
+    title: 'Views',
+    subtitle: '',
+  },
+
+}
