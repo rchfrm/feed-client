9096f0f0f90c66931d30ae6791cf9a084b990554
Benjamin E <ben@elwyn.co>
2020-04-30 10:30:51 +0100

    move chart bar overlay to top of comp

--- components/ChartBar.jsx
+++ components/ChartBar.jsx
@@ -20,6 +20,37 @@ moment.updateLocale('en', {
 })
 
 
+function BarChartOverlay({ max: maxValue, min: minValue, labels }) {
+  const max = helper.abbreviateNumber(maxValue)
+  const min = helper.abbreviateNumber(minValue)
+  const mid = helper.abbreviateNumber(((maxValue - minValue) / 2) + minValue)
+
+  const labelList = []
+
+  labels.forEach((label, index) => {
+    if (
+      index === 0
+      || index === labels.length - 1
+      || index === Math.round((labels.length - 1) / 2)
+    ) {
+      labelList.push(
+        <li key={label}>{label}</li>,
+      )
+    }
+  })
+
+  return (
+    <div className={styles.chartOverlay}>
+      <div className={styles.chartMax}>{max}</div>
+      <hr className={styles.hr} />
+      <div className={styles.chartMid}>{mid}</div>
+      <div className={styles.chartMin}>{min}</div>
+      <ul className={styles.xAxisLabels}>{labelList}</ul>
+    </div>
+  )
+}
+
+
 function ChartBar({
   data,
   earliestDataPoint,
@@ -240,34 +271,4 @@ ChartBar.propTypes = {
   data: PropTypes.object.isRequired,
   earliestDataPoint: PropTypes.string.isRequired,
   latestDataPoint: PropTypes.string.isRequired,
-}
-
-function BarChartOverlay({ max: maxValue, min: minValue, labels }) {
-  const max = helper.abbreviateNumber(maxValue)
-  const min = helper.abbreviateNumber(minValue)
-  const mid = helper.abbreviateNumber(((maxValue - minValue) / 2) + minValue)
-
-  const labelList = []
-
-  labels.forEach((label, index) => {
-    if (
-      index === 0
-      || index === labels.length - 1
-      || index === Math.round((labels.length - 1) / 2)
-    ) {
-      labelList.push(
-        <li key={label}>{label}</li>,
-      )
-    }
-  })
-
-  return (
-    <div className={styles.chartOverlay}>
-      <div className={styles.chartMax}>{max}</div>
-      <hr className={styles.hr} />
-      <div className={styles.chartMid}>{mid}</div>
-      <div className={styles.chartMin}>{min}</div>
-      <ul className={styles.xAxisLabels}>{labelList}</ul>
-    </div>
-  )
 }
