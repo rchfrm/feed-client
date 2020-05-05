658352b88f9f6f1cb9b8443c38ba8c86bcaae114
Benjamin E <ben@elwyn.co>
2020-05-05 17:02:30 +0100

    fix non cumulative charts

--- components/ChartBar.jsx
+++ components/ChartBar.jsx
@@ -180,6 +180,7 @@ function ChartBar({
 
     // DEFINE THE DATASET(S) TO DISPLAY
     const dataArray = chartHelpers.createDataArray(periodDates, currentDataSource, data)
+    console.log('dataArray', dataArray)
     // Set the limits of the charts y axis
     const max = helper.maxArrayValue(dataArray)
     const min = helper.minArrayValue(dataArray)
@@ -190,6 +191,7 @@ function ChartBar({
       max: Math.round(max * 1.01) + maxLimitModifier,
       min: Math.round(min * 0.99),
     }
+    console.log('newChartLimit', newChartLimit)
     setChartLimit(newChartLimit)
     const increaseArr = dataArray.map((datum, index) => {
       const value = datum - dataArray[index - 1]
@@ -212,7 +214,7 @@ function ChartBar({
       {
         ...baseBarConfig,
         label: currentDataSource,
-        data: carriedArr,
+        data: cumulative ? carriedArr : dataArray,
         backgroundColor: cumulative ? lightColor : chartColor,
       },
     ]
