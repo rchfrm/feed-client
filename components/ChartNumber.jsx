0d36f25c89b7beacc3416f353921d0c79cc5773a
Benjamin E <ben@elwyn.co>
2020-05-05 18:00:55 +0100

    show cumulative data on number view for daily data

--- components/ChartNumber.jsx
+++ components/ChartNumber.jsx
@@ -6,20 +6,35 @@ import helper from './helpers/helper'
 import brandColors from '../constants/brandColors'
 import styles from './InsightsPage.module.css'
 
+const combineDailyData = (dailyData) => {
+  return Object.values(dailyData).reduce((total, value) => {
+    return total + value
+  }, 0)
+}
+
 const ChartNumber = ({ data, artistCurrency }) => {
   // DEFINE STATES
   const [displayData, setDisplayData] = React.useState('')
   const [dataColor, setDataColor] = React.useState('')
+  const [title, setTitle] = React.useState('')
+  const [subtitle, setSubtitle] = React.useState('')
   // UPDATE ON DATA CHANGE
   React.useEffect(() => {
-    console.log('data', data)
-    const { source, platform, currency } = data
+    const { source, platform, currency, dataType } = data
     if (!source) return
-    const { mostRecent: { value } } = data
+    const { mostRecent: { value: mostRecentValue }, dailyData } = data
+    const calculatedValue = dataType === 'daily' ? combineDailyData(dailyData) : mostRecentValue
     const color = brandColors[platform]
-    const valueFormatted = currency ? helper.formatCurrency(value, artistCurrency) : helper.abbreviateNumber(value)
+    const valueFormatted = currency ? helper.formatCurrency(calculatedValue, artistCurrency) : helper.abbreviateNumber(calculatedValue)
+    // Set data viz
     setDisplayData(valueFormatted)
     setDataColor(color)
+    // Set titles
+    const title = dataType === 'daily' ? `Total ${data.shortTitle}` : data.shortTitle
+    const basicSubtitle = `(${data.subtitle || data.period})`
+    const subtitle = dataType === 'daily' ? `${basicSubtitle} so far` : basicSubtitle
+    setTitle(title)
+    setSubtitle(subtitle)
   }, [data.source])
   return (
     <div className={styles.chartNumber__container}>
@@ -30,8 +45,8 @@ const ChartNumber = ({ data, artistCurrency }) => {
         {displayData}
       </p>
       <p className={styles.chartNumber__title}>
-        <span className={styles.title}>{data.shortTitle}</span>
-        <span className={styles.subtitle}> ({data.subtitle || data.period})</span>
+        <span className={styles.title}>{title} </span>
+        <span className={styles.subtitle}>{subtitle}</span>
       </p>
     </div>
   )
