12e816bca14f418561773ef3168918d156bb420f
Benjamin E <ben@elwyn.co>
2020-05-12 15:04:31 +0100

    Initial tailwind setup (#96)
    * install tailwind
    
    * start config

--- postcss.config.js
+++ postcss.config.js
@@ -1,22 +1,33 @@
+const purgecss = [
+  '@fullhuman/postcss-purgecss',
+  {
+    content: ['./components/**/*.jsx', './pages/**/*.jsx'],
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+  },
+]
+
 module.exports = {
-  plugins: {
-    'postcss-flexbugs-fixes': {},
-    'postcss-import': {},
-    'postcss-mixins': {},
-    'postcss-calc': {},
-    'postcss-nested': {},
-    'postcss-extend': {},
-    'postcss-color-mod-function': {
+  plugins: [
+    'postcss-flexbugs-fixes',
+    'postcss-import',
+    'postcss-mixins',
+    'postcss-calc',
+    'postcss-nested',
+    'postcss-extend',
+    ['postcss-color-mod-function', {
       importFrom: [
         './assets/styles/vars.css',
       ],
-    },
-    'postcss-preset-env': {
+    }],
+    ['postcss-preset-env', {
       stage: 1,
       preserve: false,
       importFrom: [
         './assets/styles/vars.css',
       ],
-    },
-  },
+    }],
+    'tailwindcss',
+    'autoprefixer',
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+  ],
 }
