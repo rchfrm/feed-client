7332249725a74dbb2722374b0acdd00002863e15
Benjamin E <ben@elwyn.co>
2020-05-12 13:27:48 +0100

    handle error in artist image

--- components/elements/ArtistImage.jsx
+++ components/elements/ArtistImage.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import ProfileIcon from '../icons/ProfileIcon'
+
+import brandColors from '../../constants/brandColors'
+
 const ArtistImage = ({ pageId, size, name, className }) => {
-  const src = `//graph.facebook.com/${pageId}/picture?type=${size}`
+  const [src, setSrc] = React.useState('')
+  const [error, setError] = React.useState(false)
+
+  React.useEffect(() => {
+    const src = `//graph.facebook.com/${pageId}/picture?type=${size}`
+    setSrc(src)
+    setError(false)
+  }, [pageId])
+
+  const onError = () => {
+    setError(true)
+  }
+
+  if (error) return <ProfileIcon fill={brandColors.greyDark} />
+
   return (
-    <img className={className} alt={name} src={src} />
+    <img className={className} alt={name} src={src} onError={onError} />
   )
 }
 
@@ -15,8 +33,8 @@ ArtistImage.propTypes = {
 }
 
 ArtistImage.defaultProps = {
-  className: '',
   size: 'normal',
+  className: '',
 }
 
 export default ArtistImage
