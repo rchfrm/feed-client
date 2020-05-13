b8e6f84801949a44d16f0bb07e4c17c21e83b953
Benjamin E <ben@elwyn.co>
2020-05-13 17:37:01 +0100

    show spinner in sub nav button when artist loading

--- components/TheSubNavButton.jsx
+++ components/TheSubNavButton.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Spinner from './elements/Spinner'
 import ArtistImage from './elements/ArtistImage'
 import CloseCircle from './icons/CloseCircle'
 
@@ -9,7 +10,7 @@ import { ArtistContext } from './contexts/Artist'
 import styles from './TheSubNavButton.module.css'
 
 const TheSubNavButton = ({ toggleSubNav, navOpen, className }) => {
-  const { artist, artistId } = React.useContext(ArtistContext)
+  const { artist, artistId, artistLoading } = React.useContext(ArtistContext)
   const [fbPageId, setFbPageId] = React.useState('')
 
   React.useEffect(() => {
@@ -18,12 +19,22 @@ const TheSubNavButton = ({ toggleSubNav, navOpen, className }) => {
     setFbPageId(facebook_page_id)
   }, [artistId])
 
+  const runToggle = () => {
+    if (artistLoading) return
+    toggleSubNav()
+  }
+
   return (
-    <button className={[styles.container, className, navOpen ? styles._navOpen : ''].join(' ')} onClick={toggleSubNav}>
+    <button className={[styles.container, className, navOpen ? styles._navOpen : ''].join(' ')} onClick={runToggle}>
       <div className={styles.inner}>
         <div className={styles.frontIcon}>
           <figure className={styles.image}>
-            {fbPageId && <ArtistImage pageId={fbPageId} name={artist.name} />}
+            {artistLoading ? (
+              <Spinner className={styles.spinner} />
+            )
+              : (
+                <ArtistImage pageId={fbPageId} name={artist.name} />
+              )}
           </figure>
         </div>
         <div className={styles.backIcon}>
