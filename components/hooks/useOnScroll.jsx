b8d5de2a1661fd7e3ad7fff8d99f09eed3afc228
Benjamin E <ben@elwyn.co>
2020-05-28 12:34:21 +0100

    create on scroll hook

---
+++ components/hooks/useOnScroll.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import debounce from 'lodash/debounce'
+
+const useOnScroll = ({
+  callback = () => {},
+  throttle = 100,
+  runOnMount = true,
+  getDelta = false,
+  getDirection = false,
+}) => {
+  const isClient = typeof window === 'object'
+
+  const props = React.useRef({
+    lastScrollDelta: null,
+    lastScrollDirection: null,
+    newScroll: 0,
+    delta: 0,
+    timer: null,
+  })
+
+  // Prevent mobile negative scrolling
+  const sanitiseScroll = (scrollTop) => {
+    return Math.max(0, scrollTop)
+  }
+
+  const clear = () => {
+    props.current.lastScroll = null
+    props.current.delta = 0
+  }
+
+  const calcDelta = (scrollTop) => {
+    props.current.newScroll = scrollTop
+    if (props.current.lastScrollDelta !== null) {
+      props.current.delta = props.current.newScroll - props.current.lastScrollDelta
+    }
+    props.current.lastScrollDelta = sanitiseScroll(props.current.newScroll)
+    clearTimeout(props.current.timer)
+    props.current.timer = setTimeout(clear, throttle / 2)
+    return props.current.delta
+  }
+
+  const calcDirection = (scrollTop) => {
+    if (scrollTop > props.current.lastScrollDirection) {
+      props.current.lastScrollDirection = sanitiseScroll(scrollTop)
+      return 'down'
+    }
+    props.current.lastScrollDirection = sanitiseScroll(scrollTop)
+    return 'up'
+  }
+
+  const getScrollProps = () => {
+    const scrollTop = isClient ? sanitiseScroll(window.scrollY) : undefined
+    return {
+      scrollTop,
+      scrollLeft: isClient ? window.scrollX : undefined,
+      delta: getDelta && isClient ? calcDelta(scrollTop) : undefined,
+      direction: getDirection && isClient ? calcDirection(scrollTop) : undefined,
+    }
+  }
+
+  const [scrollProps, setScrollProps] = React.useState(getScrollProps())
+  React.useEffect(() => {
+    // Run initial callback
+    if (runOnMount) callback()
+    // Set scroll props
+    setScrollProps(getScrollProps())
+    // Setup listener
+    const debouncedCallback = debounce(() => {
+      callback()
+      setScrollProps(getScrollProps())
+    }, throttle)
+    window.addEventListener('scroll', debouncedCallback, { passive: true })
+    // Remove listener
+    return () => {
+      window.removeEventListener('scroll', debouncedCallback, { passive: true })
+    }
+  }, [])
+
+  return scrollProps
+}
+
+export default useOnScroll
