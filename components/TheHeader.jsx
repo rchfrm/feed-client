cc90e45196838357e72c08b13e5cba2a74c43b69
Benjamin E <ben@elwyn.co>
2020-06-01 11:11:41 +0100

    don't mount header until a device type has been set

--- components/TheHeader.jsx
+++ components/TheHeader.jsx
@@ -9,11 +9,14 @@ import TheHeaderMobile from './TheHeaderMobile'
 function TheHeader() {
   // Toggle mobile header
   const { width: windowWidth } = useOnResize({})
-  const [mobileHeader, setMobileHeader] = React.useState(true)
+  const [mobileHeader, setMobileHeader] = React.useState(null)
   React.useEffect(() => {
     const isDesktopLayout = window.matchMedia('(min-width: 993px)').matches
     setMobileHeader(!isDesktopLayout)
   }, [windowWidth])
+
+  // Wait until a device type has been defined
+  if (typeof mobileHeader !== 'boolean') return null
 
   if (mobileHeader) return <TheHeaderMobile windowWidth={windowWidth} />
   return <TheHeaderContents windowWidth={windowWidth} />
