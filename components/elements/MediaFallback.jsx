cbbbdab075cb4c16c4061bd8e74269076bc99d87
Benjamin E <ben@elwyn.co>
2020-06-02 17:45:19 +0100

    make media fallback classname dynamic

--- components/elements/MediaFallback.jsx
+++ components/elements/MediaFallback.jsx
@@ -1,16 +1,8 @@
 // IMPORT PACKAGES
 import React from 'react'
-// IMPORT COMPONENTS
-// IMPORT CONTEXTS
-// IMPORT ELEMENTS
-// IMPORT PAGES
-// IMPORT ASSETS
-// IMPORT CONSTANTS
-// IMPORT HELPERS
-// IMPORT STYLES
 
-function MediaFallback() {
-  return <img src="/images/fallback.png" alt="Media not found" className="center-image" />
+function MediaFallback({ className }) {
+  return <img src="/images/fallback.png" alt="Media not found" className={className} />
 }
 
 export default MediaFallback
