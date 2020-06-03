8f7b229c5dbc1b032f829d62fa6f2aa19cae457e
Benjamin E <ben@elwyn.co>
2020-06-03 17:09:33 +0100

    refactor test page ready

--- components/hoc/testPageReady.jsx
+++ components/hoc/testPageReady.jsx
@@ -6,30 +6,26 @@ import { AuthContext } from '../contexts/Auth'
 
 import * as utils from '../helpers/utils'
 
-import Spinner from '../elements/Spinner'
-
 const kickToLogin = () => Router.push(ROUTES.LOGIN)
 
-const testPageReady = (Component) => {
-  return (props) => {
-    const { pathname: currentPath } = useRouter()
-    const { auth: { token: initialToken }, authLoading } = React.useContext(AuthContext)
+const testPageReady = (Component) => (props) => {
+  const { pathname: currentPath } = useRouter()
+  const { auth: { token: initialToken }, authLoading } = React.useContext(AuthContext)
 
-    React.useEffect(() => {
-      if (authLoading) return
-      if (!initialToken) {
-        if (currentPath !== ROUTES.LOGIN) kickToLogin()
-        utils.clearLocalStorage()
-      }
-    }, [authLoading])
+  React.useEffect(() => {
+    if (authLoading) return
+    if (!initialToken) {
+      if (currentPath !== ROUTES.LOGIN) kickToLogin()
+      utils.clearLocalStorage()
+    }
+  }, [currentPath, initialToken, authLoading])
 
-    // Show spinner if auth loading
-    if (authLoading) return <Spinner width={50} />
-    // Show the content of the page
-    if (initialToken) return <Component {...props} />
-    // Stop flash of content if landing on page while not logged in
-    return null
-  }
+  // Show spinner if auth loading
+  if (authLoading) return null
+  // Show the content of the page
+  if (initialToken) return <Component {...props} />
+  // Stop flash of content if landing on page while not logged in
+  return null
 }
 
 export default testPageReady
