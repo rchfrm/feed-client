16cbbedd80555f34bf9982a1f3e704c23822c2c9
Benjamin E <ben@elwyn.co>
2020-06-08 11:33:32 +0100

    show different copy on no posts page based on user age

--- components/PostsNone.jsx
+++ components/PostsNone.jsx
@@ -1,25 +1,31 @@
 // IMPORT PACKAGES
 import React from 'react'
-// IMPORT COMPONENTS
+import PropTypes from 'prop-types'
+import moment from 'moment'
 // IMPORT CONTEXTS
 import { ArtistContext } from '@/contexts/Artist'
 import { InterfaceContext } from '@/contexts/InterfaceContext'
+import { UserContext } from '@/contexts/User'
 // IMPORT ELEMENTS
 import MarkdownText from '@/elements/MarkdownText'
 import copy from '@/copy/PostsPageCopy'
 // IMPORT STYLES
 import styles from '@/PostsPage.module.css'
 
-const PostsNone = () => {
+const testNewUser = (user) => {
+  const { created_at } = user
+  const createdAtMoment = moment(created_at)
+  const now = moment()
+  const hourDiff = now.diff(createdAtMoment, 'hours')
+  if (hourDiff <= 1) return true
+  return false
+}
   // IMPORT CONTEXTS
-  const { artist } = React.useContext(ArtistContext)
   const { setHeader } = React.useContext(InterfaceContext)
+  const { user } = React.useContext(UserContext)
 
-  const platforms = (
-    artist.facebook_page_url && artist.instagram_url
-      ? 'Facebook or Instagram'
-      : 'Facebook'
-  )
+  const isNewUser = testNewUser(user)
+  const copyMarkdown = isNewUser ? copy.newUserCopy : copy.noPostsCopy
 
   // Update header
   React.useEffect(() => {
