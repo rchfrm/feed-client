c8ac5c251a0bdb0b01f5c304780e97e2ea2e943d
Benjamin E <ben@elwyn.co>
2020-06-10 12:20:58 +0100

    Merge branch 'dev'

--- copy/PostsPageCopy.js
+++ copy/PostsPageCopy.js
@@ -3,9 +3,9 @@
 export default {
   intro: `Below are the posts **Feed** hasn't yet made into ads. It works better the more that are selected, but untick any you'd rather not promote. **Feed** excludes any posts that are more than a month old.`,
 
-  noPostsCopy: (platforms) => `It looks like you haven't posted for a little while, and by default **Feed** only promotes posts from the last month.
-  
-  Once you add new posts to ${platforms}, you'll be able to review them here.`,
+  noPostsCopy: `Looks like you haven’t posted on Facebook or Instagram yet. When you do start posting, your posts will appear here.`,
+
+  newUserCopy: `**Feed** is fetching your posts - please wait a few moments and then click the button to refresh the page.`,
 
   globalToggleIntro: `Make all posts eligible for promotion by default:`,
 
