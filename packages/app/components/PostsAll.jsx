86cac61ba9af54bfe1e1d1b3a0f1be4d0a7c12ce
Benjamin E <ben@elwyn.co>
2020-06-11 17:51:40 +0100

    start new post item with new toggle

--- packages/app/components/PostsAll.jsx
+++ packages/app/components/PostsAll.jsx
@@ -12,7 +12,7 @@ import Button from '@/elements/Button'
 import GearIcon from '@/icons/GearIcon'
 // IMPORT COMPONENTS
 import PostsSettings from '@/app/PostsSettings'
-import PostsSingle from '@/app/PostsSingle'
+import PostItem from '@/app/PostItem'
 import PostsNone from '@/app/PostsNone'
 // IMPORT ASSETS
 import MarkdownText from '@/elements/MarkdownText'
@@ -131,7 +131,7 @@ function PostsAll({
       >
         {postsWithLoadingTrigger.map((post, index) => {
           return (
-            <PostsSingle
+            <PostItem
               key={post.id}
               index={index}
               post={post}
@@ -146,7 +146,7 @@ function PostsAll({
                   className={[styles.postLoadTrigger].join(' ')}
                 />
               )}
-            </PostsSingle>
+            </PostItem>
           )
         })}
         {/* Loading spinner */}
