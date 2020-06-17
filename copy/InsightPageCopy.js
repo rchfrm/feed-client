d32817114239b392f8b9bc0371487dc2c570f48b
Benjamin E <ben@elwyn.co>
2020-06-17 10:26:22 +0100

    Update copy/InsightPageCopy.js
    Co-authored-by: jay-see-jay <6587212+jay-see-jay@users.noreply.github.com>

--- copy/InsightPageCopy.js
+++ copy/InsightPageCopy.js
@@ -4,7 +4,7 @@ import * as ROUTES from '@/constants/routes'
 export default {
   outro: `Can’t see one of your accounts? Just add it via ‘Post settings’ on the [posts page](${ROUTES.POSTS}).
 
-We’ll show you historical data once you’ve been using Feed for a week or so.`,
+We’ll show you historical data once you’ve been using **Feed** for a week or so.`,
 
   spendSummary: (days, spend) => `In the last ${days} days, you've **spent ${spend}**`,
 
