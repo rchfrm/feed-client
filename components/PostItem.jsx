856b2af1fda528140da4372a5b48e4e79cad3807
Benjamin E <ben@elwyn.co>
2020-06-18 16:53:09 +0100

    tidy post item

--- components/PostItem.jsx
+++ components/PostItem.jsx
@@ -1,24 +1,18 @@
 // IMPORT PACKAGES
 import React from 'react'
+// IMPORT ELEMENTS
+import Error from '@/elements/Error'
 // IMPORT COMPONENTS
 import PostToggle from '@/PostToggleNew'
 import PostMetaData from '@/PostMetaData'
 import PostContents from '@/PostContents'
 import PostMetrics from '@/PostMetrics'
 import PostLink from '@/PostLink'
-
-// IMPORT CONTEXTS
-import { ArtistContext } from '@/contexts/Artist'
-// IMPORT ELEMENTS
-import Error from '@/elements/Error'
 // IMPORT ASSETS
-// IMPORT HELPERS
-import * as utils from '@/helpers/utils'
 // IMPORT STYLES
 import styles from '@/PostItem.module.css'
 
-
-function PostItem({
+const PostItem = ({
   index,
   post,
   enabled,
@@ -26,36 +20,19 @@ function PostItem({
   togglePromotion,
   className = '',
   children = <></>,
-}) {
-  // IMPORT CONTEXTS
-  const { artist } = React.useContext(ArtistContext)
-  // END IMPORT CONTEXTS
-  // DEFINE STATES
-  // Track the link that will be ads using the asset, if there isn't a
-  // priority set at the asset level, use the artist priority instead
-  const initialCurrentLink = post.priority_dsp || artist.priority_dsp
-  const [currentLink, setCurrentLink] = React.useState(initialCurrentLink)
-  // Should the add url 'alert' be shown
-  const [addUrl, setAddUrl] = React.useState(false)
-  // Define state to store the chosen link
-  const [chosenLink, setChosenLink] = React.useState(currentLink)
+}) => {
   // Errors
   const [error, setError] = React.useState(null)
-
   // PROMOTABLE STATE
   const { is_promotable: postPromotable } = post
-
   // POST CAPTION
   const postCaption = React.useMemo(() => {
     return post.short_message.join('\n')
   }, [post])
-
   // SELECTED CLASS
   const enabledClass = React.useMemo(() => {
     return enabled ? styles._enabled : styles._disabled
   }, [enabled])
-
-  // console.log('post', post)
 
   return (
     <li
