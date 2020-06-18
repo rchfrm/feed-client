89d4c456775cb4e224ec69c305dc97bc58bad4b5
Benjamin E <ben@elwyn.co>
2020-06-18 11:43:27 +0100

    store device info in browser store

--- components/BrowserStoreSetup.jsx
+++ components/BrowserStoreSetup.jsx
@@ -1,22 +1,40 @@
 import React from 'react'
+import {
+  isMobile,
+  isTablet,
+  isIOS,
+  browserName,
+  isWindows,
+  isMacOs,
+} from 'react-device-detect'
 
 import browserStore from '@/store/browserStore'
 import useOnResize from '@/hooks/useOnResize'
 
 const BrowserStoreSetup = () => {
-  const [useBrowserStore, browserStoreApi] = browserStore
+  const [useBrowserStore] = browserStore
+  const setDevice = useBrowserStore(state => state.setDevice)
   const setDimensions = useBrowserStore(state => state.setDimensions)
   const setBreakpoint = useBrowserStore(state => state.setBreakpoint)
   const { width, height } = useOnResize()
+  // Set device info on mount
+  React.useEffect(() => {
+    setDevice({
+      isMobile,
+      isTablet,
+      isIOS,
+      browserName,
+      isWindows,
+      isMacOs,
+      isDevice: isMobile || isTablet,
+    })
+  }, [setDevice])
+  // Update dimensions on resize
   React.useEffect(() => {
     setDimensions(width, height)
     setBreakpoint(width)
   }, [width, height, setDimensions, setBreakpoint])
-  // Fetch initial state
-  // const browser = React.useRef(browserStoreApi.getState().browser)
-  React.useEffect(() => {
-    browserStoreApi.subscribe(({ browser }) => console.log('browser', browser))
-  }, [browserStoreApi])
+  // No render
   return null
 }
 
