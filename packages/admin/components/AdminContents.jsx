24757c08443a29f9bce612af6ba730440fc5a2a2
Benjamin E <ben@elwyn.co>
2020-06-25 18:07:50 +0100

    start admin base page

--- packages/admin/components/AdminContents.jsx
+++ packages/admin/components/AdminContents.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { InterfaceContextProvider } from '@/contexts/InterfaceContext'
+import TheLoadingOverlay from '@/TheLoadingOverlay'
 
 const AdminContents = ({ children }) => {
   return (
     <div id="container" className="page--content">
-      <main id="page--container">
-        {children}
-      </main>
+      <InterfaceContextProvider>
+        <TheLoadingOverlay />
+        <main id="page--container">
+          {children}
+        </main>
+      </InterfaceContextProvider>
     </div>
   )
 }
---
+++ packages/admin/components/BasePage.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { InterfaceContext } from '@/contexts/InterfaceContext'
+
+const BasePage = ({
+  header, // heading and punctuation
+  staticPage,
+  children,
+}) => {
+  // Get interface context
+  const { toggleGlobalLoading } = React.useContext(InterfaceContext)
+  // ON MOUNT
+  const [mounted, setMounted] = React.useState(false)
+  React.useEffect(() => {
+    if (mounted) return
+    setMounted(true)
+    // If page is static, stop global loading when mounts
+    if (staticPage) {
+      toggleGlobalLoading(false)
+    }
+  // eslint-disable-next-line
+  }, [])
+  return (
+    <>
+      <h1>{header}</h1>
+      {children}
+    </>
+  )
+}
+
+BasePage.propTypes = {
+  header: PropTypes.string,
+  staticPage: PropTypes.bool,
+  children: PropTypes.node.isRequired,
+}
+
+BasePage.defaultProps = {
+  header: '',
+  staticPage: false,
+}
+
+
+export default BasePage
