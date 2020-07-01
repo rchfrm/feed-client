2aba60e3b937391d22ea291d3065bb3a9ec595ca
Benjamin E <ben@elwyn.co>
2020-07-01 14:52:34 +0100

    adapt page query setter to display multiple params

--- packages/admin/components/PageQuerySetter.jsx
+++ packages/admin/components/PageQuerySetter.jsx
@@ -1,58 +1,70 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
 import Router from 'next/router'
+
+import produce from 'immer'
 
 import Input from '@/elements/Input'
 import Button from '@/elements/Button'
 
 const PageQuerySetter = ({
   intro,
-  label,
-  placeholder,
+  queries,
   pathname,
-  queryName,
+  submitText,
+  filledQueries,
 }) => {
-  const [query, setQuery] = React.useState('')
-  const handleChange = React.useCallback((e) => {
-    setQuery(e.target.value)
-  }, [setQuery])
+  const [typedQueries, setTypedQueries] = React.useState(filledQueries)
+  const handleChange = React.useCallback((index, value) => {
+    const newQueries = produce(typedQueries, draftQueries => {
+      draftQueries[index] = value
+    })
+    setTypedQueries(newQueries)
+  }, [typedQueries, setTypedQueries])
 
   const onSubmit = React.useCallback((e) => {
     e.preventDefault()
-    if (!query) return
-    const newUrl = `${pathname}?${queryName}=${query}`
+    if (!typedQueries.length) return
+    const newUrl = typedQueries.reduce((url, query, index) => {
+      const { queryName } = queries[index]
+      const prefix = index === 0 ? '?' : '&'
+      return `${url}${prefix}${queryName}=${query}`
+    }, `${pathname}`)
     Router.push(newUrl)
-  }, [pathname, queryName, query])
+  }, [typedQueries, queries, pathname])
 
   return (
-    <form className="flex flex-wrap items-end w-full" onSubmit={onSubmit}>
+    <form className="sm:flex flex-wrap items-end w-full" onSubmit={onSubmit}>
       {intro && <p className="w-full">{intro}</p>}
-      <Input
-        className="flex-1 mb-0 mr-5"
-        handleChange={handleChange}
-        name="Page query"
-        label={label}
-        placeholder={placeholder}
-        value={query}
-      />
-      <Button type="submit" className="mb-0" disabled={!query}>Fetch tournaments</Button>
+      {queries.map(({ label, queryName }, index) => {
+        return (
+          <Input
+            key={queryName}
+            className="flex-1 sm:mb-0 mr-5"
+            handleChange={(e) => handleChange(index, e.target.value)}
+            name="Page query"
+            label={label}
+            value={typedQueries[index]}
+          />
+        )
+      })}
+      <Button type="submit" className="mb-0" disabled={!typedQueries.length === queries.length}>{submitText}</Button>
     </form>
   )
 }
 
 PageQuerySetter.propTypes = {
   intro: PropTypes.string,
-  label: PropTypes.string,
-  placeholder: PropTypes.string,
+  queries: PropTypes.array.isRequired,
   pathname: PropTypes.string.isRequired,
-  queryName: PropTypes.string.isRequired,
+  submitText: PropTypes.string,
+  filledQueries: PropTypes.array,
 }
 
 PageQuerySetter.defaultProps = {
   intro: '',
-  label: 'Query ID',
-  placeholder: 'id string',
+  submitText: 'Go',
+  filledQueries: [],
 }
 
 
