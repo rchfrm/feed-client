52456635004af386bde73be1a434394c9ab1e122
Benjamin E <ben@elwyn.co>
2020-07-02 17:31:05 +0100

    improve tournament ad data

--- packages/admin/components/TournamentAd.jsx
+++ packages/admin/components/TournamentAd.jsx
@@ -10,33 +10,35 @@ import DataDetails from '@/admin/elements/DataDetails'
 import * as api from '@/helpers/api'
 import { UserContext } from '@/contexts/UserContext'
 
-const propsToDisplay = [
-  'name',
-  'engagement_score',
-  'spend_adjusted_engagement_score',
-]
-
 const fetcher = (path, token) => {
   return api.get(`/${path}`, token)
 }
 
 const TournamentAd = ({ ad, winner, className }) => {
+  const propsToDisplay = [
+    'name',
+    'platform',
+    'engagement_score',
+    'spend_adjusted_engagement_score',
+  ]
+
   const { user } = React.useContext(UserContext)
-  const { data, error } = useSWR(
+  const { data: adData, error } = useSWR(
     user ? [ad.ad.path, user.token] : null,
     fetcher,
   )
 
+  if (!adData) return null
   if (error) return <Error error={error} />
 
-  if (!data) return null
+  console.log('adData', adData)
 
   return (
     <div className={className}>
       <h4 className="h3"><strong>Ad: {ad.id}</strong></h4>
       {winner && <h5 className="text-green">☆ WINNER ☆</h5>}
-      <DataDetails propsToDisplay={propsToDisplay} data={data} />
-      {Object.values(data.adcreatives).map((adCreative) => {
+      <DataDetails propsToDisplay={propsToDisplay} data={adData} />
+      {Object.values(adData.adcreatives).map((adCreative) => {
         return <TournamentAdCreative key={adCreative.id} adCreative={adCreative} />
       })}
       <hr className="md:hidden" />
--- packages/admin/components/TournamentAdCreative.jsx
+++ packages/admin/components/TournamentAdCreative.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import DataDetails from '@/admin/elements/DataDetails'
+import DataDetail from '@/admin/elements/DataDetail'
 
 const propsToDisplay = [
   'id',
@@ -10,10 +11,49 @@ const propsToDisplay = [
   'effective_instagram_story_id',
 ]
 
+const getPostContent = (adCreative) => {
+  const { object_type, object_story_spec, instagram_actor_id } = adCreative
+  // Insta video
+  if (object_type === 'VIDEO' && instagram_actor_id) {
+    const { video_data: { message, image_url: imageSrc } } = object_story_spec
+    return { message, imageSrc }
+  }
+  // Insta share
+  if (object_type === 'SHARE' && instagram_actor_id) {
+    const { link_data: { message, picture: imageSrc } } = object_story_spec
+    return { message, imageSrc }
+  }
+  return {}
+}
+
 const TournamentAdCreative = ({ adCreative }) => {
+  console.log('adCreative', adCreative)
+  const {
+    instagram_permalink_url,
+    object_type,
+  } = adCreative
+  // Get permalink
+  const permalink = instagram_permalink_url
+  // Get post content
+  const { message, imageSrc } = getPostContent(adCreative)
   return (
     <div className="pt-5">
-      <DataDetails propsToDisplay={propsToDisplay} data={adCreative} header="Ad Creative" />
+      <h4><strong>Ad Creative</strong></h4>
+      {/* Ad media */}
+      <div className="flex mb-5">
+        {imageSrc && (
+          <figure className="mr-5 w-40">
+            <img className="rounded-dialogue" src={imageSrc} alt={object_type} />
+          </figure>
+        )}
+        {message && (
+          <figcaption className="small--p bg-grey-1 p-2 rounded-dialogue self-start">{message}</figcaption>
+        )}
+      </div>
+      {/* Most details */}
+      <DataDetails propsToDisplay={propsToDisplay} data={adCreative} />
+      {/* Link */}
+      {permalink && <DataDetail name="permalink" value={permalink} isLink />}
     </div>
   )
 }
