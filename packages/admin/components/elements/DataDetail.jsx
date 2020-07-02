f7da7888294d0cf6f46a8051acbd348da29cb60d
Benjamin E <ben@elwyn.co>
2020-07-02 17:30:29 +0100

    allow links in DataDetail

--- packages/admin/components/elements/DataDetail.jsx
+++ packages/admin/components/elements/DataDetail.jsx
@@ -3,7 +3,10 @@ import PropTypes from 'prop-types'
 
 import CopyTextButton from '@/elements/CopyTextButton'
 
-const DataDetail = ({ name, value, border, copy }) => {
+const DataDetail = ({ name, value, border, copy, isLink }) => {
+  const innerText = !isLink ? value : (
+    <a href={value} target="_blank" rel="noreferrer noopener">{value}</a>
+  )
   return (
     <>
       {border && <hr />}
@@ -13,7 +16,7 @@ const DataDetail = ({ name, value, border, copy }) => {
         {copy ? (
           <CopyTextButton text={value} className="ml-2" />
         ) : (
-          <strong>{value}</strong>
+          <strong>{innerText}</strong>
         )}
       </p>
     </>
@@ -28,11 +31,13 @@ DataDetail.propTypes = {
   value: PropTypes.string.isRequired,
   border: PropTypes.bool,
   copy: PropTypes.bool,
+  isLink: PropTypes.bool,
 }
 
 DataDetail.defaultProps = {
   border: false,
   copy: false,
+  isLink: false,
 }
 
 export default DataDetail
