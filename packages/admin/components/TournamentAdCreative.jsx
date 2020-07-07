c27a4bb5c4d60dbbf6761bff7295af57e9918873
Benjamin E <ben@elwyn.co>
2020-07-07 12:38:09 +0100

    use post image on tournament ad

--- packages/admin/components/TournamentAdCreative.jsx
+++ packages/admin/components/TournamentAdCreative.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import PostImage from '@/PostImage'
 import DataDetails from '@/admin/elements/DataDetails'
 import DataDetail from '@/admin/elements/DataDetail'
 
@@ -12,16 +13,19 @@ const propsToDisplay = [
 ]
 
 const getPostContent = (adCreative) => {
-  const { object_type, object_story_spec, instagram_actor_id } = adCreative
+  console.log('adCreative', adCreative)
+  const { object_type, object_story_spec, instagram_actor_id, image_url, thumbnail_url } = adCreative
   // Insta video
   if (object_type === 'VIDEO' && instagram_actor_id) {
     const { video_data: { message, image_url: imageSrc } } = object_story_spec
-    return { message, imageSrc }
+    const thumbnailOptions = [imageSrc, image_url, thumbnail_url]
+    return { message, thumbnailOptions }
   }
   // Insta share
   if (object_type === 'SHARE' && instagram_actor_id) {
     const { link_data: { message, picture: imageSrc, link: adLink } } = object_story_spec
-    return { message, imageSrc, adLink }
+    const thumbnailOptions = [imageSrc, image_url, thumbnail_url]
+    return { message, adLink, thumbnailOptions }
   }
   return {}
 }
@@ -30,24 +34,24 @@ const TournamentAdCreative = ({ adCreative }) => {
   // console.log('adCreative', adCreative)
   const {
     instagram_permalink_url,
-    object_type,
   } = adCreative
   // Get permalink
   const permalink = instagram_permalink_url
   // Get post content
-  const { message, imageSrc, adLink } = getPostContent(adCreative)
+  const { message, adLink, thumbnailOptions } = getPostContent(adCreative)
   return (
     <div className="pt-5">
       <h4><strong>Ad Creative</strong></h4>
       {/* Ad media */}
       <div className="flex mb-5">
-        {imageSrc && (
-          <figure className="mr-5 w-40">
-            <img className="rounded-dialogue" src={imageSrc} alt={object_type} />
-          </figure>
-        )}
+        <div className="mr-5 w-24">
+          <PostImage
+            thumbnailOptions={thumbnailOptions}
+            title={message}
+          />
+        </div>
         {message && (
-          <figcaption className="small--p bg-grey-1 p-2 rounded-dialogue self-start">{message}</figcaption>
+          <figcaption className="small--p bg-grey-1 p-2 rounded-dialogue self-start flex-1">{message}</figcaption>
         )}
       </div>
       {/* Ad Link */}
