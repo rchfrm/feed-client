7bd2b3d222d6f905cf19bda0c0bddd7f6b6a1144
Benjamin E <ben@elwyn.co>
2020-07-07 12:25:33 +0100

    move post image to shared

--- packages/app/components/PostContents.jsx
+++ packages/app/components/PostContents.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import PostImage from '@/app/PostImage'
+import PostImage from '@/PostImage'
 
 import styles from '@/app/PostItem.module.css'
 
