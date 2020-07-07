ecc5989827613c92b4801ca6e8e406869c168b7a
Benjamin E <ben@elwyn.co>
2020-07-07 12:03:06 +0100

    create reusable section heder

--- packages/admin/components/TournamentAds.jsx
+++ packages/admin/components/TournamentAds.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import SectionHeader from '@/admin/elements/SectionHeader'
 import TournamentAd from '@/admin/TournamentAd'
 
 const TournamentAds = ({ ads, winningAdId }) => {
   return (
     <section className="pt-5 col-span-12">
-      <h4 className="h3 mb-2"><strong>The ads</strong></h4>
-      <div style={{ height: 2 }} className="w-full bg-green mb-8" />
+      <SectionHeader header="The ads" />
       <div className="grid grid-cols-12 col-span-12 gap-8">
         {ads.map((ad) => {
           const winner = ad.id === winningAdId
