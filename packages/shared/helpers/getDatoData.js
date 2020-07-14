733dd0d685c0644c246f00c9468233a6a7541121
Benjamin E <ben@elwyn.co>
2020-07-14 15:14:58 +0100

    save cached dato query when fetched

--- packages/shared/helpers/getDatoData.js
+++ packages/shared/helpers/getDatoData.js
@@ -21,11 +21,12 @@ const axiosClient = axios.create({
 })
 
 const getDatoData = async (query, cachedFilename, forceFetch) => {
+  const currentPath = process.cwd()
+  const cachedFile = `${cachedDataDir}${cachedFilename}.json`
   // Return cached data (if running locally and cached version exists)
   if (!fetchLiveData && !forceFetch) {
     // Check if cached data exists
-    const currentPath = process.cwd()
-    const cachedDataPath = path.resolve(currentPath, `${cachedDataDir}${cachedFilename}.json`)
+    const cachedDataPath = path.resolve(currentPath, cachedFile)
     const cachedDataExists = fs.existsSync(cachedDataPath)
     if (cachedDataExists) {
       const cachedData = JSON.parse(fs.readFileSync(cachedDataPath, 'utf8'))
@@ -40,6 +41,7 @@ const getDatoData = async (query, cachedFilename, forceFetch) => {
     data: { query },
   })
 
+  // Handle errors...
   if (status !== 200) {
     const errorMessage = statusText
     throw new Error(errorMessage)
@@ -49,6 +51,10 @@ const getDatoData = async (query, cachedFilename, forceFetch) => {
     const errorMessage = data.errors.map(error => error.message).join(',')
     throw new Error(errorMessage)
   }
+
+  // Save data to cached file
+  const dataString = JSON.stringify(data)
+  fs.writeFileSync(cachedFile, dataString)
 
   return data
 }
