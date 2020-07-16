e86135196503d42c8da0d8458713666458ce5e4a
Benjamin E <ben@elwyn.co>
2020-07-16 15:09:44 +0100

    start tournament audience filters

--- packages/app/components/TournamentsContent.jsx
+++ packages/app/components/TournamentsContent.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
 
+import TournamentsAudienceFilters from '@/app/TournamentsAudienceFilters'
+
+import brandColors from '@/constants/brandColors'
+
+const audienceTypes = [
+  {
+    id: 'warm',
+    title: 'Warm',
+    color: brandColors.red,
+    activeTextColor: brandColors.white,
+  },
+  {
+    id: 'cool',
+    title: 'Cool',
+    color: brandColors.soundcloud.bg,
+    activeTextColor: brandColors.soundcloud.text,
+  },
+  {
+    id: 'cold',
+    title: 'Cold',
+    color: brandColors.twitter.bg,
+    activeTextColor: brandColors.twitter.text,
+  },
+]
+
 const TournamentsContent = () => {
+  const [currentAudienceType, setCurrentAudienceType] = React.useState(audienceTypes[0].id)
   return (
     <div>
-      Tourn
+      <TournamentsAudienceFilters
+        audienceTypes={audienceTypes}
+        currentAudienceType={currentAudienceType}
+        setCurrentAudienceType={setCurrentAudienceType}
+      />
     </div>
   )
 }
