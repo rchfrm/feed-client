08de1ebc0da58058d93f6df768b884d667f0bd8f
Benjamin E <ben@elwyn.co>
2020-07-16 16:14:59 +0100

    start tournament filters

--- packages/app/components/TournamentsContent.jsx
+++ packages/app/components/TournamentsContent.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 // import PropTypes from 'prop-types'
 
 import TournamentsAudienceFilters from '@/app/TournamentsAudienceFilters'
+import TournamentsTypeFilters from '@/app/TournamentsTypeFilters'
 
 import brandColors from '@/constants/brandColors'
 
@@ -26,14 +27,34 @@ const audienceTypes = [
   },
 ]
 
+const tournamentTypes = [
+  { id: 'posts', title: 'Posts' },
+  { id: 'stories', title: 'Stories' },
+]
+
 const TournamentsContent = () => {
   const [currentAudienceType, setCurrentAudienceType] = React.useState(audienceTypes[0].id)
+  const [currentTournamentType, setCurrentTournamentType] = React.useState(tournamentTypes[0].id)
+  // Set current tournament type to posts if selecting cold audience
+  React.useEffect(() => {
+    if (currentAudienceType === 'cold') {
+      setCurrentTournamentType(tournamentTypes[0].id)
+    }
+  }, [currentAudienceType])
   return (
     <div>
+      {/* AUDIENCE FILTERS */}
       <TournamentsAudienceFilters
         audienceTypes={audienceTypes}
         currentAudienceType={currentAudienceType}
         setCurrentAudienceType={setCurrentAudienceType}
+      />
+      {/* TOURNAMENT TYPE FILTERS */}
+      <TournamentsTypeFilters
+        tournamentTypes={tournamentTypes}
+        currentTournamentType={currentTournamentType}
+        setCurrentTournamentType={setCurrentTournamentType}
+        currentAudienceType={currentAudienceType}
       />
     </div>
   )
---
+++ packages/app/components/TournamentsTypeFilters.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import produce from 'immer'
+
+import PillOptions from '@/elements/PillOptions'
+
+const TournamentsTypeFilters = ({
+  tournamentTypes,
+  currentTournamentType,
+  setCurrentTournamentType,
+  currentAudienceType,
+}) => {
+  const pillOptions = React.useMemo(() => {
+    // Disable stories for Cold audiences
+    if (currentAudienceType === 'cold') {
+      const storiesIndex = tournamentTypes.findIndex(({ id }) => id === 'stories')
+      return produce(tournamentTypes, draftState => {
+        draftState[storiesIndex].disabled = true
+      })
+    }
+    return tournamentTypes
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentAudienceType])
+  return (
+    <PillOptions
+      label="Select a tournament type"
+      options={pillOptions}
+      activeOption={currentTournamentType}
+      setActiveOption={setCurrentTournamentType}
+      size="large"
+    />
+  )
+}
+
+TournamentsTypeFilters.propTypes = {
+  tournamentTypes: PropTypes.array.isRequired,
+  currentTournamentType: PropTypes.string.isRequired,
+  setCurrentTournamentType: PropTypes.func.isRequired,
+  currentAudienceType: PropTypes.string.isRequired,
+}
+
+export default TournamentsTypeFilters
