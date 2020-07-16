23b66e22e4b4e70f58d9482c84a241ea7d40e416
Benjamin E <ben@elwyn.co>
2020-07-16 12:19:56 +0100

    start base filters

---
+++ packages/shared/components/BaseFilters.jsx
@@ -0,0 +1,101 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+// Components
+import BaseFiltersButton from '@/BaseFiltersButton'
+// Hooks
+import useScrollToButton from '@/hooks/useScrollToButton'
+// Styles
+import styles from '@/BaseFilters.module.css'
+
+// * ******
+// * README
+// * ******
+/*
+`options` must be an array of objects formatted like this:
+
+  {
+    id: [string: option id] *required,
+    title: [string: option title, used as button text] *required,
+    subtitle: [string: option subtitle] optional (default: ''),
+    color: [string: the color of the active state] optional (default: 'green'),
+    icon: [node: the icon to include inside the button] optional (default: null),
+  }
+
+*/
+
+const BaseFilters = ({
+  options,
+  activeOptionId,
+  setActiveOptionId,
+  labelText,
+  buttonType,
+  className,
+}) => {
+  // SETUP SCROLL TO BUTTON
+  const [buttonRefs, containerRef] = useScrollToButton(options, activeOptionId)
+  // CHANGE ACTIVE COLOR
+  React.useEffect(() => {
+    if (!activeOptionId) return
+    // Find active option color
+    const { color: activeColor } = options.find(({ id }) => id === activeOptionId)
+    // Set hover color
+    if (!containerRef.current) return
+    containerRef.current.style.setProperty('--active-color', activeColor)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [activeOptionId])
+
+  if (!options.length) return null
+
+  return (
+    <div className={['breakout--width'].join(' ')}>
+      <p className={['inputLabel__text', styles.label].join(' ')}>{labelText}</p>
+      <div
+        ref={containerRef}
+        className={[
+          styles.container,
+          buttonType === 'text' ? 'pt-5' : '',
+          className,
+        ].join(' ')}
+      >
+        {options.map(({ id, title, subtitle, color, icon }, i) => {
+          const active = id === activeOptionId
+          const activeClass = active ? styles._active : ''
+          console.log('buttonRefs[i]', buttonRefs[i])
+          return (
+            <BaseFiltersButton
+              key={id}
+              id={id}
+              buttonRef={buttonRefs[i]}
+              title={title}
+              subtitle={subtitle}
+              setActiveOptionId={setActiveOptionId}
+              buttonType={buttonType}
+              backgroundColor={color}
+              icon={icon}
+              className={activeClass}
+              // textColor,
+            />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+BaseFilters.propTypes = {
+  options: PropTypes.array.isRequired,
+  activeOptionId: PropTypes.string,
+  setActiveOptionId: PropTypes.func.isRequired,
+  labelText: PropTypes.string.isRequired,
+  buttonType: PropTypes.string,
+  className: PropTypes.string,
+}
+
+BaseFilters.defaultProps = {
+  activeOptionId: '',
+  buttonType: 'pill',
+  className: '',
+}
+
+
+export default BaseFilters
---
+++ packages/shared/components/BaseFiltersButton.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import styles from '@/BaseFilters.module.css'
+
+import brandColors from '@/constants/brandColors'
+
+const BaseFiltersButton = ({
+  id,
+  buttonRef,
+  title,
+  subtitle,
+  setActiveOptionId,
+  buttonType,
+  backgroundColor,
+  textColor,
+  icon,
+  className,
+}) => {
+  // TEXT BUTTON
+  if (buttonType === 'text') {
+    return (
+      <div
+        ref={buttonRef}
+        className={[styles.buttonContainer, styles.buttonText_container, className].join(' ')}
+      >
+        <a
+          role="button"
+          className={[styles.button, styles.textButton].join(' ')}
+          onClick={() => setActiveOptionId(id)}
+        >
+          <span className={styles.buttonText_title}>{title}</span>
+          {subtitle && (
+            <>
+              <br />
+              <span className={['small--p'].join(' ')}>{subtitle}</span>
+            </>
+          )}
+        </a>
+      </div>
+    )
+  }
+  // PILL BUTTON
+  return (
+    <div
+      ref={buttonRef}
+      className={[styles.buttonContainer, styles.buttonPill_container, className].join(' ')}
+    >
+      <button
+        className={['button--filter', styles.button, styles.buttonPill, className].join(' ')}
+        onClick={() => setActiveOptionId(id)}
+        style={{
+          backgroundColor,
+          color: textColor,
+        }}
+      >
+        {icon}
+        {title}
+      </button>
+    </div>
+  )
+}
+
+BaseFiltersButton.propTypes = {
+  id: PropTypes.string.isRequired,
+  buttonRef: PropTypes.object,
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string,
+  setActiveOptionId: PropTypes.func.isRequired,
+  buttonType: PropTypes.string,
+  backgroundColor: PropTypes.string,
+  textColor: PropTypes.string,
+  icon: PropTypes.node,
+  className: PropTypes.string,
+}
+
+BaseFiltersButton.defaultProps = {
+  buttonRef: null,
+  subtitle: '',
+  buttonType: 'pill',
+  backgroundColor: brandColors.grey,
+  textColor: brandColors.textColor,
+  icon: null,
+  className: '',
+}
+
+
+export default BaseFiltersButton
