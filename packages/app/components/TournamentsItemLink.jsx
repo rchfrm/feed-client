28919562e69d1ba81985201140dc51bbc8da19db
Benjamin E <ben@elwyn.co>
2020-07-23 17:37:52 +0100

    improve ad link styling

--- packages/app/components/TournamentsItemLink.jsx
+++ packages/app/components/TournamentsItemLink.jsx
@@ -3,18 +3,20 @@ import PropTypes from 'prop-types'
 
 import LinkIcon from '@/icons/LinkIcon'
 
+import styles from '@/app/Tournaments.module.css'
+
 const TournamentsItemLink = ({ link, title, className }) => {
   const linkIcon = <LinkIcon className="h-3 mr-2" />
   return (
     <>
       {link ? (
-        <a className={className} href={link} target="_blank" rel="noopener noreferrer">
+        <a className={[className, styles.adLink].join(' ')} href={link} target="_blank" rel="noopener noreferrer">
           {linkIcon}
           {title}
         </a>
       ) : (
         <span className={className}>
-          <span style={{ visibility: 'hidden' }}>{linkIcon}</span>
+          <span className="hidden md:inline" style={{ visibility: 'hidden' }}>{linkIcon}</span>
           {title}
         </span>
       )}
