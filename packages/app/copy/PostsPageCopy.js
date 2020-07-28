e5b552b25caa2d2eeae2b57e80835225396b0c24
Benjamin E <ben@elwyn.co>
2020-07-28 13:04:46 +0200

    update connections copy

--- packages/app/copy/PostsPageCopy.js
+++ packages/app/copy/PostsPageCopy.js
@@ -11,7 +11,11 @@ export default {
 
   globalConnectionsIntro: `Set the links to use on your posts.
   
-The URLs for _Facebook_, _Instagram_, _Twitter_, _YouTube_, _Soundcloud_, and _Spotify_ will be used by Feed to track follower data on the Insights page.`,
+The URLs for _Facebook_, _Instagram_, _Twitter_, _YouTube_, _Soundcloud_, and _Spotify_ will also be used by Feed to track follower data on the Insights page.
+
+The other icons are suggested links, but you can use any field for any link.
+
+To set the default link for all posts, click the asterisk (*) to the left of the link. You can still set a different link on individual posts.`,
 
   globalStatusConfirmation: `Changing this will update the current status on all your current and future posts.
   
@@ -32,12 +36,6 @@ The URLs for _Facebook_, _Instagram_, _Twitter_, _YouTube_, _Soundcloud_, and _S
   ],
 
   connectionsTooltipSlides: [
-    `When someone clicks your ad they will be taken to one of these sites. Enter a link then click the ✔️ to save it.`,
-
-    `To set the default link for all posts, click the asterisk (*) to the left of the link. You can still set a different link on individual posts.`,
-
-    `The icons are suggested links, but you can use any field for any link.`,
-
-    `Adding links to your Spotify, YouTube, Soundcloud or Twitter profiles will also let Feed pull in data from those places on the Insights page.`,
+    ``,
   ],
 }
