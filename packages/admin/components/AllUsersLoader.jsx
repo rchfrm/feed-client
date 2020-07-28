247d12621d47e182726fab2852c7d61f927152b9
Benjamin E <ben@elwyn.co>
2020-07-28 10:44:09 +0200

    make users list look nicer

--- packages/admin/components/AllUsersLoader.jsx
+++ packages/admin/components/AllUsersLoader.jsx
@@ -1,21 +1,35 @@
 import React from 'react'
 import useGetPaginated from '@/admin/hooks/useGetPaginated'
 
-import UserOverview from '@/admin/UserOverview'
+import UsersList from '@/admin/UsersList'
 
 const AllUsersLoader = () => {
-  const { items: users, error, finishedLoading } = useGetPaginated('getAllUsers')
+  const propsToDisplay = [
+    'email',
+    'role',
+    'created_at',
+    'updated_at',
+  ]
+  const extraFields = [
+    'id',
+    'first_name',
+    'last_name',
+    'artists',
+    'organizations',
+  ]
+
+  const fields = [...propsToDisplay, ...extraFields]
+  const { items: users, error, finishedLoading } = useGetPaginated('getAllUsers', {
+    limit: 1000,
+    fields: fields.join(','),
+  })
 
   return (
     <section>
       {error && <div>Failed to fetch users</div>}
-      {!finishedLoading && <p>Loading...</p>}
-      <p>Total: {users.length}</p>
-      {users.map((user) => {
-        return (
-          <UserOverview key={user.id} user={user} />
-        )
-      })}
+      {finishedLoading ? <p>Finished loading</p> : <p>Loading...</p>}
+      <p>Total loaded: {users.length}</p>
+      <UsersList users={users} propsToDisplay={propsToDisplay} />
     </section>
   )
 }
--- packages/admin/components/UserOverview.jsx
+++ packages/admin/components/UserOverview.jsx
@@ -3,65 +3,64 @@ import PropTypes from 'prop-types'
 
 import Link from 'next/link'
 
+import SectionHeader from '@/admin/elements/SectionHeader'
 import DataDetail from '@/admin/elements/DataDetail'
 import DataDetails from '@/admin/elements/DataDetails'
 
 import * as ROUTES from '@/admin/constants/routes'
 
-const propsToDisplay = [
-  'id',
-  'email',
-  'role',
-  'created_at',
-  'updated_at',
-]
-
-const UserOverview = ({ user }) => {
+const UserOverview = ({ user, propsToDisplay }) => {
   const userName = `${user.first_name} ${user.last_name}`
   const userArtists = Object.values(user.artists)
   const userOrgs = Object.values(user.organizations)
   return (
-    <div>
-      <DataDetail name="name" value={userName} border />
+    <>
+      <SectionHeader header={userName} />
+      <DataDetail name="User ID" value={user.id} copyText />
       <DataDetails propsToDisplay={propsToDisplay} data={user} />
-      {/* Artist links */}
-      <div>
-        <h4>Artists</h4>
-        <ul>
-          {userArtists.map(({ id, name }) => {
-            return (
-              <li key={id}>
-                <Link href={{ pathname: ROUTES.ARTIST, query: { artistId: id } }}>
-                  <a>{name} ({id})</a>
-                </Link>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
+      {/* ARTIST LINKS */}
+      {userArtists && !!userArtists.length && (
+        <div>
+          <h4><strong>Artists</strong></h4>
+          <ul className="list-disc pl-5">
+            {userArtists.map(({ id, name }) => {
+              return (
+                <li className="mb-4" key={id}>
+                  <Link href={{ pathname: ROUTES.ARTIST, query: { artistId: id } }}>
+                    <a>{name}</a>
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      )}
       {/* ORG links */}
-      <div>
-        <h4>Organisations</h4>
-        <ul>
-          {userOrgs.map(({ id, name, role }) => {
-            return (
-              <li key={id}>
-                {role}
-                {': '}
-                <Link href={{ pathname: ROUTES.ORGANISATION, query: { orgId: id } }}>
-                  <a>{name} ({id})</a>
-                </Link>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
-    </div>
+      {userOrgs && !!userOrgs.length && (
+        <div>
+          <h4><strong>Organisations</strong></h4>
+          <ul className="list-disc pl-5">
+            {userOrgs.map(({ id, name, role }) => {
+              return (
+                <li className="mb-4" key={id}>
+                  <Link href={{ pathname: ROUTES.ORGANISATION, query: { orgId: id } }}>
+                    <a>{name}</a>
+                  </Link>
+                  {' '}
+                  ({role})
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      )}
+    </>
   )
 }
 
 UserOverview.propTypes = {
   user: PropTypes.object.isRequired,
+  propsToDisplay: PropTypes.array.isRequired,
 }
 
 export default UserOverview
