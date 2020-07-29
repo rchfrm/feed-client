b8a9b665f5dbd8aa1a38ca3cc1a5558270ad180d
Benjamin E <ben@elwyn.co>
2020-07-29 13:20:16 +0200

    allow change direction of list sort

--- packages/admin/components/elements/ListSort.jsx
+++ packages/admin/components/elements/ListSort.jsx
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types'
 
 import produce from 'immer'
 
-const getSortedList = (list, sortBy) => {
+const getSortedList = (list, sortBy, sortDirection) => {
   return produce(list, draftList => {
     draftList.sort((a, b) => {
       const aValue = a[sortBy]
       const bValue = b[sortBy]
-      if (aValue < bValue) return -1
-      if (aValue > bValue) return 1
+      if (aValue < bValue) return sortDirection === 'asc' ? -1 : 1
+      if (aValue > bValue) return sortDirection === 'asc' ? 1 : -1
       return 0
     })
   })
@@ -21,17 +21,22 @@ const ListSort = ({
   sortOptions,
   className,
 }) => {
+  const sortDirections = [
+    'asc',
+    'desc',
+  ]
   const [sortBy, setSortBy] = React.useState(sortOptions[0])
+  const [sortDirection, setSortDirection] = React.useState(sortDirections[0])
   // RUN SORT AND UPDATE PARENT LIST
   React.useEffect(() => {
-    const sortedList = getSortedList(fullList, sortBy)
+    const sortedList = getSortedList(fullList, sortBy, sortDirection)
     updateList(sortedList)
-  }, [sortBy, updateList, fullList])
+  }, [sortBy, updateList, fullList, sortDirection])
 
   return (
     <nav className={[className].join(' ')}>
-      <ul className="flex">
-        {/* Filter buttons */}
+      <ul className="flex items-center">
+        {/* Sort by buttons */}
         {sortOptions.map((sortOption) => {
           const statusClass = sortBy === sortOption ? '-active' : ''
           const title = sortOption.split('_').join(' ')
@@ -40,6 +45,22 @@ const ListSort = ({
               <button
                 className={['button--filter', 'capitalize', statusClass].join(' ')}
                 onClick={() => setSortBy(sortOption)}
+              >
+                {title}
+              </button>
+            </li>
+          )
+        })}
+        <div className="pr-4">|</div>
+        {/* Sort direction buttons */}
+        {sortDirections.map((sortOption) => {
+          const statusClass = sortDirection === sortOption ? '-active' : ''
+          const title = sortOption.split('_').join(' ')
+          return (
+            <li key={sortOption} className={['mr-5', statusClass].join(' ')}>
+              <button
+                className={['button--filter', 'capitalize', statusClass].join(' ')}
+                onClick={() => setSortDirection(sortOption)}
               >
                 {title}
               </button>
