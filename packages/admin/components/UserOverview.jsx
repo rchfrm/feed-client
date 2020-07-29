14529a439629abf5a2ceb2702f0cdf4f2e7806e9
Benjamin E <ben@elwyn.co>
2020-07-29 13:00:54 +0200

    collapse user artists

--- packages/admin/components/UserOverview.jsx
+++ packages/admin/components/UserOverview.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 
 import Link from 'next/link'
 
+import UserArtists from '@/admin/UserArtists'
 import SectionHeader from '@/admin/elements/SectionHeader'
 import DataDetail from '@/admin/elements/DataDetail'
 import DataDetails from '@/admin/elements/DataDetails'
@@ -20,20 +21,9 @@ const UserOverview = ({ user, propsToDisplay }) => {
       <DataDetails propsToDisplay={propsToDisplay} data={user} />
       {/* ARTIST LINKS */}
       {userArtists && !!userArtists.length && (
-        <div>
-          <h4><strong>Artists</strong></h4>
-          <ul className="list-disc pl-5">
-            {userArtists.map(({ id, name }) => {
-              return (
-                <li className="mb-4" key={id}>
-                  <Link href={{ pathname: ROUTES.ARTIST, query: { artistId: id } }}>
-                    <a>{name}</a>
-                  </Link>
-                </li>
-              )
-            })}
-          </ul>
-        </div>
+        <UserArtists
+          artists={userArtists}
+        />
       )}
       {/* ORG links */}
       {userOrgs && !!userOrgs.length && (
