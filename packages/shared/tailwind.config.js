75a7664fb50a572ba46a71ea15b89f2ffb9e04ea
Benjamin E <ben@elwyn.co>
2020-07-31 17:53:04 +0200

    add purple colour

--- packages/shared/tailwind.config.js
+++ packages/shared/tailwind.config.js
@@ -30,6 +30,7 @@ module.exports = (purgeFiles) => ({
         black: '#0D1311',
         green: '#03D8B2',
         red: '#FA5450',
+        purple: '#B593FF',
         grey: {
           1: '#E7E9E8',
           2: '#C8CBCA',
