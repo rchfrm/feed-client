444b8d2df06a7e194137f0c392ab05690093eee1
Benjamin E <ben@elwyn.co>
2020-07-31 13:00:33 +0200

    move artists filters into multi-use artists loader

--- packages/admin/components/ArtistsLoader.jsx
+++ packages/admin/components/ArtistsLoader.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import Error from '@/elements/Error'
+import ArtistsFilters from '@/admin/ArtistsFilters'
+import ListSearch from '@/admin/elements/ListSearch'
 import ArtistsList from '@/admin/ArtistsList'
+
 
 import useGetPaginated from '@/admin/hooks/useGetPaginated'
 
@@ -38,17 +41,46 @@ const ArtistsLoader = ({ artistId }) => {
     }
   }, [finishedLoading, toggleGlobalLoading])
 
+  // FILTER
+  // Filtered List
+  const [filteredArtists, setFilteredArtists] = React.useState(artists)
+  // Search state
+  const [searchedArtists, setSearchedArtists] = React.useState(filteredArtists)
+
+  // GET ARTIST ARRAY BASED ON PAGE TYPE
+  const artistsArray = isSingleArtist ? artists : searchedArtists
+
   return (
     <section className="content">
-      {error && (
-        <div className="mb-4">
-          <p>Failed to fetch artists</p>
-          <Error error={error} />
-        </div>
+      {error && <div>Failed to fetch artists</div>}
+      {!finishedLoading ? <p>Loading...</p> : <p>Finished loading all artists</p>}
+      {!isSingleArtist && (
+        <>
+          <p>Total loaded: {artists.length}</p>
+          <p>Total filtered & searched: {searchedArtists.length}</p>
+        </>
       )}
-      {!finishedLoading && <p>Loading...</p>}
-      {!isSingleArtist && <p>Total: {artists.length}</p>}
-      {artists && <ArtistsList artists={artists} propsToDisplay={propsToDisplay} />}
+      {!isSingleArtist && (
+        <>
+          {/* FILTERS */}
+          <h4>
+            <strong>Filters</strong>
+          </h4>
+          <ArtistsFilters
+            setFilteredArtists={setFilteredArtists}
+            artists={artists}
+          />
+          {/* SEARCH */}
+          {!!artists.length && (
+            <ListSearch
+              className="pt-2"
+              fullList={filteredArtists}
+              updateList={setSearchedArtists}
+            />
+          )}
+        </>
+      )}
+      {artistsArray && <ArtistsList artists={artistsArray} propsToDisplay={propsToDisplay} />}
     </section>
   )
 }
