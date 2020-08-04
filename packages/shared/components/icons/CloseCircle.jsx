f6f245afb950850cf47251b9cd39bf598ac2147d
Benjamin E <ben@elwyn.co>
2020-08-04 17:17:48 +0200

    improve close circle button

--- packages/shared/components/icons/CloseCircle.jsx
+++ packages/shared/components/icons/CloseCircle.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import brandColors from '@/constants/brandColors'
 
-const CloseCircle = () => {
+const CloseCircle = ({ className }) => {
   return (
-    <svg width="389" height="388" viewBox="0 0 389 388" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg className={className} width="389" height="388" viewBox="0 0 389 388" fill="none" xmlns="http://www.w3.org/2000/svg">
       <ellipse cx="195.633" cy="194.755" rx="193.367" ry="193.245" fill={brandColors.textColor} />
       <rect x="293" y="113.818" width="251.987" height="25.1987" transform="rotate(135 293 113.818)" fill={brandColors.bgColor} />
       <rect x="114.818" y="96" width="251.987" height="25.1987" transform="rotate(45 114.818 96)" fill={brandColors.bgColor} />
     </svg>
   )
+}
+
+CloseCircle.propTypes = {
+  className: PropTypes.string,
+}
+
+CloseCircle.defaultProps = {
+  className: null,
 }
 
 export default CloseCircle
