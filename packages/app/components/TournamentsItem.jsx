aa8487d062f1bae7bfb10cd9577cfb754052042a
Benjamin E <ben@elwyn.co>
2020-08-05 16:57:01 +0200

    restrict width on metrics

--- packages/app/components/TournamentsItem.jsx
+++ packages/app/components/TournamentsItem.jsx
@@ -81,26 +81,28 @@ const TournamentsItem = ({ tournament, lastTournament, currency, className }) =>
         />
         {/* METRICS & LINKS */}
         <div className={['swiper-slide bg-white md:bg-transparent col-span-7 col-start-6'].join(' ')}>
-          <TournamentsItemMetrics
-            adMetrics={adMetrics}
-            isAdPair={isAdPair}
-            currency={currency}
-            className={['text-center mb-4 pl-10'].join(' ')}
-          />
-          <TournamentsItemLinks
-            linkA={linkA}
-            linkB={linkB}
-          />
-          {/* CLOSE METRICS BUTTON */}
-          <div className={['text-center mt-3', 'md:hidden'].join(' ')}>
-            <button
-              className="button--cross -hover"
-              aria-label="Hide metrics"
-              title="Hide metrics"
-              onClick={switchViews}
-            >
-              <CloseCircle className="w-full h-auto" />
-            </button>
+          <div className="md:max-w-md lg:max-w-lg ml-auto mr-0">
+            <TournamentsItemMetrics
+              adMetrics={adMetrics}
+              isAdPair={isAdPair}
+              currency={currency}
+              className={['text-center mb-4 pl-10'].join(' ')}
+            />
+            <TournamentsItemLinks
+              linkA={linkA}
+              linkB={linkB}
+            />
+            {/* CLOSE METRICS BUTTON */}
+            <div className={['text-center mt-3', 'md:hidden'].join(' ')}>
+              <button
+                className="button--cross -hover"
+                aria-label="Hide metrics"
+                title="Hide metrics"
+                onClick={switchViews}
+              >
+                <CloseCircle className="w-full h-auto" />
+              </button>
+            </div>
           </div>
         </div>
       </SwiperBlock>
