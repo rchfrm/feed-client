b93aca93ec3a455543c06c8a19c1d07306063612
Benjamin E <ben@elwyn.co>
2020-08-06 17:07:17 +0200

    add demo tooltip to spend

--- packages/app/copy/tournamentsCopy.js
+++ packages/app/copy/tournamentsCopy.js
@@ -1,7 +1,7 @@
 /* eslint-disable quotes, import/prefer-default-export */
 
 export const metricTooltips = {
-  score: 'sdkjfhskdfhjs skdjfhskdfhskdhjf skdjfh skjdhf skdjhf.',
+  spend: 'sdkjfhskdfhjs skdjfhskdfhskdhjf skdjfh skjdhf skdjhf.',
   impressions: ';skdfsldf sdkfjsldkfjsdkfsldkj sldkfj lsdkfj .',
   streak: ' ldf d sdflksjdfsdfkjs dl s dsjdsdf sdflksjdf.',
   normalized_es: 'sdjf sdkfskdfjs skdjfhd dksjfs hskdj.',
