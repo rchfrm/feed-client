9b54c0db79b9025e4e424f3052dcd3d4edbb1fad
Benjamin E <ben@elwyn.co>
2020-08-25 13:30:32 +0200

    Update packages/app/copy/PostsPageCopy.js
    Co-authored-by: jay-see-jay <6587212+jay-see-jay@users.noreply.github.com>

--- packages/app/copy/PostsPageCopy.js
+++ packages/app/copy/PostsPageCopy.js
@@ -87,7 +87,7 @@ Posts that Feed hasn't yet made into ads. For the best results select as many as
     double: [
       `To switch off a post that's currently running, slide to the left. The post will stop being promoted within 15 minutes.`,
 
-      `N.B. Once you have turned off an active post, you may not be able to re-activate it.`,
+      `N.B. Once you have manually stopped a post running, you may not be able to start it again.`,
     ],
   },
 
