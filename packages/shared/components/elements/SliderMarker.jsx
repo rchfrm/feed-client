b4c0840a5d602319c7851440e6cac82640becd0a
Benjamin E <ben@elwyn.co>
2020-08-27 14:08:58 +0200

    basic marker fade in

--- packages/shared/components/elements/SliderMarker.jsx
+++ packages/shared/components/elements/SliderMarker.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+// import { CSSTransition } from 'react-transition-group'
+import FadeInOut from '@/elements/FadeInOut'
+
 const SliderMarker = ({
   sliderValueRange,
   markerValue,
@@ -12,26 +15,33 @@ const SliderMarker = ({
   const width = 2
   const leftPercent = ((markerValue - min) / (max - min)) * 100
   const left = `${leftPercent}%`
+
   return (
-    <div
-      className={[
-        'absolute',
-        '-mt-3',
-        className,
-      ].join(' ')}
-      style={{
-        ...style,
-        top: 0,
-        left,
-        marginLeft: width * 1.5,
-        transform: 'translateY(-45%)',
-      }}
+    <FadeInOut
+      show
+      unmountOnExit
     >
-      {/* LINE */}
-      <div className="h-10 bg-green" style={{ width }} />
-      {/* TEXT */}
-      <p className="absolute top-0 left-0 mb-0 -mt-1 pl-2 text-sm whitespace-no-wrap">{markerLabel}</p>
-    </div>
+      <div
+        className={[
+          'absolute',
+          '-mt-3',
+          className,
+        ].join(' ')}
+        style={{
+          ...style,
+          top: 1,
+          left,
+          marginLeft: width * 1.5,
+          transform: 'translateY(-45%)',
+          opacity: 0,
+        }}
+      >
+        {/* LINE */}
+        <div className="h-10 bg-green" style={{ width }} />
+        {/* TEXT */}
+        <p className="absolute top-0 left-0 mb-0 -mt-1 pl-2 text-sm whitespace-no-wrap">{markerLabel}</p>
+      </div>
+    </FadeInOut>
   )
 }
 
