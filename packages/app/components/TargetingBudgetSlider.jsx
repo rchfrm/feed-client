25e24ac0860b45e276ea6e4dfda1f6072ec237d0
Benjamin E <ben@elwyn.co>
2020-09-02 15:46:13 +0200

    disable side panel dragging when dragging budget slider

--- packages/app/components/TargetingBudgetSlider.jsx
+++ packages/app/components/TargetingBudgetSlider.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import { ArtistContext } from '@/contexts/ArtistContext'
+import { SidePanelContext } from '@/app/contexts/SidePanelContext'
 
 import Slider from '@/elements/Slider'
 import SliderMarker from '@/elements/SliderMarker'
@@ -10,7 +11,7 @@ import SliderMarker from '@/elements/SliderMarker'
 import { formatCurrency } from '@/helpers/utils'
 import * as targetingHelpers from '@/app/helpers/targetingHelpers'
 
-const TargetingBudgetSlider = ({ budget, minBudget, onChange }) => {
+const TargetingBudgetSlider = ({ budget, minBudget, onChange, mobileVersion }) => {
   const { artistCurrency } = React.useContext(ArtistContext)
 
   const maxBudget = 30
@@ -38,8 +39,31 @@ const TargetingBudgetSlider = ({ budget, minBudget, onChange }) => {
     return targetingHelpers.getNextBudgetUpgrade(currentBudget)
   }, [currentBudget])
 
+  // DISABLE SIDEPANEL DRAG
+  const containerRef = React.useRef(null)
+  const { setDisableDrag } = React.useContext(SidePanelContext)
+  React.useEffect(() => {
+    if (!mobileVersion) return
+    const disableDrag = () => setDisableDrag(true)
+    const enableDrag = () => setDisableDrag(false)
+    const { current: containerEl } = containerRef
+    // disable drag
+    containerEl.addEventListener('mousedown', disableDrag, { passive: true })
+    containerEl.addEventListener('touchstart', disableDrag, { passive: true })
+    // enable drag
+    containerEl.addEventListener('mouseup', enableDrag, { passive: true })
+    containerEl.addEventListener('touchend', enableDrag, { passive: true })
+    return () => {
+      containerEl.removeEventListener('mousedown', disableDrag, { passive: true })
+      containerEl.removeEventListener('touchstart', disableDrag, { passive: true })
+      containerEl.removeEventListener('mouseup', enableDrag, { passive: true })
+      containerEl.removeEventListener('touchend', enableDrag, { passive: true })
+    }
+  // eslint-disable-next-line
+  }, [mobileVersion])
+
   return (
-    <div className={['pl-2'].join(' ')}>
+    <div className={['pl-2'].join(' ')} ref={containerRef}>
       <Slider
         label="Budget"
         labelClassName="-ml-2"
@@ -71,11 +95,13 @@ TargetingBudgetSlider.propTypes = {
   budget: PropTypes.number,
   minBudget: PropTypes.number,
   onChange: PropTypes.func.isRequired,
+  mobileVersion: PropTypes.bool,
 }
 
 TargetingBudgetSlider.defaultProps = {
   budget: 2,
   minBudget: 2,
+  mobileVersion: false,
 }
 
 
