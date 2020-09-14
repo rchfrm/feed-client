1eb0a861cde369b42db879d66415c7eab7bd8a85
Benjamin E <ben@elwyn.co>
2020-09-14 17:06:51 +0100

    create base for input

--- packages/shared/components/elements/InputBase.jsx
+++ packages/shared/components/elements/InputBase.jsx
@@ -1,13 +1,12 @@
 // IMPORT PACKAGES
-import React from 'react'
+import React, { Children } from 'react'
 import PropTypes from 'prop-types'
-
-import InputBase from '@/elements/InputBase'
 
 import TickIcon from '@/icons/TickIcon'
 import CrossIcon from '@/icons/CrossIcon'
 import FacebookIcon from '@/icons/FacebookIcon'
 import InstagramIcon from '@/icons/InstagramIcon'
+import TooltipButton from '@/elements/TooltipButton'
 import brandColors from '@/constants/brandColors'
 
 
@@ -43,26 +42,19 @@ const getIconEl = (icon, error, success) => {
   return null
 }
 
-const Input = ({
-  handleChange,
-  updateValue,
+const InputBase = ({
   name,
   label,
   tooltipMessage,
-  type,
-  placeholder,
-  value,
-  version,
-  width,
   readOnly,
   required,
   className,
   icon,
   error,
   success,
-  autoComplete,
   autoFocus,
   disabled,
+  children,
 }) => {
   const containerClasses = ['input--container', className]
   // Handle error and success states
@@ -92,99 +84,60 @@ const Input = ({
     }
   }, [autoFocus])
 
-  const onChange = React.useCallback((e) => {
-    if (!handleChange && !updateValue) {
-      console.error(`Please provide an function to update the value in ${name}.`)
-      return
-    }
-    if (handleChange && updateValue) {
-      console.error(`Please only provide *either* handleChange or updateValue for ${name}, not both.`)
-      return
-    }
-    if (updateValue) {
-      updateValue(e.target.value)
-      return
-    }
-    handleChange(e)
-  }, [handleChange, updateValue, name])
-
   return (
-    <InputBase
-      name={name}
-      label={label}
-      tooltipMessage={tooltipMessage}
-      readOnly={readOnly}
-      required={required}
-      className={className}
-      icon={icon}
-      error={error}
-      success={success}
-      autoFocus={autoFocus}
-      disabled={disabled}
-    >
-      <input
-        className={['input', `input--${version}`].join(' ')}
-        name={name}
-        type={type}
-        onChange={onChange}
-        placeholder={placeholder}
-        style={{
-          width: `${width}%`,
-        }}
-        value={value}
-        readOnly={readOnly || disabled}
-        required={required}
-        ref={inputElement}
-        autoComplete={!autoComplete ? 'off' : ''}
-      />
-    </InputBase>
+    <div className={containerClasses.join(' ')}>
+      <label
+        className="inputLabel"
+        htmlFor={name}
+      >
+        {/* LABEL */}
+        {label && (
+          <span className="inputLabel__text">
+            {label}
+            {required && <span className="asterisk">*</span>}
+            {/* LABEL TOOLTIP */}
+            {tooltipMessage && (
+              <TooltipButton copy={tooltipMessage} direction="right" />
+            )}
+          </span>
+        )}
+        {/* INPUT */}
+        <div className="input--inner">
+          {children}
+          {/* ICON */}
+          {iconEl}
+        </div>
+      </label>
+    </div>
   )
 }
 
-Input.propTypes = {
-  handleChange: PropTypes.func,
-  updateValue: PropTypes.func,
+InputBase.propTypes = {
   name: PropTypes.string.isRequired,
   label: PropTypes.string,
   tooltipMessage: PropTypes.string,
-  type: PropTypes.string,
-  placeholder: PropTypes.string,
   readOnly: PropTypes.bool,
-  value: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number,
-  ]),
-  version: PropTypes.string,
-  width: PropTypes.number,
   required: PropTypes.bool,
   className: PropTypes.string,
   icon: PropTypes.string,
   error: PropTypes.bool,
   success: PropTypes.bool,
-  autoComplete: PropTypes.bool,
   autoFocus: PropTypes.bool,
   disabled: PropTypes.bool,
+  children: PropTypes.node.isRequired,
 }
 
-Input.defaultProps = {
-  handleChange: null,
-  updateValue: null,
-  placeholder: '',
+InputBase.defaultProps = {
   readOnly: false,
-  type: 'text',
   label: '',
   tooltipMessage: '',
-  value: '',
-  version: 'box',
-  width: 100,
   required: false,
   className: '',
   icon: '',
   error: false,
   success: false,
-  autoComplete: true,
   autoFocus: false,
   disabled: false,
 }
 
-export default Input
+export default InputBase
