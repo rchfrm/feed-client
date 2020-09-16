56ce577a0df495da23ba300d52747ce8beb6c76f
Benjamin E <ben@elwyn.co>
2020-09-16 15:51:23 +0100

    change initial budget value

--- packages/app/helpers/targetingHelpers.js
+++ packages/app/helpers/targetingHelpers.js
@@ -15,7 +15,7 @@ export const demotargetingState = {
     { key: 'marseille', name: 'Marseille', countryCode: 'FR' },
     { key: 'lislesursogue', name: 'L\'Isle sur Sogue', countryCode: 'FR' },
   ],
-  budget: 3,
+  budget: 4.5,
   minReccBudget: 2,
   paused: false,
 }
