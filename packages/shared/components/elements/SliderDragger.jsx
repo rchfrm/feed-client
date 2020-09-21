aa53f8c31cb95534ee20b1bd0f9b21c53eea4019
Benjamin E <ben@elwyn.co>
2020-09-21 15:50:51 +0100

    create slider ghost marker

--- packages/shared/components/elements/SliderDragger.jsx
+++ packages/shared/components/elements/SliderDragger.jsx
@@ -1,26 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const SliderDragger = ({ isMarker }) => {
+const SliderDragger = ({ isMarker, className, style }) => {
   return (
     <div
       className={[
         'h-6 w-6',
         '-mt-1',
         'mx-auto rounded-full',
-        !isMarker ? 'bg-green' : null,
+        isMarker ? 'border-solid border-green border-2' : 'bg-green',
+        isMarker ? 'z0' : null,
+        className,
       ].join(' ')}
-      style={{ transform: 'translateY(-20%)', cursor: 'grab' }}
+      style={{ ...style, transform: 'translateY(-20%)', cursor: 'grab' }}
     />
   )
 }
 
 SliderDragger.propTypes = {
   isMarker: PropTypes.bool,
+  className: PropTypes.string,
+  style: PropTypes.object,
 }
 
 SliderDragger.defaultProps = {
   isMarker: false,
+  className: null,
+  style: {},
 }
 
 
---
+++ packages/shared/components/elements/SliderGhost.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import SliderDragger from '@/elements/SliderDragger'
+
+const SliderGhost = ({
+  sliderValueRange,
+  markerValue,
+  className,
+  style,
+}) => {
+  const [min, max] = sliderValueRange
+  const leftPercent = ((markerValue - min) / (max - min)) * 100
+  const left = `${leftPercent}%`
+
+  return (
+    <SliderDragger
+      isMarker
+      className={[
+        'absolute',
+        className,
+      ].join(' ')}
+      style={{
+        ...style,
+        left,
+      }}
+    />
+  )
+}
+
+SliderGhost.propTypes = {
+  sliderValueRange: PropTypes.array.isRequired,
+  markerValue: PropTypes.number.isRequired,
+  className: PropTypes.string,
+  style: PropTypes.object,
+}
+
+SliderGhost.defaultProps = {
+  className: null,
+  style: {},
+}
+
+
+export default SliderGhost
