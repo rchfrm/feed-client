1edb6d1d41b8606b1fb80889c78f78bf9414b2e4
Benjamin E <ben@elwyn.co>
2020-09-21 13:47:52 +0100

    tidy and remove feed recommended campaign options

--- packages/app/components/TargetingSummary.jsx
+++ packages/app/components/TargetingSummary.jsx
@@ -1,33 +1,20 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
 
-import Button from '@/elements/Button'
-
 import TargetingSummaryList from '@/app/TargetingSummaryList'
-import TargetingCampaignReccs from '@/app/TargetingCampaignReccs'
 import TargetingSummaryButtons from '@/app/TargetingSummaryButtons'
 
 import { TargetingContext } from '@/app/contexts/TargetingContext'
-
-import { demoRecs } from '@/app/helpers/targetingHelpers'
 
 const TargetingSummary = () => {
   const {
     initialTargetingState,
     currency,
-    selectedCampaignRecc,
-    setSelectedCampaignRecc,
     togglePauseCampaign,
     toggleMobileBudget,
     isDesktopLayout,
     setCurrentView,
   } = React.useContext(TargetingContext)
-  // SELECTED RECCS
-  const selectedReccId = React.useMemo(() => {
-    if (!selectedCampaignRecc) return null
-    const { id } = selectedCampaignRecc
-    return id
-  }, [selectedCampaignRecc])
 
   return (
     <div>
@@ -36,13 +23,6 @@ const TargetingSummary = () => {
         targetingState={initialTargetingState}
         currency={currency}
       />
-      {/* RECCS */}
-      {/* <TargetingCampaignReccs
-        reccs={demoRecs}
-        selectedReccId={selectedReccId}
-        setSelectedCampaignRecc={setSelectedCampaignRecc}
-        currency={currency}
-      /> */}
       {/* BUTTONS */}
       <TargetingSummaryButtons
         campaignPaused={initialTargetingState.paused}
