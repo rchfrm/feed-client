4d2d9749f82ce8fa0ddbd8f4bb9db7f87ea7f780
Benjamin E <ben@elwyn.co>
2020-09-22 10:25:28 +0100

    improve input placeholder text

--- packages/app/components/TargetingBudgetSetter.jsx
+++ packages/app/components/TargetingBudgetSetter.jsx
@@ -34,7 +34,7 @@ const TargetingBudgetSetter = ({
   const showCustomInitially = budget > sliderValueRange[1]
   const [showCustomBudget, setShowCustomBudget] = React.useState(showCustomInitially)
   // CUSTOM INPUT
-  const inputPlaceholder = `min. rec. budget: ${formatCurrency(minReccBudget, currency)}`
+  const inputPlaceholder = `Min. rec. budget: ${formatCurrency(minReccBudget, currency)}`
 
   return (
     <>
