1a7db674b8fd7cc803de6964211d0ea5d0f8de60
Benjamin E <ben@elwyn.co>
2020-09-24 17:53:02 +0100

    start using alert modal

--- packages/app/components/hooks/useSaveTargeting.js
+++ packages/app/components/hooks/useSaveTargeting.js
@@ -1,13 +1,25 @@
 import React from 'react'
 
+import useAlertModal from '@/hooks/useAlertModal'
+
 const useSaveTargeting = ({
   targetingState,
   saveCampaignSettings,
 }) => {
   // HANDLE ALERT
+  const { showAlert, closeAlert } = useAlertModal()
+  // SAVE FUNCTION
   const saveTargeting = React.useCallback((trigger, newState = null) => {
+    const { paused: isPaused } = targetingState
+    // Warn about updating settings when paused
+    if (isPaused && trigger === 'settings') {
+      const alertCopy = 'Are you sure?'
+      showAlert({ copy: alertCopy })
+      console.log('SHOW ALERT')
+      return
+    }
     saveCampaignSettings(newState || targetingState)
-  }, [saveCampaignSettings, targetingState])
+  }, [saveCampaignSettings, targetingState, showAlert])
 
   return saveTargeting
 }
--- packages/app/helpers/targetingHelpers.js
+++ packages/app/helpers/targetingHelpers.js
@@ -246,7 +246,7 @@ const formatSettings = (settings, currencyOffset) => {
   // Format settings
   return produce(settings, draftSettings => {
     if (typeof draftSettings.paused !== 'boolean') {
-      draftSettings.paused = false
+      draftSettings.paused = true
     }
     const { cities, countries } = draftSettings.geo_locations
     draftSettings.budget *= currencyOffset
