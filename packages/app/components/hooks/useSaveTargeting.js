0ffe757a2d67e4eed0bd5746abe4c7060d7a7972
Benjamin E <ben@elwyn.co>
2020-09-28 12:56:57 +0100

    show alert when saving targeting settings when paused

--- packages/app/components/hooks/useSaveTargeting.js
+++ packages/app/components/hooks/useSaveTargeting.js
@@ -2,6 +2,27 @@ import React from 'react'
 
 import useAlertModal from '@/hooks/useAlertModal'
 
+import copy from '@/app/copy/targetingPageCopy'
+
+const getWarningButtons = ({ warningType, saveCampaignSettings, saveState }) => {
+  if (warningType === 'saveWhenPaused') {
+    return [
+      {
+        text: 'Save and Resume Spending',
+        onClick: () => {
+          saveCampaignSettings({ ...saveState, paused: false })
+        },
+        color: 'red',
+      },
+      {
+        text: 'Save and Keep Paused',
+        onClick: () => saveCampaignSettings(saveState),
+        color: 'green',
+      },
+    ]
+  }
+}
+
 const useSaveTargeting = ({
   targetingState,
   saveCampaignSettings,
@@ -11,14 +32,21 @@ const useSaveTargeting = ({
   // SAVE FUNCTION
   const saveTargeting = React.useCallback((trigger, newState = null) => {
     const { paused: isPaused } = targetingState
+    const saveState = newState || targetingState
     // Warn about updating settings when paused
     if (isPaused && trigger === 'settings') {
-      const alertCopy = 'Are you sure?'
-      showAlert({ copy: alertCopy })
+      const alertCopy = copy.saveWhenPausedCopy
+      const buttons = getWarningButtons({
+        warningType: 'saveWhenPaused',
+        saveCampaignSettings,
+        saveState,
+      })
+      showAlert({ copy: alertCopy, buttons })
       console.log('SHOW ALERT')
       return
     }
-    saveCampaignSettings(newState || targetingState)
+    // Basic save
+    saveCampaignSettings(saveState)
   }, [saveCampaignSettings, targetingState, showAlert])
 
   return saveTargeting
---
+++ packages/app/copy/targetingPageCopy.js
@@ -0,0 +1,8 @@
+/* eslint-disable quotes */
+import * as ROUTES from '@/app/constants/routes'
+
+export default {
+  saveWhenPausedCopy: `**Spending is currently paused**
+  
+  Would you like to resume spending with these settings? Or just save these settings and keep the spending paused?`,
+}
