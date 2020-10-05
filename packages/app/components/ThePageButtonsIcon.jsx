e90edf7bd8ed51a4d9badb8e650a375a899d1239
Benjamin E <ben@elwyn.co>
2020-10-05 13:46:18 +0100

    put back currency option on page buttons icons

--- packages/app/components/ThePageButtonsIcon.jsx
+++ packages/app/components/ThePageButtonsIcon.jsx
@@ -6,17 +6,30 @@ import PostsIcon from '@/icons/PostsIcon'
 import ResultsIcon from '@/icons/ResultsIcon'
 import InsightsIcon from '@/icons/InsightsIcon'
 
+import BudgetEuroIcon from '@/icons/BudgetEuroIcon'
+import BudgetDollarIcon from '@/icons/BudgetDollarIcon'
+import BudgetPoundIcon from '@/icons/BudgetPoundIcon'
+
 import ThePageButtonsBadge from '@/app/ThePageButtonsBadge'
 
-const getIcon = (icon) => {
+import styles from '@/app/ThePageButtons.module.css'
+
+const getCurrencyIcon = (icon, currency) => {
+  if (currency === 'gbp') return <BudgetPoundIcon className={styles.shortIcon} />
+  if (currency === 'eur') return <BudgetEuroIcon className={styles.shortIcon} />
+  return <BudgetDollarIcon />
+}
+
+const getIcon = (icon, currency) => {
+  if (icon === 'budget') return getCurrencyIcon(icon, currency)
   if (icon === 'controls') return <JoystickIcon />
   if (icon === 'posts') return <PostsIcon />
   if (icon === 'results') return <ResultsIcon />
   if (icon === 'insights') return <InsightsIcon />
 }
 
-const ThePageButtonsIcon = ({ icon, className, showBadge }) => {
-  const iconEl = getIcon(icon)
+const ThePageButtonsIcon = ({ icon, className, showBadge, currency }) => {
+  const iconEl = getIcon(icon, currency.toLowerCase())
   return (
     <>
       {showBadge && <ThePageButtonsBadge />}
@@ -31,11 +44,13 @@ ThePageButtonsIcon.propTypes = {
   icon: PropTypes.string.isRequired,
   className: PropTypes.string,
   showBadge: PropTypes.bool,
+  currency: PropTypes.string,
 }
 
 ThePageButtonsIcon.defaultProps = {
   className: '',
   showBadge: false,
+  currency: '',
 }
 
 
