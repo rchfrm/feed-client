b8194d160614eef97bb971ac19f3b18ba7ae2655
Benjamin E <ben@elwyn.co>
2020-10-14 12:18:00 +0100

    fix initial default link

--- packages/app/components/contexts/PostsContext.jsx
+++ packages/app/components/contexts/PostsContext.jsx
@@ -11,7 +11,7 @@ const initialState = {
   togglePromotionGlobal: () => () => {},
   setTogglePromotionGlobal: () => {},
   goToPostSettings: () => {},
-  defaultLink: '',
+  defaultLink: {},
   setDefaultLink: () => {},
 }
 
