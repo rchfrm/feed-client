7dd44e7b42529b462c679ee21ce4a9b8f2ebbaf1
Benjamin E <ben@elwyn.co>
2020-10-14 13:10:12 +0100

    890 Test and remove FB ad account code (#210)
    * change filetype of facebook helpers
    
    * remove unused facebook helper functions
    
    * change export method for FB helpers
    
    * rename facebook helpers file
    
    * use latest FB graphQL api

---
+++ packages/app/helpers/facebookHelpers.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/prefer-default-export */
+
+const url = 'https://graph.facebook.com/v8.0/'
+
+export const getInstagramBusinessUsername = async (ig_business_id, fb_access_token) => {
+  const endpoint = `${url + ig_business_id}?fields=username&access_token=${fb_access_token}`
+  try {
+    const res = await fetch(endpoint, {
+      method: 'GET',
+    })
+    if (res.ok) {
+      const jsonResponse = await res.json()
+      return jsonResponse.username
+    }
+    throw new Error('Request to GET Instagram Username failed')
+  } catch (err) {
+    console.log(err)
+  }
+}
