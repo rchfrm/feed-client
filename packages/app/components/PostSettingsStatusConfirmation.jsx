7c18a1a9b951013daba5d5ebeb948d2da80515e0
Benjamin E <ben@elwyn.co>
2020-10-14 16:50:41 +0100

    fix props

--- packages/app/components/PostSettingsStatusConfirmation.jsx
+++ packages/app/components/PostSettingsStatusConfirmation.jsx
@@ -61,7 +61,7 @@ const PostSettingsStatusConfirmation = ({
 PostSettingsStatusConfirmation.propTypes = {
   triggerStatusUpdate: PropTypes.func.isRequired,
   confirmationOpen: PropTypes.bool.isRequired,
-  hideConfirmation: PropTypes.func.isRequired,
+  dismissConfirmation: PropTypes.func.isRequired,
 }
 
 export default PostSettingsStatusConfirmation
