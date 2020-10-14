0fa8402ae9a6a068f7b356d4b162ca52402786ed
Benjamin E <ben@elwyn.co>
2020-10-14 11:32:13 +0100

    use latest FB graphQL api

--- packages/app/helpers/facebookHelpers.js
+++ packages/app/helpers/facebookHelpers.js
@@ -1,7 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 
-const url = 'https://graph.facebook.com/v4.0/'
-
+const url = 'https://graph.facebook.com/v8.0/'
 
 export const getInstagramBusinessUsername = async (ig_business_id, fb_access_token) => {
   const endpoint = `${url + ig_business_id}?fields=username&access_token=${fb_access_token}`
