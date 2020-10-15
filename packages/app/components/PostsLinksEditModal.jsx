1c2bccf53c24a0b2569952976d1f1d351320c19a
Benjamin E <ben@elwyn.co>
2020-10-15 17:53:07 +0100

    WIP start work on save link modal

--- packages/app/components/PostsLinksEditModal.jsx
+++ packages/app/components/PostsLinksEditModal.jsx
@@ -1,11 +1,62 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import produce from 'immer'
+
+import Input from '@/elements/Input'
+import Select from '@/elements/Select'
+
 const PostsLinksEditModal = ({ link }) => {
+  const [linkProps, setLinkProps] = React.useState(link || {})
+  const handleInput = React.useCallback((e, prop) => {
+    const newLinkProps = produce(linkProps, draftProps => {
+      draftProps[prop] = e.target.value
+    })
+    setLinkProps(newLinkProps)
+  }, [linkProps])
   return (
-    <div>
-      Edit link
-      {link && link.name}
+    <div className="pt-3">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+        }}
+      >
+        <Input
+          placeholder="https://"
+          type="url"
+          version="box"
+          label="Link URL"
+          name="link-url"
+          handleChange={(e) => handleInput(e, 'href')}
+          value={linkProps.href}
+          required
+        />
+        <Input
+          placeholder=""
+          type="text"
+          version="box"
+          label="Link Name"
+          name="link-name"
+          handleChange={(e) => handleInput(e, 'name')}
+          value={linkProps.name}
+          required
+        />
+        {/* <Select
+          handleChange={handleSelect}
+          name="patchArtist"
+          label="Patch what?"
+          selectedValue={linkProps.folder}
+          options={patchOptions}
+        /> */}
+        <p>
+          <a
+            role="button"
+            className="no-underline -hover--green"
+          >
+            <strong>+ Add Folder</strong>
+          </a>
+        </p>
+      </form>
     </div>
   )
 }
