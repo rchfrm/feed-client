b2797ec927750e3d5cc09d7c40dbe419b6cc02bc
Benjamin E <ben@elwyn.co>
2020-10-15 13:25:50 +0100

    fix svg icon in safari

--- packages/shared/components/elements/Button.jsx
+++ packages/shared/components/elements/Button.jsx
@@ -60,7 +60,7 @@ const Button = React.forwardRef(({
       {loading && <Spinner className="button--spinner" />}
       <span className="button--innerText">
         {icon ? (
-          <span className="flex flex-grow text-center w-full">
+          <span className="flex flex-grow text-center items-center w-full">
             {icon}
             <span className="flex-grow pl-4" style={{ transform: 'translateX(-1rem)' }}>{children}</span>
           </span>
