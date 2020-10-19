be0a830705e4b634de7db617a3bdfd4fadbf0c7c
Benjamin E <ben@elwyn.co>
2020-10-19 16:44:39 +0100

    handle editing and deleting folder

--- packages/app/components/PostsLinksEditModalFolder.jsx
+++ packages/app/components/PostsLinksEditModalFolder.jsx
@@ -6,6 +6,7 @@ import produce from 'immer'
 import useAlertModal from '@/hooks/useAlertModal'
 
 import Input from '@/elements/Input'
+import copy from '@/app/copy/PostsPageCopy'
 
 const PostsLinksEditModalFolder = ({
   folder,
@@ -14,9 +15,30 @@ const PostsLinksEditModalFolder = ({
   runSaveFolder,
 }) => {
   const [folderProps, setFolderProps] = React.useState(folder || {})
+  // GET MODAL PROPS
+  const { setButtons, showAlert, closeAlert } = useAlertModal()
 
-  // UPDATE MODAL SAVE BUTTON
-  const { setButtons } = useAlertModal()
+  // CONFIRM DELETE
+  const confirmDelete = React.useCallback(() => {
+    showAlert({
+      children: null,
+      copy: copy.confirmDeleteFolder,
+      buttons: [
+        {
+          text: 'Yes, I\'m sure',
+          onClick: () => runSaveFolder(folderProps, 'delete'),
+          color: 'red',
+        },
+        {
+          text: 'Cancel',
+          onClick: closeAlert,
+          color: 'black',
+        },
+      ],
+    })
+  }, [showAlert, closeAlert, folderProps, runSaveFolder])
+
+  // UPDATE ALERT MODAL BUTTONS
   React.useEffect(() => {
     const newButtons = produce(modalButtons, draftButtons => {
       // Is buttons disabled
@@ -26,7 +48,7 @@ const PostsLinksEditModalFolder = ({
       draftButtons[0].disabled = !saveEnabled
       // Update delete button
       if (draftButtons[1].id === 'delete') {
-        draftButtons[1].onClick = () => runSaveFolder(folderProps, 'delete')
+        draftButtons[1].onClick = confirmDelete
       }
     })
     setButtons(newButtons)
