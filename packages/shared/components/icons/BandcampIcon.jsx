f4196c4e6ef5213a16f21ad51cc9f8b7cb40298e
Benjamin E <ben@elwyn.co>
2020-10-20 16:44:09 +0100

    improve social media icons

--- packages/shared/components/icons/BandcampIcon.jsx
+++ packages/shared/components/icons/BandcampIcon.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-function BandcampIcon({ width, fill }) {
-  const name = 'Bandcamp'
+import brandColors from '@/constants/brandColors'
+
+const BandcampIcon = ({
+  fill,
+  className,
+  title,
+  style,
+  width,
+}) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 418.83 246.11"
       width={width}
-      name={name}
+      name={title}
+      className={className}
+      style={style}
     >
-      <title>{name}</title>
-
+      <title>{title}</title>
       <polygon
         fill={fill}
         points="285.5 246.11 0 246.11 133.32 0 418.83 0 285.5 246.11"
@@ -18,6 +27,22 @@ function BandcampIcon({ width, fill }) {
 
     </svg>
   )
+}
+
+BandcampIcon.propTypes = {
+  fill: PropTypes.string,
+  title: PropTypes.string,
+  className: PropTypes.string,
+  style: PropTypes.object,
+  width: PropTypes.number,
+}
+
+BandcampIcon.defaultProps = {
+  fill: brandColors.textColor,
+  title: 'Bandcamp',
+  className: '',
+  style: null,
+  width: null,
 }
 
 export default BandcampIcon
