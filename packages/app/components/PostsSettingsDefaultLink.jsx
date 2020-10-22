acc322957de0b349cebb18a05f01edfea5b5d7d8
Benjamin E <ben@elwyn.co>
2020-10-22 16:55:41 +0100

    use post links select component in selecting default link

--- packages/app/components/PostsSettingsDefaultLink.jsx
+++ packages/app/components/PostsSettingsDefaultLink.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import LinkIcon from '@/icons/LinkIcon'
-import PencilIcon from '@/icons/PencilIcon'
-
-import brandColors from '@/constants/brandColors'
-
-// import PostsDefaultLinkSelector from '@/app/PostsDefaultLinkSelector'
+import PostLinksSelect from '@/app/PostLinksSelect'
 
 const PostsSettingsDefaultLink = ({
   defaultLink,
@@ -19,36 +14,10 @@ const PostsSettingsDefaultLink = ({
         className,
       ].join(' ')}
     >
-      <button
-        className={[
-          'relative',
-          'inline-block max-w-full',
-          'p-3',
-          'rounded-button border-black border-solid border-2',
-        ].join(' ')}
-        aria-label="Edit default link"
-        onClick={() => {}}
-      >
-        <p className={[
-          'flex items-baseline',
-          'mb-0',
-        ].join(' ')}
-        >
-          <span className="w-4 mr-3" style={{ transform: 'translateY(-0.1rem)' }}>
-            <LinkIcon className="w-full h-auto" />
-          </span>
-          <span className="truncate">{defaultLink.name}</span>
-        </p>
-        <div
-          className={[
-            'absolute top-0 right-0 -mt-5 -mr-6',
-            'flex items-center justify-center',
-            'bg-green rounded-full w-10 h-10',
-          ].join(' ')}
-        >
-          <PencilIcon className="w-5 h-auto" fill={brandColors.bgColor} />
-        </div>
-      </button>
+      <PostLinksSelect
+        currentLinkId={defaultLink.id}
+        onSelect={() => {}}
+      />
     </div>
   )
 }
