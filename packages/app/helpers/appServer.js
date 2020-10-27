bf6e8fde993f7face2709a89e930c434bd4cbe92
Benjamin E <ben@elwyn.co>
2020-10-27 17:44:17 +0000

    remove unused

--- packages/app/helpers/appServer.js
+++ packages/app/helpers/appServer.js
@@ -257,30 +257,3 @@ export const saveTargetingSettings = async (artistId, payload) => {
 export const getIntegrationErrors = async (artistId) => {
   return api.get(`/artists/${artistId}/integrations/errors`)
 }
-
-
-
-
-export const catchAxiosError = (error) => {
-  if (error.response) {
-    /*
-      * The request was made and the server responded with a
-      * status code that falls out of the range of 2xx
-      */
-    console.log('error.response.data', error.response.data)
-    console.log('error.response.status', error.response.status)
-    console.log('error.response.headers', error.response.headers)
-  } else if (error.request) {
-    /*
-      * The request was made but no response was received, `error.request`
-      * is an instance of XMLHttpRequest in the browser and an instance
-      * of http.ClientRequest in Node.js
-      */
-    console.log('error.request', error.request)
-  } else {
-    // Something happened in setting up the request and triggered an Error
-    console.log('Error', error.message)
-  }
-  console.log(error)
-  throw new Error(error.message)
-}
