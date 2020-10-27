10062e9c7448fb8d56b8b7586cd67218618209ee
Benjamin E <ben@elwyn.co>
2020-10-27 17:16:03 +0000

    start notifications page

---
+++ packages/app/pages/notifications/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import BasePage from '@/app/BasePage'
+import testPageReady from '@/hoc/testPageReady'
+import NotificationsContent from '@/app/NotificationsContent'
+
+const headerConfig = {
+  text: 'notifications',
+}
+
+const Page = () => {
+  return (
+    <BasePage
+      headerConfig={headerConfig}
+      artistRequired
+    >
+      <NotificationsContent />
+    </BasePage>
+  )
+}
+
+export default testPageReady('app')(Page)
