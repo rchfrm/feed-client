dc5fb58c071c1187238650513f42819cdbdab6a6
Benjamin E <ben@elwyn.co>
2020-11-17 16:02:12 +0100

    integrations copy change

--- packages/app/copy/integrationsCopy.js
+++ packages/app/copy/integrationsCopy.js
@@ -14,7 +14,7 @@ export default {
 - track your audience data on the Insights page (all integrations)
 - create your audience breakdown for geographic targeting options on the Controls page (Spotify only).
 
-When you add a new integration, audience data will appear on the Insights page after 24 hours.`,
+When you add a new integration, audience data will appear on the Insights page within 24 hours.`,
 
   // MODAL COPY
   cannotDelete: (platformTitle) => `It's currently not possible to disconnect or edit your ${platformTitle} integration.
