a901f79cb024f11acfefd1d6124fc1bfdf539d54
Benjamin E <ben@elwyn.co>
2020-11-23 17:05:19 +0100

    refactor notifications store

--- packages/app/components/store/notificationsStore.js
+++ packages/app/components/store/notificationsStore.js
@@ -5,10 +5,18 @@ import { fetchNotifications } from '@/app/helpers/notificationsHelpers'
 
 const initialState = {
   artistId: '',
-  notificationsNew: [],
-  notificationsOld: [],
+  notifications: [],
+  totalUnreadNotifications: 0,
   openNotification: null,
   artistsWithNotifications: [],
+}
+
+// COUNT UNREAD NOTIFICATIONS
+const countUnreadNotifications = (notifications) => {
+  return notifications.reduce((total, { read }) => {
+    if (read) return total
+    return total + 1
+  }, 0)
 }
 
 // FETCH NOTIFICATIONS (called whenever artist mounts)
@@ -16,8 +24,7 @@ const fetchAndSetNotifications = (set, get) => async (artistId) => {
   // If requesting for the same artist, just return data from store
   if (artistId === get().artistId) {
     return {
-      notificationsNew: get().notificationsNew,
-      notificationsOld: get().notificationsOld,
+      notifications: get().notifications,
     }
   }
   // Else fetch notifications from server
@@ -25,60 +32,62 @@ const fetchAndSetNotifications = (set, get) => async (artistId) => {
   // Stop here if error
   if (error) return
   const { notifications, artistIds } = res
-  // Split notifications into old and new
-  const { notificationsNew, notificationsOld } = notifications.reduce((notificationsObj, notification) => {
-    const { read } = notification
-    return produce(notificationsObj, draftState => {
-      if (read) {
-        draftState.notificationsOld.push(notification)
-      } else {
-        draftState.notificationsNew.push(notification)
-      }
-    })
-  }, {
-    notificationsNew: [],
-    notificationsOld: [],
-  })
   // Get array of artist IDs with notifications
   const artistsWithNotifications = artistIds.map(({ id }) => id)
+  // GET TOTAL UNREAD NOTIFICATIONS
+  const totalUnreadNotifications = countUnreadNotifications(notifications)
   // SET
-  set({ notificationsNew, notificationsOld, artistsWithNotifications, artistId })
+  set({
+    artistId,
+    notifications,
+    totalUnreadNotifications,
+    artistsWithNotifications,
+  })
   // RETURN
-  return { notificationsNew, notificationsOld }
+  return { notifications }
+}
+
+
+// UPDATE A PROP ON A NOTIFICATION
+const updateNotification = (set, get) => (notificationId, prop, value) => {
+  const { notifications } = get()
+  const notificationsUpdated = produce(notifications, draftNotifications => {
+    const notificationIndex = draftNotifications.findIndex(({ id }) => id === notificationId)
+    if (notificationIndex === -1) return
+    draftNotifications[notificationIndex][prop] = value
+  })
+  set({ notifications: notificationsUpdated })
+  return notificationsUpdated
 }
 
 // SET NOTIFICATION AS ACTIVE
 const setAsOpen = (set, get) => (notificationId) => {
-  const { setAsRead, notificationsNew: notifications } = get()
+  const { setAsRead, notifications } = get()
   const openNotification = notifications.find(({ id }) => id === notificationId)
   set({ openNotification })
   // Set notification as read
   setAsRead(notificationId)
 }
 
-// UPDATE A PROP ON A NOTIFICATION
-const updateNotification = (set, get) => (notificationId, prop, value) => {
-  const notifications = get().notificationsNew
-  const notificationsUpdated = produce(notifications, draftNotifications => {
-    const notificationIndex = draftNotifications.findIndex(({ id }) => id === notificationId)
-    if (notificationIndex === -1) return
-    draftNotifications[notificationIndex][prop] = value
-  })
-  set({ notificationsNew: notificationsUpdated })
+// SET NOTIFICATION AS READ
+const setAsRead = (set, get) => (notificationId) => {
+  const notificationsUpdated = updateNotification(set, get)(notificationId, 'read', true)
+  const totalNotificationsUnread = countUnreadNotifications(notificationsUpdated)
+  set({ totalNotificationsUnread })
 }
 
 // EXPORT
 const useNotificationsStore = create((set, get) => ({
   // STATE
   artistId: initialState.artistId,
-  notificationsNew: initialState.notificationsNew,
-  notificationsOld: initialState.notificationsOld,
+  notifications: initialState.notifications,
+  totalUnreadNotifications: initialState.totalUnreadNotifications,
   openNotification: initialState.openNotification,
   artistsWithNotifications: initialState.artistsWithNotifications,
   // GETTERS
   fetchAndSetNotifications: fetchAndSetNotifications(set, get),
   // SETTERS
-  setAsRead: (id) => updateNotification(set, get)(id, 'read', true),
+  setAsRead: (id) => setAsRead(set, get)(id),
   setAsOpen: (id) => setAsOpen(set, get)(id),
   clear: () => set(initialState),
 }))
