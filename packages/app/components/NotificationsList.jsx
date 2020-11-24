939eaa65e7cc6c13672dcfb8c764a08bf69116dc
Benjamin E <ben@elwyn.co>
2020-11-24 17:08:34 +0100

    setup notification keyboard controls

--- packages/app/components/NotificationsList.jsx
+++ packages/app/components/NotificationsList.jsx
@@ -1,17 +1,62 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Mousetrap from 'mousetrap'
+import shallow from 'zustand/shallow'
+
 import useNotificationStore from '@/app/store/notificationsStore'
+
+import useBreakpointTest from '@/hooks/useBreakpointTest'
 
 import MarkdownText from '@/elements/MarkdownText'
 import NotificationItem from '@/app/NotificationItem'
 
 import copy from '@/app/copy/notificationsCopy'
 
-const getNotifications = state => state.notifications
+const readNotificationsStore = (state) => ({
+  notifications: state.notifications,
+  openNotificationId: state.openNotificationId,
+  setAsOpen: state.setAsOpen,
+})
 
 const NotificationsList = ({ className }) => {
-  const notifications = useNotificationStore(getNotifications)
+  const {
+    notifications,
+    openNotificationId,
+    setAsOpen,
+  } = useNotificationStore(readNotificationsStore, shallow)
+
+  // NAVIGATE UP AND DOWN
+  const navigateNotification = React.useCallback(({ key }) => {
+    if (!openNotificationId) return
+    const direction = key === 'ArrowDown' ? 'down' : 'up'
+    const openNotificationIndex = notifications.findIndex(({ id }) => id === openNotificationId)
+    const atStart = openNotificationIndex === 0
+    const atEnd = openNotificationIndex === notifications.length - 1
+    // Stop here if at end
+    if ((direction === 'up' && atStart) || (direction === 'down' && atEnd)) return
+    // Open next not
+    const navToIndex = direction === 'up' ? openNotificationIndex - 1 : openNotificationIndex + 1
+    const nextNotification = notifications[navToIndex]
+    setAsOpen(nextNotification.id)
+  }, [openNotificationId, notifications, setAsOpen])
+
+  // SETUP KEYBOARD CONTROLS
+  const isDesktopLayout = useBreakpointTest('md')
+  React.useEffect(() => {
+    if (!isDesktopLayout) {
+      Mousetrap.unbind('up', navigateNotification)
+      Mousetrap.unbind('down', navigateNotification)
+      return
+    }
+    Mousetrap.bind('up', navigateNotification)
+    Mousetrap.bind('down', navigateNotification)
+    return () => {
+      Mousetrap.unbind('up', navigateNotification)
+      Mousetrap.unbind('down', navigateNotification)
+    }
+  }, [navigateNotification, isDesktopLayout])
+
   return (
     <div
       className={[
