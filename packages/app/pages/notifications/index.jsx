19f2450b871c0dd391e3810ba9d4e48248e897eb
Benjamin E <ben@elwyn.co>
2020-11-24 16:06:40 +0100

    fetch notifications dictionary from Dato

--- packages/app/pages/notifications/index.jsx
+++ packages/app/pages/notifications/index.jsx
@@ -3,22 +3,56 @@ import React from 'react'
 import BasePage from '@/app/BasePage'
 import testPageReady from '@/hoc/testPageReady'
 
+import { formatDictionary } from '@/app/helpers/notificationsHelpers'
+
 import NotificationsContent from '@/app/NotificationsContent'
+
+// Dato data
+import getDatoData from '@/helpers/getDatoData'
 
 const headerConfig = {
   text: 'notifications',
 }
 
-const Page = () => {
+const Page = ({ notificationsDictionary }) => {
+  const dictionaryFormatted = React.useMemo(() => {
+    return formatDictionary(notificationsDictionary)
+  }, [notificationsDictionary])
   return (
     <BasePage
       headerConfig={headerConfig}
       artistRequired
       staticPage
     >
-      <NotificationsContent />
+      <NotificationsContent notificationsDictionary={dictionaryFormatted} />
     </BasePage>
   )
 }
+
+const query = `
+  query {
+    allNotifications {
+      id
+      code
+      subcode
+      title
+      actionable
+      dismissable
+      appMessage
+      emailMessage
+      emailFrom
+    }
+  }
+`
+
+export async function getStaticProps() {
+  const { data: { allNotifications } } = await getDatoData(query, 'notificationsQuery')
+  return {
+    props: {
+      notificationsDictionary: allNotifications,
+    },
+  }
+}
+
 
 export default testPageReady('app')(Page)
