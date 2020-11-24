aac9fb4279e78b5c712038ba5ff9cbaa9dbdf400
Benjamin E <ben@elwyn.co>
2020-11-24 12:25:01 +0100

    handle notifications loading error

--- packages/app/components/NotificationsContent.jsx
+++ packages/app/components/NotificationsContent.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 
+import Error from '@/elements/Error'
+
 import NotificationsLoader from '@/app/NotificationsLoader'
 import NotificationsList from '@/app/NotificationsList'
 import NotificationCurrentInfo from '@/app/NotificationCurrentInfo'
 
+import useNotificationStore from '@/app/store/notificationsStore'
+
+const getNotificationsError = state => state.notificationsError
+
 const NotificationsContent = () => {
   const containerRef = React.useRef(null)
+  const notificationsError = useNotificationStore(getNotificationsError)
   return (
     <div ref={containerRef} className="relative">
+      <Error error={notificationsError} />
       <NotificationsList />
       <NotificationCurrentInfo containerRef={containerRef} />
     </div>
