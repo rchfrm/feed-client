60c5bdf3c8210e5897237600c4f34909afede042
Benjamin E <ben@elwyn.co>
2020-11-25 16:55:57 +0100

    use flag icon for actionable warnings

--- packages/app/components/NotificationItemLabel.jsx
+++ packages/app/components/NotificationItemLabel.jsx
@@ -1,23 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import FlagIcon from '@/icons/FlagIcon'
+
+import brandColors from '@/constants/brandColors'
+
 const NotificationItemLabel = ({ type }) => {
   const label = type === 'unread' ? 'Unread notification' : 'Notification requires action'
   return (
     <div
       className={[
-        'absolute w-2 h-2 rounded-full',
+        'absolute w-2 h-2',
         type === 'unread' ? 'md:-ml-2' : 'md:-mr-4',
-        type === 'unread' ? 'bg-green' : 'bg-red',
+        type === 'unread' ? 'md:-ml-2' : 'md:-mr-4',
+        type === 'unread' ? 'bg-green rounded-full' : null,
       ].join(' ')}
       style={{
-        top: '1.2rem',
+        top: type === 'unread' ? '1.2rem' : '1.05rem',
         ...(type === 'unread' && { left: '0.75rem' }),
         ...(type === 'action' && { right: '4.5rem' }),
       }}
       aria-label={label}
       title={label}
-    />
+    >
+      {type === 'action' && (
+        <FlagIcon className="w-full h-auto" fill={brandColors.red} />
+      )}
+    </div>
   )
 }
 
