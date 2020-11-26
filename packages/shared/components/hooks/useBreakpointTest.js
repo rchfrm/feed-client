c71d3a62b560497feda9d96b11db19a1579e6a0b
Benjamin E <ben@elwyn.co>
2020-11-26 17:52:05 +0100

    fix use breakpoint test to only run in browser (#267)

--- packages/shared/components/hooks/useBreakpointTest.js
+++ packages/shared/components/hooks/useBreakpointTest.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 import useBrowserStore from '@/hooks/useBrowserStore'
 
 /*
@@ -7,10 +9,19 @@ import useBrowserStore from '@/hooks/useBrowserStore'
 
 */
 
-const useBreakpointTest = (breakpoint) => {
-  const { breakpointsKeyedByName } = useBrowserStore()
-  const breakpointWidth = breakpointsKeyedByName[breakpoint]
+const testBreakpoint = (breakpointWidth) => {
   return window.matchMedia(`(min-width: ${breakpointWidth}px)`).matches
+}
+
+const useBreakpointTest = (breakpoint) => {
+  const isClient = typeof window === 'object'
+  const { breakpointsKeyedByName, width } = useBrowserStore()
+  const breakpointWidth = breakpointsKeyedByName[breakpoint]
+  const [matchesBreakpoint, setMatchesBreakpoint] = React.useState(isClient ? testBreakpoint(breakpointWidth) : false)
+  React.useEffect(() => {
+    setMatchesBreakpoint(testBreakpoint(breakpointWidth))
+  }, [width, breakpointWidth])
+  return matchesBreakpoint
 }
 
 export default useBreakpointTest
