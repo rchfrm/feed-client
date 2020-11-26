d06ed9f64075a5b40436efe49325c9a4dca70d8e
Benjamin E <ben@elwyn.co>
2020-11-26 12:42:43 +0100

    WIP more on signing up with referral code

--- packages/app/components/SignupAddReferralCode.jsx
+++ packages/app/components/SignupAddReferralCode.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
 
+import Input from '@/elements/Input'
+import Button from '@/elements/Button'
+
+
+import styles from '@/LoginPage.module.css'
+
 const SignupAddReferralCode = ({}) => {
+  const [code, setCode] = React.useState('')
   return (
-    <div>
-      Referral
+    <div className={styles.container}>
+      <h2 className="h3">Sign up here with your referral code</h2>
+      <form
+        onSubmit={(e) => {
+          if (!code) return
+          e.preventDefault()
+        }}
+      >
+        <Input
+          updateValue={setCode}
+          name="referral-code"
+          label="Referral code"
+          value={code}
+          autoFocus
+        />
+        <div className="flex justify-end">
+          <Button
+            type="submit"
+            disabled={!code}
+          >
+            Sign up here
+          </Button>
+        </div>
+      </form>
     </div>
   )
 }
