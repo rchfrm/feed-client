5ac03874d95a9cf641ee4af940c7461d20ee2c06
Benjamin E <ben@elwyn.co>
2020-11-26 15:08:06 +0100

    store used referral code when true

--- packages/app/components/store/referralStore.js
+++ packages/app/components/store/referralStore.js
@@ -1,9 +1,10 @@
 import create from 'zustand'
 
 const initialState = {
-  referralCode: '',
+  userReferralCode: '',
   hasValidCode: false, // does the code meet the regex pattern?
   hasTrueCode: false, // Is the code for real?
+  usedReferralCode: '', // The code that was used to sign up
 }
 
 // TODO define regex test for valid code
@@ -31,19 +32,22 @@ const testCodeValidity = (set) => (code) => {
 const testCodeTruth = (set, get) => async (code) => {
   const { hasValidCode } = get()
   const hasTrueCode = await truthTest(code, hasValidCode)
-  set({ hasTrueCode })
+  const usedReferralCode = hasTrueCode ? code : ''
+  set({ hasTrueCode, usedReferralCode })
   return hasTrueCode
 }
 
 const [useReferralStore] = create((set, get) => ({
-  referralCode: initialState.referralCode,
+  userReferralCode: initialState.userReferralCode,
+  usedReferralCode: initialState.usedReferralCode,
   hasValidCode: initialState.hasValidCode,
   hasTrueCode: initialState.hasTrueCode,
   // ACTIONS
   testCodeValidity: (code) => testCodeValidity(set)(code),
   testCodeTruth: (code) => testCodeTruth(set, get)(code),
   // SETTERS
-  setReferralCode: (referralCode) => set({ referralCode }),
+  setUserReferralCode: (userReferralCode) => set({ userReferralCode }),
+  setUsedReferralCode: (usedReferralCode) => set({ usedReferralCode }),
   setHasValidCode: (state) => set({ hasValidCode: state }),
   setHasTrueCode: (state) => set({ hasTrueCode: state }),
 }))
