7cc6c106ca506080dfde5e8697dbff8014184bbf
Benjamin E <ben@elwyn.co>
2020-11-26 10:44:56 +0100

    start on signup closed page

--- packages/app/components/SignupClosedContent.jsx
+++ packages/app/components/SignupClosedContent.jsx
@@ -2,13 +2,56 @@ import React from 'react'
 // import PropTypes from 'prop-types'
 
 import MarkdownText from '@/elements/MarkdownText'
+import Input from '@/elements/Input'
+import Button from '@/elements/Button'
+
+import { testValidEmail } from '@/helpers/utils'
+
+import styles from '@/LoginPage.module.css'
 
 import copy from '@/app/copy/referralCodeCopy'
 
 const SignupClosedContent = () => {
+  const [email, setEmail] = React.useState('')
+  const [formValid, setFormValid] = React.useState(false)
+  React.useEffect(() => {
+    const isEmailValid = testValidEmail(email)
+    setFormValid(isEmailValid)
+  }, [email])
+
+  // TODO handle submitting email
+  const handleFormSubmission = () => {
+    // Stop here if form not valid
+    if (!formValid) return
+    console.log('Form submitted')
+  }
+
   return (
-    <div>
-      <MarkdownText markdown={copy.signupClosedCopy} />
+    <div className={styles.container}>
+      <MarkdownText markdown={copy.signupClosedCopy} allowHtml />
+      <form
+        className="pt-2"
+        onSubmit={(e) => {
+          e.preventDefault()
+          handleFormSubmission()
+        }}
+      >
+        <Input
+          updateValue={setEmail}
+          name="email"
+          label="Your email"
+          value={email}
+          autoFocus
+        />
+        <div className="flex justify-end">
+          <Button
+            type="submit"
+            disabled={!formValid}
+          >
+            Join the queue
+          </Button>
+        </div>
+      </form>
     </div>
   )
 }
--- packages/app/copy/referralCodeCopy.js
+++ packages/app/copy/referralCodeCopy.js
@@ -4,7 +4,7 @@ import * as ROUTES from '@/app/constants/routes'
 export default {
   signupClosedCopy: `There is currently a waiting list to join the Feed beta.
   
-  Have a <span>referral code️</span> from a Feed user, or one of our partners? [Sign up here](${ROUTES.REFERRAL}).
+  Have a <span class="green--underline">referral code️</span> from a Feed user, or one of our partners? [Sign up here](${ROUTES.REFERRAL}).
 
 If not, add your email below and we'll get in touch as soon as a space is available!`,
   explanation: `This is your referral code. Share it with others to let them jump the queue and sign up to Feed. You'll also benefit from our referral scheme when it starts running.`,
