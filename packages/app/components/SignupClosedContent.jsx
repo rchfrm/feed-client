9d225d955988d68870bc913934eb83aaa5845a73
Benjamin E <ben@elwyn.co>
2020-12-07 13:31:55 +0100

    start on signup queue form

--- packages/app/components/SignupClosedContent.jsx
+++ packages/app/components/SignupClosedContent.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 // import PropTypes from 'prop-types'
 
 import MarkdownText from '@/elements/MarkdownText'
-import Input from '@/elements/Input'
-import Button from '@/elements/Button'
 
-import { testValidEmail } from '@/helpers/utils'
+import SignupQueueForm from '@/app/SignupQueueForm'
 
 import styles from '@/LoginPage.module.css'
 
@@ -13,47 +11,12 @@ import copy from '@/app/copy/referralCodeCopy'
 import loginCopy from '@/app/copy/LoginPageCopy'
 
 const SignupClosedContent = () => {
-  const [email, setEmail] = React.useState('')
-  const [formValid, setFormValid] = React.useState(false)
-  React.useEffect(() => {
-    const isEmailValid = testValidEmail(email)
-    setFormValid(isEmailValid)
-  }, [email])
-
-  // TODO handle submitting email
-  const handleFormSubmission = () => {
-    // Stop here if form not valid
-    if (!formValid) return
-    console.log('Form submitted')
-  }
-
   return (
     <div className={styles.container}>
       <MarkdownText markdown={copy.signupClosedCopy} allowHtml />
-      <form
+      <SignupQueueForm
         className="pt-2 mb-8"
-        onSubmit={(e) => {
-          e.preventDefault()
-          handleFormSubmission()
-        }}
-      >
-        <Input
-          updateValue={setEmail}
-          name="email"
-          label="Your email"
-          value={email}
-          autoFocus
-        />
-        <div className="flex justify-end">
-          <Button
-            type="submit"
-            className="w-full sm:w-buttonWidthWide"
-            disabled={!formValid}
-          >
-            Join the queue
-          </Button>
-        </div>
-      </form>
+      />
       {/* Link to login page */}
       <MarkdownText markdown={loginCopy.loginReminder} />
     </div>
---
+++ packages/app/components/SignupQueueForm.jsx
@@ -0,0 +1,174 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import produce from 'immer'
+
+import Input from '@/elements/Input'
+import Button from '@/elements/Button'
+
+import { testValidUrl, testValidEmail } from '@/helpers/utils'
+
+const formElements = [
+  {
+    id: 'MERGE0',
+    label: 'Email',
+    type: 'email',
+    autoCapitalize: 'off',
+    autoCorrect: 'off',
+    required: true,
+    errorMessage: 'Please enter a valid email',
+  },
+  {
+    id: 'MERGE1',
+    label: 'Name',
+    required: true,
+    hidden: true,
+  },
+  {
+    id: 'MERGE2',
+    label: 'Surname',
+    hidden: true,
+  },
+  {
+    id: 'MMERGE8',
+    label: 'Facebook page',
+    required: true,
+    type: 'url',
+    errorMessage: 'Please inlcude a valid link',
+  },
+  {
+    id: 'MMERGE9',
+    label: 'Instagram page',
+    type: 'url',
+  },
+  {
+    id: 'MMERGE10',
+    label: 'Link to your work',
+    placeholder: 'https://',
+    type: 'url',
+    required: true,
+    errorMessage: 'Please inlcude a valid link',
+  },
+  {
+    id: 'MMERGE11',
+    label: 'Reason for wanting to use Feed',
+  },
+]
+
+const testValidInput = (type, value) => {
+  if (!type) return !!value
+  if (type === 'url') return testValidUrl(value)
+  if (type === 'email') return testValidEmail(value)
+}
+
+const SignupQueueForm = ({ className }) => {
+  const [values, setValues] = React.useState({})
+  const [isFormValid, setIsFormValid] = React.useState(false)
+
+  React.useEffect(() => {
+    const hasEmptyRequired = formElements.some(({ required, id }) => {
+      const { value, valid } = values[id] || {}
+      return !value && required && !valid
+    })
+    setIsFormValid(!hasEmptyRequired)
+  }, [values])
+  return (
+    <form
+      className={[
+        'relative',
+        className,
+      ].join(' ')}
+    >
+      {/* HIDDEN To add them to the 'Waiting List' group */}
+      <input
+        type="checkbox"
+        id="group_64"
+        name="group[6082][64]"
+        value="1"
+        className="av-checkbox"
+        checked
+        readOnly
+        style={{
+          position: 'absolute',
+          left: '-10000em',
+        }}
+      />
+
+      {formElements.map((element, index) => {
+        const {
+          id,
+          type,
+          label,
+          placeholder,
+          autoCapitalize,
+          autoCorrect,
+          hidden,
+          required,
+          errorMessage,
+        } = element
+        if (hidden) return
+        const { value, valid, error } = values[id] || {}
+        return (
+          <Input
+            key={id}
+            type={type}
+            autoFocus={index === 0}
+            autoCapitalize={autoCapitalize}
+            autoCorrect={autoCorrect}
+            name={id}
+            id={id}
+            value={value}
+            label={label}
+            placeholder={placeholder}
+            required={required}
+            error={error}
+            errorMessage={errorMessage}
+            handleChange={(e) => {
+              const { target: { value } } = e
+              setValues((values) => {
+                const valid = testValidInput(type, value)
+                return produce(values, draftValues => {
+                  draftValues[id] = {
+                    value,
+                    valid,
+                    error: false,
+                  }
+                })
+              })
+            }}
+            onBlur={() => {
+              console.log('valid', valid)
+              setValues((values) => {
+                return produce(values, draftValues => {
+                  draftValues[id] = {
+                    error: !valid && required,
+                  }
+                })
+              })
+            }}
+          />
+        )
+      })}
+      <div className="flex justify-end">
+        <Button
+          type="submit"
+          className="w-full sm:w-auto"
+          disabled={!isFormValid}
+        >
+          Join the queue
+        </Button>
+      </div>
+    </form>
+  )
+}
+
+SignupQueueForm.propTypes = {
+  className: PropTypes.string,
+}
+
+SignupQueueForm.defaultProps = {
+  className: null,
+}
+
+
+export default SignupQueueForm
