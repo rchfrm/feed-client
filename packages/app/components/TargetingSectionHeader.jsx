1b155bd82aec152056b4fc1d3111459aaf08d4bf
Benjamin E <ben@elwyn.co>
2021-01-11 15:34:57 +0000

    add tooltip options to targeting section header

--- packages/app/components/TargetingSectionHeader.jsx
+++ packages/app/components/TargetingSectionHeader.jsx
@@ -1,20 +1,37 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const TargetingSectionHeader = ({ header, className }) => {
+import TooltipButton from '@/elements/TooltipButton'
+
+const TargetingSectionHeader = ({
+  header,
+  tooltipMessage,
+  className,
+}) => {
   return (
     <h4 className={['font-body', className].join(' ')}>
       <strong>{header}</strong>
+      {tooltipMessage && (
+        <TooltipButton
+          copy={tooltipMessage}
+          direction="top"
+          buttonStyle={{
+            transform: 'translateY(0.62rem)',
+          }}
+        />
+      )}
     </h4>
   )
 }
 
 TargetingSectionHeader.propTypes = {
   header: PropTypes.string.isRequired,
+  tooltipMessage: PropTypes.string,
   className: PropTypes.string,
 }
 
 TargetingSectionHeader.defaultProps = {
+  tooltipMessage: '',
   className: null,
 }
 
