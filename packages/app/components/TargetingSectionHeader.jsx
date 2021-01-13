b8fe0bb50f935690c49396d275196ed7268f6af0
Benjamin E <ben@elwyn.co>
2021-01-13 14:53:43 +0000

    WEBAPP-51 Add facebook and instagram options to Controls page (#285)
    * start platform selector buttons
    
    * make gender selection code more generic
    
    * add tooltip options to targeting section header
    
    * add gender tooltip
    
    * remove margin bottom after text block in tooltip
    
    * add platform selectors
    
    * add platform selection to targeting summary
    
    * update platform selection on server
    
    * get warning copy for changing selected platform
    
    * put platform warning copy below selectors
    
    * add intro warning to targeting settings
    
    * add description to platform selector
    
    * improve targeting copy
    
    * update copy
    
    * update confirmation text
    
    * fix default prop type
    
    * improve spacing of targeting settings intro
    
    * Update packages/app/copy/targetingPageCopy.js
    
    Co-authored-by: jay-see-jay <6587212+jay-see-jay@users.noreply.github.com>
    
    * reduce margin at bottom of help text
    
    * improve full height container resizing
    
    * adjust settings intro margin
    
    * improve settings intro margin
    
    Co-authored-by: jay-see-jay <6587212+jay-see-jay@users.noreply.github.com>

--- packages/app/components/TargetingSectionHeader.jsx
+++ packages/app/components/TargetingSectionHeader.jsx
@@ -1,20 +1,46 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const TargetingSectionHeader = ({ header, className }) => {
+import TooltipButton from '@/elements/TooltipButton'
+import MarkdownText from '@/elements/MarkdownText'
+
+const TargetingSectionHeader = ({
+  header,
+  description,
+  tooltipMessage,
+  className,
+}) => {
   return (
-    <h4 className={['font-body', className].join(' ')}>
-      <strong>{header}</strong>
-    </h4>
+    <>
+      <h4 className={['font-body', className].join(' ')}>
+        <strong>{header}</strong>
+        {tooltipMessage && (
+        <TooltipButton
+          copy={tooltipMessage}
+          direction="top"
+          buttonStyle={{
+            transform: 'translateY(0.62rem)',
+          }}
+        />
+        )}
+      </h4>
+      {description && (
+        <MarkdownText markdown={description} className="-mt-2 pb-1" />
+      )}
+    </>
   )
 }
 
 TargetingSectionHeader.propTypes = {
   header: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  tooltipMessage: PropTypes.string,
   className: PropTypes.string,
 }
 
 TargetingSectionHeader.defaultProps = {
+  description: '',
+  tooltipMessage: '',
   className: null,
 }
 
