825160c14aa8829690b7a5a91e343a7b2630a72b
Benjamin E <ben@elwyn.co>
2021-01-13 17:11:01 +0000

    WIP start on new pixel modal

---
+++ packages/app/components/hooks/useCreateNewPixel.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+import useAlertModal from '@/hooks/useAlertModal'
+
+import { createNewPixel } from '@/app/helpers/settingsHelpers'
+
+const useCreateNewPixel = ({
+  artistId,
+  onSave = () => {},
+  onError = () => {},
+  onCancel = () => {},
+}) => {
+  // HANDLE ALERT
+  const { showAlert, closeAlert } = useAlertModal()
+
+  // SAVE PIXEL FUNCTION
+  const savePixel = React.useCallback((pixelName) => {
+    const { res, error } = createNewPixel(artistId, pixelName)
+    if (error) {
+      onError(error)
+      return
+    }
+    onSave(res)
+  }, [artistId, onError, onSave])
+
+  // OPEN MODAL
+  const openNewPixelModal = React.useCallback(() => {
+    const buttons = [
+      {
+        text: 'Save pixel',
+        onClick: () => {},
+        color: 'green',
+        id: 'save',
+      },
+      {
+        text: 'Cancel',
+        onClick: () => {
+          closeAlert()
+          onCancel()
+        },
+        color: 'black',
+      },
+    ]
+  }, [])
+
+  return openNewPixelModal
+}
+
+export default useCreateNewPixel
