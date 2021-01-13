d2aa3d2cdad32cf2474441eb2e78a881f75a4d94
Benjamin E <ben@elwyn.co>
2021-01-13 12:45:01 +0000

    adjust settings intro margin

--- packages/app/components/TargetingSettings.jsx
+++ packages/app/components/TargetingSettings.jsx
@@ -85,7 +85,7 @@ const TargetingSettings = () => {
         <MarkdownText
           markdown={copy.settingsIntro}
           className={[
-            '-mt-6 mb-4',
+            '-mt-6 mb-0',
             'xxs:mt-0',
             'minContent:-mt-6 pb-12',
             'md:pb-4',
