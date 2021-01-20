7758798f214329e51b7e51d603a6ba76c83b39fd
Benjamin E <ben@elwyn.co>
2021-01-20 11:50:32 +0000

    WEBAPP-60 Remove old results page (#288)
    * remove content from results page
    
    * remove all results page content
    
    * remove components that only appeared on results page

--- packages/app/pages/results/index.jsx
+++ packages/app/pages/results/index.jsx
@@ -2,28 +2,19 @@ import React from 'react'
 
 import BasePage from '@/app/BasePage'
 import testPageReady from '@/hoc/testPageReady'
-import ResultsLoader from '@/app/ResultsLoader'
-
-import Alert from '@/elements/Alert'
-import ResultsPostsPagePopup from '@/app/ResultsPostsPagePopup'
 
 const headerConfig = {
   text: 'results',
 }
 
 const Page = () => {
-  const [popupContents, setPopupContents] = React.useState(<ResultsPostsPagePopup />)
   return (
     <BasePage
       headerConfig={headerConfig}
       noArtistHeader={headerConfig}
       artistRequired
     >
-      <Alert
-        contents={popupContents}
-        resetAlert={() => setPopupContents(null)}
-      />
-      <ResultsLoader />
+      Content
     </BasePage>
   )
 }
