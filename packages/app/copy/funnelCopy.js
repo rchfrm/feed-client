7c2467e8b4ac09e970ec26701b54aeaf847e43d4
Benjamin E <ben@elwyn.co>
2021-01-27 16:28:13 +0000

    rename audiences

--- packages/app/copy/funnelCopy.js
+++ packages/app/copy/funnelCopy.js
@@ -1,21 +1,25 @@
 /* eslint-disable quotes */
+import * as ROUTES from '@/app/constants/routes'
 
 export default {
   // AUDIENCE TEXT
-  cold: {
-    title: 'cold',
-    tooltip: `Tooltip about cold`,
+  growA: {
+    title: 'Grow A',
+    tooltip: ``,
     description: 'Engagement from new audiences.',
+    noAds: () => `No ads running. [Opt in more posts](${ROUTES.POSTS}?postStatus=not-run) to enable this audience and boost performance.`,
   },
-  cool: {
-    title: 'cool',
-    tooltip: `Tooltip about coold`,
+  growB: {
+    title: 'Grow B',
+    tooltip: ``,
     description: 'Mmm yeah cool.',
+    noAds: (minBudget) => `No ads running. [Opt in more posts](${ROUTES.POSTS}?postStatus=not-run) and make sure your budget is set to ${minBudget} to enable this audience and boost performance.`,
   },
-  warm: {
-    title: 'warm',
-    tooltip: `Tooltip about warm`,
+  nurture: {
+    title: 'Nurture',
+    tooltip: ``,
     description: 'Feels good man.',
+    noAds: () => `No ads running. [Opt in more posts](${ROUTES.POSTS}?postStatus=not-run) to enable this audience and boost performance.`,
   },
   hot: {
     title: 'hot',
