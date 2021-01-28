e5966b4bbefe98f784396761d055581322f99f66
Benjamin E <ben@elwyn.co>
2021-01-28 15:18:43 +0000

    refactor funnel loader into its own component

--- packages/app/components/FunnelsContent.jsx
+++ packages/app/components/FunnelsContent.jsx
@@ -1,19 +1,12 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
 
-import { useAsync } from 'react-async'
-
-import Error from '@/elements/Error'
-import Spinner from '@/elements/Spinner'
 import MarkdownText from '@/elements/MarkdownText'
 import ButtonHelp from '@/elements/ButtonHelp'
 
 // import ResultsSummaryText from '@/app/ResultsSummaryText'
+import FunnelsLoader from '@/app/FunnelsLoader'
 import FunnelsSelectionButtons from '@/app/FunnelsSelectionButtons'
-import FunnelView from '@/app/FunnelView'
-
-import { ArtistContext } from '@/contexts/ArtistContext'
-import { InterfaceContext } from '@/contexts/InterfaceContext'
 
 import useBreakpointTest from '@/hooks/useBreakpointTest'
 import useAnimateScroll from '@/hooks/useAnimateScroll'
@@ -22,15 +15,10 @@ import * as funnelHelpers from '@/app/helpers/funnelHelpers'
 
 import copy from '@/app/copy/funnelCopy'
 
-const FunnelsContent = () => {
-  const { funnelOptions, audienceTypes } = funnelHelpers
-  // Import interface context
-  const { artistId, artistLoading } = React.useContext(ArtistContext)
-  const { toggleGlobalLoading } = React.useContext(InterfaceContext)
+const { funnelOptions } = funnelHelpers
 
+const FunnelsContent = () => {
   const [activeFunnelId, setActiveFunnelId] = React.useState(funnelOptions[0].id)
-  const [activeFunnelData, setActiveFunnelData] = React.useState(null)
-  const [error, setError] = React.useState(null)
 
   const isTwoColumns = useBreakpointTest('lg')
 
@@ -47,29 +35,6 @@ const FunnelsContent = () => {
     scrollTo({ offset: top - 16 })
   // eslint-disable-next-line
   }, [activeFunnelId])
-
-  // LOAD AUDIENCES
-  const { isPending } = useAsync({
-    promiseFn: funnelHelpers.fetchAudiences,
-    watchFn: funnelHelpers.watchFunction,
-    // The variable(s) to pass to promiseFn
-    artistId,
-    activeFunnelId,
-    // When fetch finishes
-    onResolve: (data) => {
-      // Turn off global loading
-      toggleGlobalLoading(false)
-      // Handle result...
-      const dataFormatted = funnelHelpers.formatData(data)
-      setActiveFunnelData(dataFormatted)
-    },
-    // Handle errors
-    onReject(error) {
-      setError(error)
-      setActiveFunnelData(null)
-      toggleGlobalLoading(false)
-    },
-  })
 
   // MORE INFO BUTTON
   const moreInfoButton = (
@@ -126,26 +91,11 @@ const FunnelsContent = () => {
             </div>
           )}
         </div>
-        <div
+        <FunnelsLoader
           className="col-span-8 bmw:col-span-8 lg:ml-10"
           style={{ minHeight: '30rem' }}
-        >
-          {artistLoading || isPending || !activeFunnelData ? (
-            // LOADING SPINNER
-            <Spinner />
-          ) : (
-            // FUNNEL
-            <>
-              <Error messagePrefix="Failed to load ads: " error={error} />
-              <FunnelView
-                funnelData={activeFunnelData}
-                audienceTypes={audienceTypes}
-                activeFunnelId={activeFunnelId}
-                classNameInner="sm:max-w-xl lg:mx-auto"
-              />
-            </>
-          )}
-        </div>
+          activeFunnelId={activeFunnelId}
+        />
       </div>
     </div>
   )
