49d2d1e476d37ea18507f6d75a14b4a5fd9786d9
Benjamin E <ben@elwyn.co>
2021-02-01 14:51:14 +0000

    add min budget recc for stories

--- packages/shared/components/contexts/ArtistContext.jsx
+++ packages/shared/components/contexts/ArtistContext.jsx
@@ -283,23 +283,27 @@ function ArtistProvider({ children, disable }) {
     const minUnit = utils.roundToFactorOfTen((fbMin) / 0.9)
     const minHard = utils.roundToFactorOfTen((2 * fbMin) / 0.9)
     const minReccomendedBase = utils.roundToFactorOfTen((3 * fbMin) / 0.9)
+    const minReccomendedStories = utils.roundToFactorOfTen((5 * fbMin) / 0.9)
     // The values in the smallest currency unit (eg pence)
     const smallestUnit = {
       minUnit,
       minHard,
       minReccomendedBase,
+      minReccomendedStories,
     }
     // The value in the largest currency unit (eg pound)
     const largestUnit = {
       minUnit: minUnit / currencyOffset,
       minHard: minHard / currencyOffset,
       minReccomendedBase: minReccomendedBase / currencyOffset,
+      minReccomendedStories: minReccomendedStories / currencyOffset,
     }
     // The value as a string
     const string = {
       minUnit: utils.formatCurrency(largestUnit.minUnit, currencyCode),
       minHard: utils.formatCurrency(largestUnit.minHard, currencyCode),
       minReccomendedBase: utils.formatCurrency(largestUnit.minReccomendedBase, currencyCode),
+      minReccomendedStories: utils.formatCurrency(largestUnit.minReccomendedStories, currencyCode),
     }
     return {
       smallestUnit,
