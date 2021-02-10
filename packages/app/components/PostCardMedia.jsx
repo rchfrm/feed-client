b64b356a634ef522e270b6c379ae0647a9f2ce15
Benjamin E <ben@elwyn.co>
2021-02-10 12:35:00 +0000

    improve post card media

--- packages/app/components/PostCardMedia.jsx
+++ packages/app/components/PostCardMedia.jsx
@@ -10,23 +10,61 @@ const PostCardMedia = ({
   className,
 }) => {
   const [hasMedia, setHasMedia] = React.useState(true)
+  const [selectedThumbnail, setSelectedThumbnail] = React.useState({})
+  const [ready, setReady] = React.useState(false)
+
   React.useEffect(() => {
     setHasMedia(!!media)
   }, [media])
+
   return (
     <div
       className={[
+        'relative rounded-dialogue overflow-hidden',
         className,
       ].join(' ')}
     >
-      {hasMedia && (
-        <PostImage
-          mediaSrc={media}
-          thumbnailOptions={thumbnails}
-          title={caption}
-          setHasMedia={setHasMedia}
-        />
-      )}
+      <div
+        className={[
+          'w-full relative bg-grey-1',
+          ready ? 'opacity-1' : 'opacity-0',
+        ].join(' ')}
+        style={{ paddingTop: '100%' }}
+      >
+        {/* BLURRED BG (if there is a thumbnail and it's not square) */}
+        {selectedThumbnail.src && selectedThumbnail.ratio !== 1 && (
+          <div
+            className="absolute w-full h-full blurred--image--bg"
+            style={{
+              backgroundImage: `url(${selectedThumbnail.src})`,
+              width: '110%',
+              height: '110%',
+              left: '-5%',
+              top: '-5%',
+            }}
+          />
+        )}
+        {/* POST IMAGE (if there is media) */}
+        {hasMedia && (
+          <div
+            className={[
+              'absolute top-0 left-0 w-full h-full',
+              selectedThumbnail.src && selectedThumbnail.ratio > 1 ? 'px-3' : null,
+            ].join(' ')}
+          >
+            <PostImage
+              className="h-full"
+              mediaSrc={media}
+              thumbnailOptions={thumbnails}
+              title={caption}
+              onFinishedSelection={(thumbnail = {}) => {
+                setSelectedThumbnail(thumbnail)
+                setReady(true)
+              }}
+            />
+          </div>
+        )}
+      </div>
     </div>
   )
 }
