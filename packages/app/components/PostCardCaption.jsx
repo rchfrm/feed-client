56b8d0cbbe398ceeab456c7bc8fe67992ef4bf4f
Benjamin E <ben@elwyn.co>
2021-02-10 16:56:39 +0000

    create post card caption text

---
+++ packages/app/components/PostCardCaption.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const PostCardCaption = ({ caption, className }) => {
+  return (
+    <div
+      className={[
+        'absolute top-0 left-0 w-full h-full bg-grey-1',
+        'p-4',
+        'font-display text-lg',
+        'force--text--wrap',
+        'overflow-auto',
+        className,
+      ].join(' ')}
+    >
+      {caption}
+    </div>
+  )
+}
+
+PostCardCaption.propTypes = {
+  className: PropTypes.string,
+}
+
+PostCardCaption.defaultProps = {
+  className: null,
+}
+
+export default React.memo(PostCardCaption)
