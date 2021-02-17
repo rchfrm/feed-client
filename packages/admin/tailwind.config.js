36cf192f19b7c670d9123b03bb0975c7fadf1880
Benjamin E <ben@elwyn.co>
2021-02-17 10:03:25 +0000

    Fix monorepo (#306)
    * tidy admin next config
    
    * tidy tailwind
    
    * fix after upgrading next-transpile-modules
    
    * move integration helpers into shared
    
    * fix admin dev start
    
    * refactor package.jsons across monorepo

--- packages/admin/tailwind.config.js
+++ packages/admin/tailwind.config.js
@@ -1,7 +1,9 @@
 const config = require('../shared/tailwind.config.js')
+
 const purgeFiles = [
   './components/**/*.jsx',
   './pages/**/*.jsx',
   '../shared/components/**/*.jsx',
 ]
+
 module.exports = config(purgeFiles)
