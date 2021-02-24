283ea7f844d03934a92a3ba02724ed24a861769e
Benjamin E <ben@elwyn.co>
2021-02-24 16:28:24 +0000

    start controls content

---
+++ packages/app/components/ControlsContent.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+
+const ControlsContent = () => {
+  return (
+    <div>
+      Controls
+    </div>
+  )
+}
+
+ControlsContent.propTypes = {
+
+}
+
+export default ControlsContent
