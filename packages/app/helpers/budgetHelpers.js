b906ec3bbe3c33a1e9c0aa024569ae5fafa86efc
Benjamin E <ben@elwyn.co>
2021-02-25 12:24:55 +0000

    BUDGET: improve budget calculations

--- packages/app/helpers/budgetHelpers.js
+++ packages/app/helpers/budgetHelpers.js
@@ -1,23 +1,50 @@
 import * as utils from '@/helpers/utils'
 
+
+// ROUNDING
+// --------
+
+const getRoundingValue = (amount) => {
+  if (amount < 750) return 100
+  if (amount < 5000) return 1_000
+  if (amount < 50000) return 10_000
+  return 100_000
+}
+
+const roundUpAmount = (amount) => {
+  const roundTo = getRoundingValue(amount)
+  return Math.ceil(amount / roundTo) * roundTo
+}
+
+const getFbMinMultipleCalc = (fbMin, serviceFee) => (multiple) => {
+  return roundUpAmount((multiple * fbMin) / (1 - serviceFee))
+}
+
 export const calcFeedMinBudgetInfo = (artist) => {
   if (!artist || !artist.min_daily_budget_info) return
+  // Service Feed
+  const serviceFee = 10 / 100
   // Extra info from artist
   const { min_daily_budget_info: {
     amount: fbMin,
     currency: { code: currencyCode, offset: currencyOffset },
   } } = artist
+  const calcFbMinMultiple = getFbMinMultipleCalc(fbMin, serviceFee)
   // Calc min values
-  const minBase = utils.roundToFactorOfTen((fbMin) / 0.9)
-  const minHard = utils.roundToFactorOfTen((2 * fbMin) / 0.9)
-  const minReccomendedBase = utils.roundToFactorOfTen((3 * fbMin) / 0.9)
-  const minReccomendedStories = utils.roundToFactorOfTen((5 * fbMin) / 0.9)
+  const minBase = calcFbMinMultiple(1)
+  const minHard = Math.min(calcFbMinMultiple(2), minBase * 2)
+  const minReccomendedBase = Math.min(calcFbMinMultiple(3), minBase * 3)
+  const minReccomendedStories = Math.min(calcFbMinMultiple(5), minBase * 5)
+  const extraCountryCost = minReccomendedBase
+  const extraCityCost = minReccomendedBase / 4
   // The values in the smallest currency unit (eg pence)
   const smallestUnit = {
     minBase,
     minHard,
     minReccomendedBase,
     minReccomendedStories,
+    extraCountryCost,
+    extraCityCost,
   }
   // The value in the largest currency unit (eg pound)
   const largestUnit = {
@@ -25,14 +52,19 @@ export const calcFeedMinBudgetInfo = (artist) => {
     minHard: minHard / currencyOffset,
     minReccomendedBase: minReccomendedBase / currencyOffset,
     minReccomendedStories: minReccomendedStories / currencyOffset,
+    extraCountryCost: extraCountryCost / currencyOffset,
+    extraCityCost: extraCityCost / currencyOffset,
   }
-  // The value as a string
+  // The value as a formatted string
   const string = {
     minBase: utils.formatCurrency(largestUnit.minBase, currencyCode),
     minHard: utils.formatCurrency(largestUnit.minHard, currencyCode),
     minReccomendedBase: utils.formatCurrency(largestUnit.minReccomendedBase, currencyCode),
     minReccomendedStories: utils.formatCurrency(largestUnit.minReccomendedStories, currencyCode),
+    extraCountryCost: utils.formatCurrency(largestUnit.extraCountryCost, currencyCode),
+    extraCityCost: utils.formatCurrency(largestUnit.extraCityCost, currencyCode),
   }
+
   return {
     smallestUnit,
     largestUnit,
