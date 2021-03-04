0bc751e92ae049aa2cb1094aa93f34dcdace81ac
Benjamin E <ben@elwyn.co>
2021-03-04 15:26:40 +0000

    improve sending missing email

--- packages/app/components/SignupMissingEmail.jsx
+++ packages/app/components/SignupMissingEmail.jsx
@@ -4,9 +4,17 @@ import PropTypes from 'prop-types'
 import MarkdownText from '@/elements/MarkdownText'
 import Input from '@/elements/Input'
 import Button from '@/elements/Button'
+import Error from '@/elements/Error'
+
+import { AuthContext } from '@/contexts/AuthContext'
+import { ArtistContext } from '@/contexts/ArtistContext'
+import { UserContext } from '@/contexts/UserContext'
 
 import { testValidEmail } from '@/helpers/utils'
+import { redirectPage } from '@/app/helpers/signupHelpers'
+import { trackSignUp } from '@/app/helpers/trackingHelpers'
 
+import * as ROUTES from '@/app/constants/routes'
 import copy from '@/app/copy/signupCopy'
 
 import styles from '@/LoginPage.module.css'
@@ -17,17 +25,42 @@ const SignupMissingEmail = ({ fbEmail, className }) => {
   const [isEmailValid, setIsEmailValid] = React.useState(false)
   const [showEmailError, setShowEmailError] = React.useState(false)
   const [loading, setLoading] = React.useState(false)
+  const [error, setError] = React.useState(null)
   React.useEffect(() => {
     const isEmailValid = testValidEmail(email)
     setIsEmailValid(isEmailValid)
   }, [email])
+  // GET AUTH and ARTIST context
+  const { auth } = React.useContext(AuthContext)
+  const { setNoArtist } = React.useContext(ArtistContext)
+  // GET USER CREATION
+  const { runCreateUser, setUserLoading } = React.useContext(UserContext)
+  console.log('auth', auth)
   // Submit form
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault()
     if (!isEmailValid) return
     setLoading(true)
-    console.log('submit email', email)
+    setUserLoading(true)
+    // STORE USER
+    const { res: user, error } = await runCreateUser({
+      firstName: first_name,
+      lastName: last_name,
+    })
+    if (error) {
+      setError(error)
+      return
+    }
+    // Clear artists (beacuse new user)
+    setNoArtist()
+    // Stop loading
+    setUserLoading(false)
     setLoading(false)
+    setError(null)
+    // TRACK
+    trackSignUp({ authProvider: 'facebook', userId: user.id })
+    // REDIRECT
+    redirectPage(ROUTES.SIGN_UP_CONTINUE)
   }
   return (
     <div
@@ -37,11 +70,12 @@ const SignupMissingEmail = ({ fbEmail, className }) => {
       ].join(' ')}
     >
       <MarkdownText markdown={copy.missingFbEmail} />
+      <Error error={error} />
       <form onSubmit={onSubmit}>
         <Input
           name="emailContact"
-          label="Contact email address"
-          placeholder=""
+          // label="Contact email address"
+          placeholder="Contact email"
           value={email}
           updateValue={setEmail}
           type="email"
