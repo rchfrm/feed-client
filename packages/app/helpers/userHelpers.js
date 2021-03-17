68fa149491ab826464feea9eb70aaf1d0a833ae0
Benjamin E <ben@elwyn.co>
2021-03-17 16:38:36 +0000

    fix sorting user artists

--- packages/app/helpers/userHelpers.js
+++ packages/app/helpers/userHelpers.js
@@ -3,9 +3,7 @@ import { sortArtistsAlphabetically } from '@/app/helpers/artistHelpers'
 
 export const sortUserArtists = (user) => {
   return produce(user, draft => {
-    const { artists } = draft
-    draft.artists = sortArtistsAlphabetically(Object.values(artists))
-    return draft
+    draft.artists = sortArtistsAlphabetically(Object.values(user.artists))
   })
 }
 
