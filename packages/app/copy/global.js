f0390af371753e1ee7b4e6dfcb23f1aaf238163e
Benjamin E <ben@elwyn.co>
2021-03-24 11:26:49 +0000

    tidy

--- packages/app/copy/global.js
+++ packages/app/copy/global.js
@@ -5,7 +5,7 @@ import * as ROUTES from '@/app/constants/routes'
 export default {
   noArtists: `It looks like you haven't connected any Facebook pages yet. Please finish the sign up process [here](${ROUTES.SIGN_UP_CONNECT_PROFILES}).`,
 
-  unverifiedEmails: ({ emails = [], isNewUser, isAccountPage }) => {
+  unverifiedEmails: ({ emails = [], isAccountPage }) => {
     return `It looks like you have ${emails.length === 1 ? `an unverified email: **${emails[0]}**` : `some unverified emails: ${emails.join(', ')}`}.
     
 Please check your inbox to confirm. ${!isAccountPage ? `Or change the email address on the [Account Page](${ROUTES.ACCOUNT}).` : ''}`
