352b23694a4e765caa7bc98207defeeff935f132
Benjamin E <ben@elwyn.co>
2021-03-29 15:30:15 +0100

    no need for billing context on account page

--- packages/app/components/AccountPageLoader.jsx
+++ packages/app/components/AccountPageLoader.jsx
@@ -1,19 +1,13 @@
 import React from 'react'
 // IMPORT CONTEXTS
 import { UserContext } from '@/contexts/UserContext'
-import { BillingContextProvider } from '@/app/contexts/BillingContext'
 // IMPORT COMPONENTS
 import AccountPageDetails from '@/app/AccountPageDetails'
 
 function AccountPageLoader() {
   // IMPORT USER STATE
   const { user } = React.useContext(UserContext)
-
-  return (
-    <BillingContextProvider user={user}>
-      <AccountPageDetails user={user} />
-    </BillingContextProvider>
-  )
+  return <AccountPageDetails user={user} />
 }
 
 export default AccountPageLoader
