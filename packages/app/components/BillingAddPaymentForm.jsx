5c545e88dd79cdfb02ceb589732498e63cdcce7c
Benjamin E <ben@elwyn.co>
2021-03-30 11:22:28 +0100

    refactor billing form

--- packages/app/components/BillingAddPaymentForm.jsx
+++ packages/app/components/BillingAddPaymentForm.jsx
@@ -1,49 +1,192 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import { loadStripe } from '@stripe/stripe-js'
+
+import {
+  CardNumberElement,
+  CardCvcElement,
+  CardExpiryElement,
+  Elements,
+  useElements,
+  useStripe,
+} from '@stripe/react-stripe-js'
+
+import Button from '@/elements/Button'
+import Error from '@/elements/Error'
+import Input from '@/elements/Input'
+
+
+const STRIPE_ELEMENT_OPTIONS = {
+  style: {
+    base: {
+      fontSize: '18px',
+      color: '#424770',
+      letterSpacing: '0.025em',
+      '::placeholder': {
+        color: '#aab7c4',
+      },
+    },
+    invalid: {
+      color: '#9e2146',
+    },
+  },
+}
+
+// UPDATE DB
+const postPaymentMethod = async (paymentMethod, { name, postalCode }) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({ paymentMethod, name, postalCode })
+    }, 500)
+  })
+}
+
+// THE FORM
+const FORM = ({ setSidePanelButton, setSuccess }) => {
+  const elements = useElements()
+  const stripe = useStripe()
+  const [name, setName] = React.useState('')
+  const [postalCode, setPostalCode] = React.useState('')
+  const [error, setError] = React.useState(null)
+  const [paymentMethod, setPaymentMethod] = React.useState(null)
+
+  // FORM STATE
+  const [isFormValid, setIsFormValid] = React.useState(false)
+  const [isLoading, setIsLoading] = React.useState(true)
+
+  // WAIT FOR STRIPE TO LOAD
+  React.useEffect(() => {
+    if (!stripe || !elements) {
+      setIsLoading(true)
+      return
+    }
+    setIsLoading(false)
+  }, [elements, stripe])
+
+  // TEST FORM IS VALID
+  React.useEffect(() => {
+    const formValid = !!(name && postalCode && elements && stripe)
+    setIsFormValid(formValid)
+  }, [name, postalCode, elements, stripe])
+
+  // HANDLE FORM
+  const onSubmit = React.useCallback(async () => {
+    if (!isFormValid || isLoading) return
+    setIsLoading(true)
+    const cardElement = elements.getElement(CardNumberElement)
+    const { paymentMethod, error: stripeError } = await stripe.createPaymentMethod({
+      type: 'card',
+      card: cardElement,
+      billing_details: {
+        name,
+        address: {
+          postal_code: postalCode,
+        },
+      },
+    })
+    if (stripeError) {
+      setError(stripeError)
+      setIsLoading(false)
+      return
+    }
+    console.log('paymentMethod', paymentMethod)
+    const { res, error } = await postPaymentMethod(paymentMethod, { name, postalCode })
+    console.log('res', res)
+    setIsLoading(false)
+    if (!error) {
+      setSuccess(true)
+    }
+  }, [isFormValid, isLoading, name, postalCode, setSuccess, stripe, elements])
+
+  // CHANGE SIDEPANEL BUTTON
+  React.useEffect(() => {
+    const button = <Button version="green" disabled={!isFormValid} onClick={onSubmit}>Submit</Button>
+    setSidePanelButton(button)
+  }, [isFormValid, onSubmit, setSidePanelButton])
+
+  return (
+    <form
+      className="relative"
+      onSubmit={(e) => {
+        e.preventDefault()
+        onSubmit(e)
+      }}
+    >
+      <Error error={error} />
+
+      {/* NAME */}
+      <Input
+        label="Full name"
+        value={name}
+        updateValue={setName}
+      />
+
+      <label htmlFor="cardNumber">Card Number</label>
+      <CardNumberElement
+        id="cardNumber"
+        onChange={(e) => {
+          console.log('change stripe', e)
+        }}
+        options={STRIPE_ELEMENT_OPTIONS}
+      />
+      <label htmlFor="expiry">Card Expiration</label>
+      <CardExpiryElement
+        id="expiry"
+        onChange={(e) => {
+          console.log('change stripe', e)
+        }}
+        options={STRIPE_ELEMENT_OPTIONS}
+      />
+      <label htmlFor="cvc">CVC</label>
+      <CardCvcElement
+        id="cvc"
+        onChange={(e) => {
+          console.log('change stripe', e)
+        }}
+        options={STRIPE_ELEMENT_OPTIONS}
+      />
+
+      {/* POSTAL CODE */}
+      <Input
+        label="Postal Code"
+        value={postalCode}
+        updateValue={setPostalCode}
+      />
+
+      {/* HIDE BUTTON BECAUSE IT IS IN SIDEPANEL */}
+      <button type="submit" disabled={!stripe} className="absolute" style={{ left: '-1000em' }}>
+        Pay
+      </button>
+    </form>
+  )
+}
+
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe(process.env.stripe_provider)
+
 const BillingAddPaymentForm = ({
-  isFormValid,
-  setIsFormValid,
-  cardDetails,
-  setCardDetails,
-  billingDetails,
-  setBillingDetails,
-  isLoading,
-  onSubmit,
-  className,
+  setSidePanelButton,
+  setSuccess,
 }) => {
   return (
-    <div
-      className={[
-        className,
-      ].join(' ')}
-    >
-      <form
-        onSubmit={(e) => {
-          e.preventDefault()
-          onSubmit()
-        }}
-      >
-        Form
-      </form>
-    </div>
+    <Elements stripe={stripePromise}>
+      {/* Defined above... */}
+      <FORM
+        setSidePanelButton={setSidePanelButton}
+        setSuccess={setSuccess}
+      />
+    </Elements>
   )
 }
 
 BillingAddPaymentForm.propTypes = {
-  isFormValid: PropTypes.bool.isRequired,
-  setIsFormValid: PropTypes.func.isRequired,
-  cardDetails: PropTypes.object.isRequired,
-  setCardDetails: PropTypes.func.isRequired,
-  billingDetails: PropTypes.object.isRequired,
-  setBillingDetails: PropTypes.func.isRequired,
-  isLoading: PropTypes.bool.isRequired,
-  onSubmit: PropTypes.func.isRequired,
-  className: PropTypes.string,
+  setSidePanelButton: PropTypes.func.isRequired,
+  setSuccess: PropTypes.func.isRequired,
 }
 
 BillingAddPaymentForm.defaultProps = {
-  className: null,
 }
 
 export default BillingAddPaymentForm
--- packages/app/components/BillingPaymentAdd.jsx
+++ packages/app/components/BillingPaymentAdd.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { loadStripe } from '@stripe/stripe-js'
-import { Elements } from '@stripe/react-stripe-js'
-
 import Button from '@/elements/Button'
 
 import BillingAddPaymentForm from '@/app/BillingAddPaymentForm'
@@ -14,51 +11,21 @@ import PaymentPageSuccess from '@/app/PaymentPageSuccess'
 import styles from '@/app/PaymentPage.module.css'
 import sidePanelStyles from '@/app/SidePanel.module.css'
 
-// Create the Stripe object yourself...
-const stripePromise = loadStripe(process.env.stripe_provider)
-
-// ADD PAYMENT FUNCTION
-const postPaymentMethod = async (paymentDetails) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(paymentDetails)
-    }, 500)
-  })
-}
 
 const BillingPaymentAdd = ({
   setSidePanelButton,
   toggleSidePanel,
 }) => {
+  // HANDLE SUCCESS
   const [success, setSuccess] = React.useState(false)
-  const [cardDetails, setCardDetails] = React.useState({})
-  const [billingDetails, setBillingDetails] = React.useState({})
 
-  // HANDLE FORM
-  const [isFormValid, setIsFormValid] = React.useState(false)
-  const [isLoading, setIsLoading] = React.useState(false)
-  const onSubmit = React.useCallback(async () => {
-    if (!isFormValid) return
-    const paymentDetails = { ...billingDetails, ...cardDetails }
-    setIsLoading(true)
-    const { res, error } = await postPaymentMethod(paymentDetails)
-    console.log('res', res)
-    setIsLoading(false)
-    if (!error) {
-      setSuccess(true)
-    }
-  }, [isFormValid, billingDetails, cardDetails])
-
-  // CHANGE SIDEPANEL BUTTON
+  // CHANGE SIDEPANEL BUTTON on SUCCESS
   React.useEffect(() => {
     if (success) {
-      const button = <Button version="green" onClick={() => toggleSidePanel(false)} />
+      const button = <Button version="green" onClick={() => toggleSidePanel(false)}>Done</Button>
       setSidePanelButton(button)
-      return
     }
-    const button = <Button version="green" disabled={!isFormValid} onClick={onSubmit} />
-    setSidePanelButton(button)
-  }, [success, isFormValid, onSubmit, setSidePanelButton, toggleSidePanel])
+  }, [success, setSidePanelButton, toggleSidePanel])
 
   return (
 
@@ -67,19 +34,11 @@ const BillingPaymentAdd = ({
       <h2 className={sidePanelStyles.SidePanel__Header}>Enter your card details</h2>
 
       <div>
-        {success ? <PaymentPageSuccess cardDetails={cardDetails} /> : (
-          <Elements stripe={stripePromise}>
-            <BillingAddPaymentForm
-              isFormValid={isFormValid}
-              setIsFormValid={setIsFormValid}
-              cardDetails={cardDetails}
-              setCardDetails={setCardDetails}
-              billingDetails={billingDetails}
-              setBillingDetails={setBillingDetails}
-              onSubmit={onSubmit}
-              isLoading={isLoading}
-            />
-          </Elements>
+        {success ? <PaymentPageSuccess /> : (
+          <BillingAddPaymentForm
+            setSidePanelButton={setSidePanelButton}
+            setSuccess={setSuccess}
+          />
         )}
       </div>
 
