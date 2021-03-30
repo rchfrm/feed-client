9dc333c9142e3000ba0eedb9c26d67dc6fc4394b
Benjamin E <ben@elwyn.co>
2021-03-30 16:01:49 +0100

    start payment card element

--- packages/app/components/BillingPaymentCard.jsx
+++ packages/app/components/BillingPaymentCard.jsx
@@ -1,21 +1,61 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+const formatDate = (int) => {
+  return int.toString().padStart(2, '0')
+}
+
 const BillingPaymentCard = ({
   card,
   billingDetails,
   isDefault,
   isButton,
+  isSelected,
   className,
 }) => {
+  const { brand, exp_month, exp_year: year, last4 } = card
+  const { name } = billingDetails
+  const month = formatDate(exp_month)
+  const ElWrapper = isButton ? 'button' : 'div'
   return (
-    <div
+    <ElWrapper
       className={[
+        'flex flex-column justify-between',
+        'max-w-md h-48',
+        'bg-grey-1 rounded-dialogue p-4',
+        `border-2 border-solid ${isSelected ? 'border-green' : 'border-grey-1'}`,
         className,
       ].join(' ')}
+      onClick={() => {
+        if (!isButton) return
+        console.log('SELECT')
+      }}
+      aria-label={isButton ? 'Select' : null}
     >
-      Card
-    </div>
+      <div className="flex justify-between items-center">
+        <p className="capitalize text-lg"><strong>{brand}</strong></p>
+      </div>
+      <div className="flex justify-between items-end">
+        {/* DETAILS */}
+        <div>
+          {/* NAME */}
+          <p className="mb-1">{name}</p>
+          {/* DATE */}
+          <p className="mb-0">
+            <span>Expires </span>
+            <span className="font-mono text-lg">{month}/{year}</span>
+          </p>
+          {/* NUMBER */}
+          <p className="mb-0 font-mono text-lg">
+            xxxx xxxx xxxx {last4}
+          </p>
+        </div>
+        {/* DEFAULT LABEL */}
+        {isDefault && (
+          <p className="text-green font-bold mb-0">Default</p>
+        )}
+      </div>
+    </ElWrapper>
   )
 }
 
@@ -24,12 +64,14 @@ BillingPaymentCard.propTypes = {
   billingDetails: PropTypes.object.isRequired,
   isDefault: PropTypes.bool,
   isButton: PropTypes.bool,
+  isSelected: PropTypes.bool,
   className: PropTypes.string,
 }
 
 BillingPaymentCard.defaultProps = {
   isDefault: false,
   isButton: false,
+  isSelected: false,
   className: null,
 }
 
