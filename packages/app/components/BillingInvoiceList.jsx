e3872555dad2946a913fe8a0acc0d6baf5742c46
Benjamin E <ben@elwyn.co>
2021-04-06 15:41:18 +0100

    tidy list

--- packages/app/components/BillingInvoiceList.jsx
+++ packages/app/components/BillingInvoiceList.jsx
@@ -5,43 +5,39 @@ import moment from 'moment'
 
 import LinkIcon from '@/icons/LinkIcon'
 
-const formatDate = (date) => {
-  return moment(date).format('DD MMM YYYY')
-}
+import sidePanelStyles from '@/app/SidePanel.module.css'
+
+const formatDate = (date) => moment(date).format('DD MMM YYYY')
 
 const BillingInvoiceList = ({
   invoices,
-  className,
 }) => {
   return (
-    <ul
-      className={[
-        className,
-      ].join(' ')}
-    >
-      {invoices.map(({ date, link }) => {
-        return (
-          <li key={date} className="flex mb-4 last:mb-4">
-            <a href={link} className="flex" target="_blank" rel="noreferrer noopener">
-              <LinkIcon className="h-4 w-auto" />
-              <p className="mb-0">Invoice</p>
-              <p className="mb-0">{formatDate(date)}</p>
-            </a>
-          </li>
-        )
-      })}
-    </ul>
+    <div>
+      <h2 className={sidePanelStyles.SidePanel__Header}>Invoices</h2>
+      <ul>
+        {invoices.map(({ date, link }) => {
+          return (
+            <li key={date} className="flex mb-4 last:mb-4">
+              <a href={link} className="flex" target="_blank" rel="noreferrer noopener">
+                <LinkIcon className="h-4 w-auto" />
+                <p className="mb-0">Invoice</p>
+                <p className="mb-0">{formatDate(date)}</p>
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
   )
 }
 
 BillingInvoiceList.propTypes = {
   invoices: PropTypes.array,
-  className: PropTypes.string,
 }
 
 BillingInvoiceList.defaultProps = {
   invoices: [],
-  className: null,
 }
 
 export default React.memo(BillingInvoiceList)
