5ed9b69e2980e6c23ed647aa253592bd23f3ca2e
Benjamin E <ben@elwyn.co>
2021-04-07 13:27:40 +0100

    start current invoice summary

--- packages/app/components/BillingInvoiceSummary.jsx
+++ packages/app/components/BillingInvoiceSummary.jsx
@@ -6,8 +6,8 @@ const BillingInvoiceSummary = ({
   className,
 }) => {
   console.log('nextInvoice', nextInvoice)
-  const { date } = nextInvoice
-  const header = `Next invoice: ${date}`
+  const { date, invoiceSections = [], totalFee } = nextInvoice || {}
+  const header = nextInvoice ? `Next invoice: ${date}` : 'No upcoming invoice'
   return (
     <div
       className={[
@@ -15,16 +15,43 @@ const BillingInvoiceSummary = ({
       ].join(' ')}
     >
       <h3 className="font-body font-bold mb-6">{header}</h3>
+      {/* INVOICE SECTIONS */}
+      {invoiceSections.map((section, index) => {
+        return (
+          <ul key={index} className="bg-grey-1 rounded-dialogue p-5 mb-5">
+            {section.map(({ slug, title, value }) => {
+              return (
+                <li key={slug} className="flex justify-between mb-4 last:mb-0">
+                  <span>{title}</span>
+                  <strong>{value}</strong>
+                </li>
+              )
+            })}
+          </ul>
+        )
+      })}
+      {/* TOTAL */}
+      <p
+        className={[
+          'text-lg',
+          'flex justify-between mb-0 py-3 px-5',
+          'border-solid border-2 border-green rounded-dialogue',
+        ].join(' ')}
+      >
+        <span>TOTAL FEE</span>
+        <strong>{totalFee}</strong>
+      </p>
     </div>
   )
 }
 
 BillingInvoiceSummary.propTypes = {
-  nextInvoice: PropTypes.object.isRequired,
+  nextInvoice: PropTypes.object,
   className: PropTypes.string,
 }
 
 BillingInvoiceSummary.defaultProps = {
+  nextInvoice: null,
   className: null,
 }
 
