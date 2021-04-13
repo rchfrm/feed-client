1861d8b859d6a3bf6d23747b5fc84766b30f0a2c
Benjamin E <ben@elwyn.co>
2021-04-13 11:15:31 +0100

    update copy to dummy

--- packages/app/copy/AccountPageCopy.js
+++ packages/app/copy/AccountPageCopy.js
@@ -19,5 +19,5 @@ When you enter a link, make sure it starts with "https://".
 
   More details on Feed’s pricing is [here](${ROUTES.PRICING}).`,
 
-  changeEmailAlert: `After changing your email you will be logged out of your account.`,
+  changeEmailAlert: `After changing your email you will be asked to verify it [etc etc etc etc etc etc etc etc].`,
 }
