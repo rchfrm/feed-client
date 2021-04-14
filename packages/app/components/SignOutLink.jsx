710e5aa21676198e5c4f5576c9e60170b56b5091
Benjamin E <ben@elwyn.co>
2021-04-14 16:59:19 +0100

    use useSignOut hook in signout link component

--- packages/app/components/SignOutLink.jsx
+++ packages/app/components/SignOutLink.jsx
@@ -2,59 +2,25 @@
 
 // IMPORT PACKAGES
 import React from 'react'
-import Router from 'next/router'
+import PropTypes from 'prop-types'
 
-// IMPORT CONTEXTS
-import { AuthContext } from '@/contexts/AuthContext'
-import { UserContext } from '@/app/contexts/UserContext'
-import { ArtistContext } from '@/app/contexts/ArtistContext'
-import { InterfaceContext } from '@/contexts/InterfaceContext'
+import useSignOut from '@/hooks/useSignOut'
 
-// IMPORT CONSTANTS
-import * as ROUTES from '@/app/constants/routes'
-// IMPORT HELPERS
-import * as firebaseHelpers from '@/helpers/firebaseHelpers'
-import { mixpanelSignOut } from '@/app/helpers/mixpanelHelpers'
-
-function SignOutLink({ className = '' }) {
-  const { setNoAuth, clearRejectedPathPath } = React.useContext(AuthContext)
-  const { setNoUser } = React.useContext(UserContext)
-  const { setNoArtist } = React.useContext(ArtistContext)
-  const { toggleGlobalLoading } = React.useContext(InterfaceContext)
-
-  const clearContexts = React.useRef(null)
-
-  const signOut = async () => {
-    toggleGlobalLoading(true)
-    Router.events.on('routeChangeComplete', clearContexts.current)
-    await firebaseHelpers.doSignOut()
-      .catch((err) => {
-        toggleGlobalLoading(false)
-        throw (err)
-      })
-    Router.push(ROUTES.LOGIN)
-  }
-
-  const signoutCallback = () => {
-    Router.events.off('routeChangeComplete', clearContexts.current)
-    clearRejectedPathPath()
-    mixpanelSignOut()
-    setNoAuth()
-    setNoUser()
-    setNoArtist()
-    toggleGlobalLoading(false)
-  }
-
-  React.useEffect(() => {
-    clearContexts.current = signoutCallback
-  // eslint-disable-next-line
-  }, [])
-
+const SignOutLink = ({ className }) => {
+  const signOut = useSignOut()
   return (
     <a className={className} role="button" version="sign-out" onClick={signOut}>
       sign out
     </a>
   )
+}
+
+SignOutLink.propTypes = {
+  className: PropTypes.string,
+}
+
+SignOutLink.defaultProps = {
+  className: null,
 }
 
 export default SignOutLink
