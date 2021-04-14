c24e631caccee80cc6150e458f0efc5dd7191fd9
Benjamin E <ben@elwyn.co>
2021-04-14 16:59:37 +0100

    create copy for email verify success page

--- packages/app/copy/signupCopy.js
+++ packages/app/copy/signupCopy.js
@@ -10,4 +10,10 @@ export default {
   missingFbEmail: `There isn't an email address associated with your Facebook account, and we need a way to keep you updated on Feed's performance. Enter one below.`,
 
   emailVerify: (email) => `Please confirm your email address **${email}** by clicking on the link in the email we sent you.`,
+
+  emailVerifySuccess: {
+    success: (email) => `Success! Your email **${email}** has been verified!`,
+    logoutExplainer: `You will need to log out and sign in with the email you just verified.`,
+    button: `Sign out and continue`,
+  },
 }
