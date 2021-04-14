f9c60e396e7a4a02df9ea863864d4b2fab46e70e
Benjamin E <ben@elwyn.co>
2021-04-14 16:59:04 +0100

    create useSignOut hook

---
+++ packages/shared/components/hooks/useSignOut.js
@@ -0,0 +1,54 @@
+// IMPORT PACKAGES
+import React from 'react'
+import Router from 'next/router'
+
+// IMPORT CONTEXTS
+import { AuthContext } from '@/contexts/AuthContext'
+import { UserContext } from '@/contexts/UserContext'
+import { ArtistContext } from '@/contexts/ArtistContext'
+import { InterfaceContext } from '@/contexts/InterfaceContext'
+
+// IMPORT CONSTANTS
+import * as ROUTES from '@/app/constants/routes'
+// IMPORT HELPERS
+import * as firebaseHelpers from '@/helpers/firebaseHelpers'
+import { mixpanelSignOut } from '@/app/helpers/mixpanelHelpers'
+
+const useSignOut = () => {
+  const { setNoAuth, clearRejectedPathPath } = React.useContext(AuthContext)
+  const { setNoUser } = React.useContext(UserContext)
+  const { setNoArtist } = React.useContext(ArtistContext)
+  const { toggleGlobalLoading } = React.useContext(InterfaceContext)
+
+  const clearContexts = React.useRef(null)
+
+  const signOut = React.useCallback(async () => {
+    toggleGlobalLoading(true)
+    Router.events.on('routeChangeComplete', clearContexts.current)
+    await firebaseHelpers.doSignOut()
+      .catch((err) => {
+        toggleGlobalLoading(false)
+        throw (err)
+      })
+    Router.push(ROUTES.LOGIN)
+  }, [toggleGlobalLoading])
+
+  const signoutCallback = () => {
+    Router.events.off('routeChangeComplete', clearContexts.current)
+    clearRejectedPathPath()
+    mixpanelSignOut()
+    setNoAuth()
+    setNoUser()
+    setNoArtist()
+    toggleGlobalLoading(false)
+  }
+
+  React.useEffect(() => {
+    clearContexts.current = signoutCallback
+  // eslint-disable-next-line
+  }, [])
+
+  return signOut
+}
+
+export default useSignOut
