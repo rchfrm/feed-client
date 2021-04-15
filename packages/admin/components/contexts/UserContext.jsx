2e85ef0a0324fe30af3690b611c6a93b5d2ac579
Benjamin E <ben@elwyn.co>
2021-04-15 12:50:21 +0100

    simplify admin UserContext

--- packages/admin/components/contexts/UserContext.jsx
+++ packages/admin/components/contexts/UserContext.jsx
@@ -1,19 +1,11 @@
+// * ADMIN VERSION
+
 import React from 'react'
 import { useImmerReducer } from 'use-immer'
 
-import useReferralStore from '@/app/stores/referralStore'
-import useNotificationStore from '@/app/stores/notificationsStore'
 // IMPORT HELPERS
 import * as sharedServer from '@/helpers/sharedServer'
-import * as appServer from '@/app/helpers/appServer'
-import { sortUserArtists } from '@/app/helpers/userHelpers'
-import { updateTracking } from '@/app/helpers/trackingHelpers'
 import { fireSentryError } from '@/app/helpers/sentryHelpers'
-
-
-// Read from referralStore
-const getGetStoredReferrerCode = state => state.getStoredReferrerCode
-const getSetArtistsWithNotifications = state => state.setArtistsWithNotifications
 
 const initialUserState = {
   id: '',
@@ -65,46 +57,6 @@ function UserProvider({ children }) {
     setUserLoading(false)
   }, [setUser])
 
-  // Get Getter to read reffere code from store
-  const getStoredReferrerCode = useReferralStore(getGetStoredReferrerCode)
-
-  const runCreateUser = React.useCallback(async ({ firstName, lastName, email }) => {
-    setUserLoading(true)
-    // Get referrer code (from local storage)
-    const referrerCode = getStoredReferrerCode()
-    // Create user in DB
-    const { res: newUser, error: errorCreatingUser } = await sharedServer.createUser({
-      firstName,
-      lastName,
-      ...(email && { email }),
-      referrerCode,
-    })
-    // Handle error creating user
-    if (errorCreatingUser) {
-      setUserLoading(false)
-      return { error: errorCreatingUser }
-    }
-    // Accept T&Cs
-    const { error: errorAcceptingTerms } = await appServer.acceptTerms(newUser.id)
-    // Handle error accepting terms
-    if (errorAcceptingTerms) {
-      setUserLoading(false)
-      return { error: errorCreatingUser }
-    }
-    // Sort artists
-    const sortedArtistUser = sortUserArtists(newUser)
-    setUser({
-      type: 'set-user',
-      payload: {
-        user: sortedArtistUser,
-      },
-    })
-    setUserLoading(false)
-    return { res: newUser }
-  }, [setUser, getStoredReferrerCode])
-
-  const setArtistsWithNotifications = useNotificationStore(getSetArtistsWithNotifications)
-
   const storeUser = React.useCallback(async () => {
     setUserLoading(true)
     const { res: user, error } = await sharedServer.getUser()
@@ -118,52 +70,28 @@ function UserProvider({ children }) {
       setUserLoading(false)
       return { error }
     }
-    // TODO If 404, then call /accounts/register
-    const sortedArtistUser = sortUserArtists(user)
-    // Store artists with notifications in Not store
-    setArtistsWithNotifications(Object.values(sortedArtistUser.artists))
-    // Update user type in track helpers
-    updateTracking(user)
     // Update user state
     setUser({
       type: 'set-user',
       payload: {
-        user: sortedArtistUser,
+        user,
       },
     })
     setUserLoading(false)
-    return { user: sortedArtistUser }
-  }, [setUser, setArtistsWithNotifications])
+    return { user }
+  }, [setUser])
 
   const updateUser = React.useCallback((user) => {
-    const sortedArtistUser = sortUserArtists(user)
     setUser({
       type: 'set-user-details',
       payload: {
-        user: sortedArtistUser,
+        user,
       },
     })
-    return sortedArtistUser
+    return user
   }, [setUser])
 
-  // STORE IF PENDING EMAIL
-  const [hasPendingEmail, setHasPendingEmail] = React.useState(false)
-  const testForPendingEmail = React.useCallback((user) => {
-    const {
-      contact_email: contactEmail,
-      pending_email: pendingEmail,
-      pending_contact_email: pendingContactEmail,
-      email_verified: emailVerified,
-      contact_email_verified: contactEmailVerified,
-    } = user
-    return !!(pendingEmail || !emailVerified || pendingContactEmail || (!contactEmailVerified && contactEmail))
-  }, [])
-  React.useEffect(() => {
-    setHasPendingEmail(testForPendingEmail(user))
-  }, [user, testForPendingEmail])
-
   const value = {
-    runCreateUser,
     setNoUser,
     setUser,
     setUserError,
@@ -173,8 +101,6 @@ function UserProvider({ children }) {
     userError,
     userLoading,
     setUserLoading,
-    testForPendingEmail,
-    hasPendingEmail,
   }
 
   return (
