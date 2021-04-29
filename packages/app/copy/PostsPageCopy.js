cf73c2883aaab9cf9c0ad204e6a4683b817eeaf5
Benjamin E <ben@elwyn.co>
2021-04-29 16:03:35 +0100

    update copy

--- packages/app/copy/PostsPageCopy.js
+++ packages/app/copy/PostsPageCopy.js
@@ -125,7 +125,7 @@ By default, Feed won’t promote posts older than 28 days unless you opt them in
 
   // SETTINGS SIDEPANEL
   postLinkSetting: 'Which link should be used when this post is made into an ad?',
-  editCaption: `Edit the caption that's used on the ad.`,
+  editCaption: `Edit the caption used in this ad.`,
   confirmEditCaption: `Editing the caption will cause the ad to go back into review.
   
 Are you sure you want to continue?`,
