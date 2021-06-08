d5a6a7b6fefc80d6c2f46f731cf7b4d2644bc6c6
Dennis Dijkstra <mail@dennisdijkstra.com>
2021-06-08 19:14:34 +0200

    FD-567 Adjust control settings order and copy

--- packages/app/copy/controlsPageCopy.js
+++ packages/app/copy/controlsPageCopy.js
@@ -4,34 +4,34 @@ export default {
   controlsOptions: [
     {
       key: 'targeting',
-      title: 'Targeting Settings',
+      title: 'Targeting',
       description: 'Control who sees your ads.',
       icon: 'crosshair',
     },
     {
-      key: 'adSettings',
-      title: 'Ad Settings',
-      description: 'Default settings for Grow & Nurture ads.',
-      icon: 'megaphone',
-    },
-    {
-      key: 'conversionsSettings',
-      title: 'Conversion Ad Settings',
-      description: 'Control conversion ad settings.',
-      icon: 'currency',
-      hidden: true,
-    },
-    {
       key: 'linkBank',
-      title: 'Link Bank',
-      description: 'Add and edit the links used in your ads.',
+      title: 'Links',
+      description: 'Add and edit the links that are used in your ads.',
       icon: 'link',
     },
     {
       key: 'integrations',
       title: 'Integrations',
-      description: 'Connect social & streaming platforms to Feed.',
+      description: 'Connect Feed to other platforms.',
       icon: 'jigsaw',
+    },
+    {
+      key: 'adSettings',
+      title: 'Ad Defaults',
+      description: 'Control the global settings of your ads.',
+      icon: 'megaphone',
+    },
+    {
+      key: 'conversionsSettings',
+      title: 'Conversion Defaults',
+      description: 'Settings specific to conversion campaigns.',
+      icon: 'currency',
+      hidden: false,
     },
   ],
 }
