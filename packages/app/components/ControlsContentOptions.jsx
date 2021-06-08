8d9dc1c9dba06eff8bb1230a3d3f3b26dec341e9
Dennis Dijkstra <mail@dennisdijkstra.com>
2021-06-08 19:20:31 +0200

    FD-567 Remove icons

--- packages/app/components/ControlsContentOptions.jsx
+++ packages/app/components/ControlsContentOptions.jsx
@@ -1,22 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import LinkIcon from '@/icons/LinkIcon'
-import CrosshairIcon from '@/icons/CrosshairIcon'
-import JigsawIcon from '@/icons/JigsawIcon'
-import MegaphoneIcon from '@/icons/MegaphoneIcon'
-
-import brandColors from '@/constants/brandColors'
 import copy from '@/app/copy/controlsPageCopy'
 
 const { controlsOptions } = copy
-
-const icons = {
-  link: <LinkIcon fill={brandColors.blue} className="w-6 h-auto" />,
-  crosshair: <CrosshairIcon fill={brandColors.blue} className="w-6 h-auto" />,
-  jigsaw: <JigsawIcon fill={brandColors.blue} className="w-6 h-auto" />,
-  megaphone: <MegaphoneIcon fill={brandColors.blue} className="w-6 h-auto" />,
-}
 
 const ControlsContentOptions = ({ className }) => {
   const [activeOptionKey, setActiveOptionKey] = React.useState(controlsOptions[0].key)
@@ -29,9 +16,9 @@ const ControlsContentOptions = ({ className }) => {
     >
       {controlsOptions.map((option) => {
         if (option.hidden) return null
-        const { key, title, description, icon: iconSlug } = option
-        const Icon = icons[iconSlug] || null
+        const { key, title, description } = option
         const isActive = key === activeOptionKey
+        console.log(isActive)
         return (
           <a
             key={key}
@@ -43,14 +30,6 @@ const ControlsContentOptions = ({ className }) => {
             ].join(' ')}
             onClick={() => setActiveOptionKey(key)}
           >
-            {/* ICON */}
-            <div className="mr-5" style={{ opacity: isActive ? 1 : 1 }}>
-              <div
-                // className="w-8 h-8 rounded-full flex items-center justify-center"
-              >
-                {Icon}
-              </div>
-            </div>
             {/* TITLE */}
             <div>
               <p className="font-bold mb-2">{title}</p>
