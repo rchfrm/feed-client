105b2bb095f1911951c18db5a5a4c35068a95ad2
Dennis Dijkstra <mail@dennisdijkstra.com>
2021-06-14 10:03:04 +0200

    FD-568 Render all steps within the wizard context provider and pass steps array to it

--- packages/app/components/ConversionsWizard.jsx
+++ packages/app/components/ConversionsWizard.jsx
@@ -1,24 +1,59 @@
 import React from 'react'
 
-import Button from '@/elements/Button'
-import ArrowAltIcon from '@/icons/ArrowAltIcon'
+import { WizardContextProvider } from '@/app/contexts/WizardContext'
 
-import brandColors from '@/constants/brandColors'
+import ConversionsWizardStartingStep from '@/app/ConversionsWizardStartingStep'
+import ConversionsWizardBudgetStep from '@/app/ConversionsWizardBudgetStep'
+import ConversionsWizardLinkStep from '@/app/ConversionsWizardLinkStep'
+import ConversionsWizardFacebookPixelStep from '@/app/ConversionsWizardFacebookPixelStep'
+import ConversionsWizardFacebookPixelEventStep from '@/app/ConversionsWizardFacebookPixelEventStep'
+import ConversionsWizardCallToActionStep from '@/app/ConversionsWizardCallToActionStep'
+import ConversionsWizardPostOptInStep from '@/app/ConversionsWizardPostOptInStep'
+
+const steps = [
+  {
+    id: 0,
+    shouldSkip: false,
+  },
+  {
+    id: 1,
+    shouldSkip: false,
+  },
+  {
+    id: 2,
+    shouldSkip: false,
+  },
+  {
+    id: 3,
+    shouldSkip: false,
+  },
+  {
+    id: 4,
+    shouldSkip: false,
+  },
+  {
+    id: 5,
+    shouldSkip: false,
+  },
+  {
+    id: 6,
+    shouldSkip: false,
+  },
+]
 
 const ConversionsWizard = () => {
   return (
-    <Button
-      version="green icon"
-      onClick={() => console.log('Start running conversions')}
-      className="w-full"
-    >
-      Start Running Conversions
-      <ArrowAltIcon
-        className="ml-3"
-        fill={brandColors.white}
-        direction="right"
-      />
-    </Button>
+    <div>
+      <WizardContextProvider steps={steps}>
+        <ConversionsWizardStartingStep />
+        <ConversionsWizardBudgetStep />
+        <ConversionsWizardLinkStep />
+        <ConversionsWizardFacebookPixelStep />
+        <ConversionsWizardFacebookPixelEventStep />
+        <ConversionsWizardCallToActionStep />
+        <ConversionsWizardPostOptInStep />
+      </WizardContextProvider>
+    </div>
   )
 }
 
