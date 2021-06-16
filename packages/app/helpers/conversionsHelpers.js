7ee48533e52a5aa5a9334b3a4bbdbc934b8a8b37
Dennis Dijkstra <mail@dennisdijkstra.com>
2021-06-16 10:50:17 +0200

    FD-568 Define api request functions in conversionsHelpers file

---
+++ packages/app/helpers/conversionsHelpers.js
@@ -0,0 +1,51 @@
+import * as api from '@/helpers/api'
+
+export const getFacebookPixelEvents = async () => {
+  const endpoint = '/preferences/facebook_pixel_events'
+  const payload = {}
+  const errorTracking = {
+    category: 'Conversions',
+    action: 'Get Facebook Pixel Events',
+  }
+  return api.requestWithCatch('get', endpoint, payload, errorTracking)
+}
+
+export const updateFacebookPixelEvent = async (artistId, event) => {
+  const endpoint = `/artists/${artistId}`
+  const payload = { preferences: { conversions: { facebook_pixel_event: event } } }
+  const errorTracking = {
+    category: 'Conversions',
+    action: 'Save Facebook Pixel Event',
+  }
+  return api.requestWithCatch('patch', endpoint, payload, errorTracking)
+}
+
+export const getCallToActions = async () => {
+  const endpoint = '/preferences/call_to_actions'
+  const payload = {}
+  const errorTracking = {
+    category: 'Conversions',
+    action: 'Get Call to Actions',
+  }
+  return api.requestWithCatch('get', endpoint, payload, errorTracking)
+}
+
+export const updateCallToAction = async (artistId, callToAction) => {
+  const endpoint = `/artists/${artistId}`
+  const payload = { preferences: { conversions: { call_to_action: callToAction } } }
+  const errorTracking = {
+    category: 'Conversions',
+    action: 'Save Call to Action',
+  }
+  return api.requestWithCatch('patch', endpoint, payload, errorTracking)
+}
+
+export const enableConversions = async (artistId) => {
+  const endpoint = `/artists/${artistId}`
+  const payload = { conversions_enabled: true }
+  const errorTracking = {
+    category: 'Conversions',
+    action: 'Enable conversions',
+  }
+  return api.requestWithCatch('patch', endpoint, payload, errorTracking)
+}
