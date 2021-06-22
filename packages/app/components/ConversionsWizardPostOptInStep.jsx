550b213583acf4b122c93f0c48eab7bdb9f90df4
Dennis Dijkstra <mail@dennisdijkstra.com>
2021-06-22 22:34:20 +0200

    FD-608 Remove redundant request on post optin wizard step

--- packages/app/components/ConversionsWizardPostOptInStep.jsx
+++ packages/app/components/ConversionsWizardPostOptInStep.jsx
@@ -3,35 +3,18 @@ import Router from 'next/router'
 
 import * as ROUTES from '@/app/constants/routes'
 
-import { enableConversions } from '@/app/helpers/conversionsHelpers'
-
 import Button from '@/elements/Button'
-import Error from '@/elements/Error'
 import MarkdownText from '@/elements/MarkdownText'
 
 import ArrowAltIcon from '@/icons/ArrowAltIcon'
-
-import { ArtistContext } from '@/app/contexts/ArtistContext'
 
 import copy from '@/app/copy/controlsPageCopy'
 
 import brandColors from '@/constants/brandColors'
 
 const ConversionsWizardOptInStep = () => {
-  const [isLoading, setIsLoading] = React.useState(false)
-  const [error, setError] = React.useState(null)
-  const { artist: { id: artistId } } = React.useContext(ArtistContext)
-
-  // Handle API request and navigate to the posts page
-  const updateConversions = async () => {
-    setIsLoading(true)
-    const { res, error } = await enableConversions(artistId)
-    setIsLoading(false)
-
-    if (error) {
-      setError({ message: error.message })
-      return
-    }
+  // Navigate to the posts page
+  const goToPostsPage = async () => {
     Router.push({
       pathname: ROUTES.HOME,
       query: { postStatus: 'not-run' },
@@ -42,11 +25,9 @@ const ConversionsWizardOptInStep = () => {
     <>
       <h2>Get going</h2>
       <MarkdownText markdown={copy.postOptInStepDescription} />
-      <Error error={error} />
       <Button
         version="green icon"
-        onClick={updateConversions}
-        loading={isLoading}
+        onClick={goToPostsPage}
         className="w-full"
       >
         Opt in posts
