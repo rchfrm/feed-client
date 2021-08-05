cf3e11b6f1a3f64a4178a26e6aa4bd4b4492c7bd
Dennis Dijkstra <mail@dennisdijkstra.com>
2021-08-05 13:56:12 +0200

    FD-658 Add optional navigation buttons, title and progressbar to wizard context component

--- packages/app/components/contexts/WizardContext.jsx
+++ packages/app/components/contexts/WizardContext.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import ProgressBar from '@/app/ProgressBar'
+
+import ArrowAltIcon from '@/icons/ArrowAltIcon'
+
+import brandColors from '@/constants/brandColors'
+
 const initialContext = {
   next: () => {},
   back: () => {},
@@ -10,10 +16,12 @@ const initialContext = {
 
 const WizardContext = React.createContext(initialContext)
 
-const WizardContextProvider = ({ steps, children }) => {
+const WizardContextProvider = ({ steps, children, hasNavigation }) => {
   const [currentStep, setCurrentStep] = React.useState(0)
   const totalSteps = steps.length - 1
   const shouldSkipStep = steps[currentStep].shouldSkip
+  const isFirstStep = currentStep === 0
+  const isLastStep = currentStep === steps.length - 1
 
   const next = React.useCallback(() => {
     if (currentStep === totalSteps) return
@@ -38,7 +46,37 @@ const WizardContextProvider = ({ steps, children }) => {
         setCurrentStep,
       }}
     >
+      <h1>{steps[currentStep].title}</h1>
+      <ProgressBar percentage={((currentStep + 1) / (totalSteps + 1)) * 100} className="mb-6" />
       {!shouldSkipStep && children[currentStep]}
+      {(hasNavigation && !isFirstStep && !isLastStep) ? (
+        <div className="w-full flex justify-between mt-auto">
+          <a
+            role="button"
+            onClick={back}
+            className="flex text-grey-2 no-underline"
+          >
+            <ArrowAltIcon
+              className="w-3 mr-3"
+              direction="left"
+              fill={brandColors.grey}
+            />
+            Back
+          </a>
+          <a
+            role="button"
+            onClick={next}
+            className="flex text-grey-2 no-underline"
+          >
+            Skip
+            <ArrowAltIcon
+              className="w-3 ml-3"
+              direction="right"
+              fill={brandColors.grey}
+            />
+          </a>
+        </div>
+      ) : null}
     </WizardContext.Provider>
   )
 }
@@ -50,6 +88,11 @@ WizardContextProvider.propTypes = {
       shouldSkip: PropTypes.bool.isRequired,
     }),
   ).isRequired,
+  hasNavigation: PropTypes.bool,
+}
+
+WizardContextProvider.defaultProps = {
+  hasNavigation: false,
 }
 
 export { WizardContext, WizardContextProvider }
