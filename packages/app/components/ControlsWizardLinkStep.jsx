43396394bdc2323e12aca17b53a00798a8da7f3b
Dennis Dijkstra <mail@dennisdijkstra.com>
2021-08-06 18:07:58 +0200

    FD-658 Save link and set as default

--- packages/app/components/ControlsWizardLinkStep.jsx
+++ packages/app/components/ControlsWizardLinkStep.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 // import PropTypes from 'prop-types'
 
 import { WizardContext } from '@/app/contexts/WizardContext'
+import { ArtistContext } from '@/app/contexts/ArtistContext'
+import useControlsStore from '@/app/stores/controlsStore'
+
+import { saveLink, setDefaultLink } from '@/app/helpers/linksHelpers'
 
 import Button from '@/elements/Button'
 import MarkdownText from '@/elements/MarkdownText'
 import Input from '@/elements/Input'
+import Error from '@/elements/Error'
 
 import ArrowAltIcon from '@/icons/ArrowAltIcon'
 
@@ -13,16 +18,57 @@ import brandColors from '@/constants/brandColors'
 
 import copy from '@/app/copy/controlsPageCopy'
 
+const getControlsStoreState = (state) => ({
+  artistId: state.artistId,
+  savedFolders: state.savedFolders,
+  updateControlsStore: state.updateControlsStore,
+  updatePreferences: state.updatePreferences,
+})
+
 const ControlsWizardLinkStep = ({ setIsWizardActive }) => {
-  const [defaultLink, setDefaultLink] = React.useState('')
+  const [link, setLink] = React.useState({ name: 'Default link', href: '' })
+  const [error, setError] = React.useState('')
   const { next } = React.useContext(WizardContext)
+  const { artistId, setPostPreferences } = React.useContext(ArtistContext)
+  const { savedFolders, updateControlsStore, updatePreferences } = useControlsStore(getControlsStoreState)
 
   React.useEffect(() => {
     setIsWizardActive(true)
   }, [setIsWizardActive])
 
   const handleChange = (e) => {
-    setDefaultLink(e.target.value)
+    setLink({ ...link, href: e.target.value })
+  }
+
+  const save = async () => {
+    const { res: savedLink, error } = await saveLink(artistId, link, savedFolders)
+    if (error) {
+      const saveLinkError = `Error saving link: ${error.message}`
+      setError({ message: saveLinkError })
+      return
+    }
+    if (savedLink) {
+      const { res: newArtist, error } = await setDefaultLink(artistId, savedLink.id)
+      if (error) {
+        const setDefaultLinkError = `Error setting link as default: ${error.message}`
+        setError({ message: setDefaultLinkError })
+        return
+      }
+      // Update controls store
+      const { preferences: { posts: { default_link_id } } } = newArtist
+      // Add the new link to the controls store
+      updateControlsStore('add', { newLink: savedLink })
+      // Set the new link as the default link
+      updateControlsStore('chooseNewDefaultLink', { newArtist, newLink: savedLink })
+      // Update the post preferences object
+      updatePreferences(
+        'postsPreferences',
+        { defaultLinkId: default_link_id },
+      )
+      // Update artist status
+      setPostPreferences('default_link_id', default_link_id)
+    }
+    next()
   }
 
   return (
@@ -33,12 +79,13 @@ const ControlsWizardLinkStep = ({ setIsWizardActive }) => {
         type="url"
         version="box"
         name="link-url"
-        value={defaultLink}
+        value={link.href}
         handleChange={handleChange}
       />
+      <Error error={error} />
       <Button
         version="green icon"
-        onClick={next}
+        onClick={save}
         className="w-full mb-6"
       >
         Next
