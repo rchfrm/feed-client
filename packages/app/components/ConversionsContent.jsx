382229671bd6f3a1cd394458163ba92326451718
Dennis Dijkstra <mail@dennisdijkstra.com>
2021-08-22 21:11:38 +0200

    FD-668 Redirect to controls page if conversions feature isnt enabled

--- packages/app/components/ConversionsContent.jsx
+++ packages/app/components/ConversionsContent.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
 
+import Router from 'next/router'
+
 import useControlsStore from '@/app/stores/controlsStore'
 
 import ConversionsSettings from '@/app/ConversionsSettings'
 import ConversionsWizard from '@/app/ConversionsWizard'
+
+import { ArtistContext } from '@/app/contexts/ArtistContext'
+
+import * as ROUTES from '@/app/constants/routes'
 
 const getConversionsPreferences = state => state.conversionsPreferences
 
 const ConversionsContent = () => {
   const [isWizardActive, setIsWizardActive] = React.useState(false)
   const conversionsPreferences = useControlsStore(getConversionsPreferences)
+  const { featureFlags: { conversionsEnabled: conversionsFeatureEnabled } } = React.useContext(ArtistContext)
+
+  // Redirect to the controls page if conversion feature isn't enabled
+  React.useEffect(() => {
+    if (!conversionsFeatureEnabled) {
+      Router.push(ROUTES.CONTROLS)
+    }
+  }, [conversionsFeatureEnabled])
+
   // Check if conversions link, pixel event and call to action are all set
   const hasSetUpConversions = Object.values(conversionsPreferences).every(Boolean)
   return (
