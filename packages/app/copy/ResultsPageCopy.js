fdc5e140007168fe58d0e78acd0487ac194fa997
Dennis Dijkstra <mail@dennisdijkstra.com>
2021-08-25 16:05:45 +0200

    FD-715 Remove copy

--- packages/app/copy/ResultsPageCopy.js
+++ packages/app/copy/ResultsPageCopy.js
@@ -1,30 +1,3 @@
 /* eslint-disable quotes */
-import * as ROUTES from '@/app/constants/routes'
 
-export default {
-  intro: `Feed automatically keeps your strongest promoted posts running, and turns off those that don’t do as well. To manually switch off a post just hit the red "X".`,
-
-  noResultsWithBudget: `**Feed** is setting up your posts for promotion.
-  
-There may be a delay whilst posts await approval, once promotions have started you'll be able to see your results below.`,
-
-  noResultsNoBudget: `**Feed** hasn't started promoting your posts yet, get started by entering a daily budget [here](${ROUTES.CONTROLS}).`,
-
-  noActiveWithBudget: `### **Feed** is setting up your posts for promotion.
-  
-There may be a delay whilst posts await approval, once promotions have started you'll be able to see your results here.
-  `,
-
-  noActiveNoBudget: `### **Feed** isn't currently promoting any of your posts, [set a daily budget](${ROUTES.CONTROLS}) to get it started`,
-
-  resultToggleWarning: (promotion_enabled) => {
-    if (promotion_enabled) {
-      return `# Are you sure?
-    
-Just to double check, are you sure you want this post to stop being promoted?`
-    }
-    return `# Are you sure?
-
-Clicking 'Yes' below will mean **Feed** starts to promote the post again.`
-  },
-}
+export default {}
--- packages/app/copy/funnelCopy.js
+++ packages/app/copy/funnelCopy.js
@@ -1,7 +1,8 @@
 /* eslint-disable quotes */
 import * as ROUTES from '@/app/constants/routes'
 
-const audienceProps = {
+export default {
+  // AUDIENCE TEXT
   growA: {
     title: 'Grow A',
     tooltip: ``,
@@ -35,30 +36,4 @@ const audienceProps = {
     title: 'hot',
     description: 'Coming soon bro',
   },
-}
-
-export default {
-  // FUNNEL VIZ INTRO TEXT
-  funnelVizIntroText: `Review which ads are currently running to different audience types and see which are performing best.`,
-  // NO ACTIVE ADS TEXT
-  noActiveAds: (postsType) => `**You have no ads running.** Unpause Feed and set a budget from the [Controls page](${ROUTES.CONTROLS}) and opt in some ${postsType} on the [posts page](${ROUTES.POSTS}?postStatus=not-run) to get started.`,
-  // AUDIENCE TEXT
-  ...audienceProps,
-  // NEED HELP TEXT
-  needHelp: `Feed helps you find new audiences, keep your whole following engaged and ultimately grow income from your biggest supporters.
-
-This page shows what ads are currently running and to which audiences. There is a description of each audience below. 
-
-### ${audienceProps.growA.title}
-  
-People who haven’t heard of you yet, but are likely to engage. Feed grows an [audience of people who have engaged with you](${ROUTES.INSIGHTS}?dataset=instagram_engaged_1y), and over time via Nurture turns those people into followers and drives deeper engagement.
-
-### ${audienceProps.growB.title}
-
-People who haven’t heard about you before, but are likely to visit your website or an external channel/streaming profile. People who do this automatically fall into your [engaged audience](${ROUTES.INSIGHTS}?dataset=instagram_engaged_1y).
-
-### ${audienceProps.nurture.title}
-
-Feed empowers you to invest in the relationships with your [engaged audience](${ROUTES.INSIGHTS}?dataset=instagram_engaged_1y) to create your biggest supporters. This isn’t just your followers and people you can reach organically, but anyone who has engaged with you before. Keep the conversation going and get more posts in front of your engaged audience to build followers, website visitors and customers/superfans over time.`,
-
 }
