a7a93f80401d8135117b7314cf80ad18a443d496
Dennis Dijkstra <mail@dennisdijkstra.com>
2021-09-14 13:32:46 +0200

    FD-750 Add code snippet to _document file

--- packages/app/pages/_document.jsx
+++ packages/app/pages/_document.jsx
@@ -23,6 +23,31 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <div id="fb-root" />
+          <div id="fb-customer-chat" className="fb-customerchat" />
+          <script dangerouslySetInnerHTML={{
+            __html: `
+              var chatbox = document.getElementById('fb-customer-chat');
+              chatbox.setAttribute("page_id", "110394157234637");
+              chatbox.setAttribute("attribution", "biz_inbox");
+
+              window.fbAsyncInit = function() {
+                FB.init({
+                  xfbml: true,
+                  version: 'v11.0'
+                });
+              };
+
+              (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/en_GB/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+              }(document, 'script', 'facebook-jssdk'));
+            `,
+          }}
+          />
         </body>
       </Html>
     )
