218da8a5f29773f5fde9b11858a90c0a87615e55
jay-see-jay <6587212+jay-see-jay@users.noreply.github.com>
2021-09-21 15:23:27 +0100

    Adjust width of intro video at different screen sizes

--- packages/landing/components/IntroVideo.jsx
+++ packages/landing/components/IntroVideo.jsx
@@ -7,9 +7,21 @@ const IntroVideo = () => {
       className={[
         'section--padding',
         'bmw',
+        'grid',
+        'grid-cols-12',
+        'gap-4',
       ].join(' ')}
     >
       <VideoEmbed
+        className={[
+          'col-span-12',
+
+          'md:col-span-8',
+          'md:col-start-3',
+
+          'lg:col-span-6',
+          'lg:col-start-4',
+        ].join(' ')}
         video={{
           provider: 'vimeo',
           providerUid: '606336333',
