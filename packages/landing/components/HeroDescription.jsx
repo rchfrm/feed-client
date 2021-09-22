ea6be1a81bdfebd2f7dba33dd4222acee96c1895
jay-see-jay <6587212+jay-see-jay@users.noreply.github.com>
2021-09-22 14:09:28 +0100

    Adjust font-size of hero description

--- packages/landing/components/HeroDescription.jsx
+++ packages/landing/components/HeroDescription.jsx
@@ -4,11 +4,15 @@ export default function HeroDescription({ description }) {
   return (
     <MarkdownText
       className={[
-        'text-2xl',
+        'text-xl',
         'font-display',
         'col-span-12',
         'row-start-3',
         'col-start-1',
+
+        'sm:text-2xl',
+
+        'mb-12',
 
         'md:z-10',
         'md:col-end-8',
