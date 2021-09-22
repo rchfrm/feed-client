540b36592a0ac18b71451d7e8227157371501483
jay-see-jay <6587212+jay-see-jay@users.noreply.github.com>
2021-09-22 16:09:23 +0100

    Add bg colour to intro video

--- packages/landing/components/IntroVideo.jsx
+++ packages/landing/components/IntroVideo.jsx
@@ -6,28 +6,35 @@ const IntroVideo = () => {
       id="intro-video"
       className={[
         'section--padding',
-        'bmw',
-        'grid',
-        'grid-cols-12',
-        'gap-4',
+        'bg-grey-1',
       ].join(' ')}
     >
-      <VideoEmbed
+      <div
         className={[
-          'col-span-12',
-
-          'md:col-span-8',
-          'md:col-start-3',
-
-          'lg:col-span-6',
-          'lg:col-start-4',
+          'w-full',
+          'bmw',
+          'grid',
+          'grid-cols-12',
+          'gap-4',
         ].join(' ')}
-        video={{
-          provider: 'vimeo',
-          providerUid: '606336333',
-          url: 'https://vimeo.com/606336333',
-        }}
-      />
+      >
+        <VideoEmbed
+          className={[
+            'col-span-12',
+
+            'md:col-span-8',
+            'md:col-start-3',
+
+            'lg:col-span-6',
+            'lg:col-start-4',
+          ].join(' ')}
+          video={{
+            provider: 'vimeo',
+            providerUid: '606336333',
+            url: 'https://vimeo.com/606336333',
+          }}
+        />
+      </div>
     </section>
   )
 }
