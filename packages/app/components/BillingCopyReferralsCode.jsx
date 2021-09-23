281659420c8580abca7e1c35b58587c3227e54ba
Dennis Dijkstra <mail@dennisdijkstra.com>
2021-09-23 16:43:29 +0200

    FD-539 Remove repeated ternary

--- packages/app/components/BillingCopyReferralsCode.jsx
+++ packages/app/components/BillingCopyReferralsCode.jsx
@@ -6,10 +6,9 @@ import ButtonShare from '@/elements/ButtonShare'
 
 const BillingCopyReferralsCode = () => {
   const { user: { referral_code } } = React.useContext(UserContext)
-  const baseUrl = process.env.isDev ? 'https://localhost:3001' : 'https://app.tryfeed.co'
-  const baseText = process.env.isDev ? 'localhost:3001' : 'app.tryfeed.co'
+  const baseUrl = !process.env.isDev ? 'https://localhost:3001' : 'https://app.tryfeed.co'
   const joinUrl = `${baseUrl}/join?code=${referral_code}`
-  const copyText = `${baseText}/join?code=${referral_code}`
+  const copyText = joinUrl.replace(/^https?:\/\//, '')
   return (
     <ButtonShare
       url={joinUrl}
