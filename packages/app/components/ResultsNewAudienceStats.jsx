dba0efd34eff8d59aeda84b2c49d39dd78b7e805
Dennis Dijkstra <mail@dennisdijkstra.com>
2021-09-30 15:40:11 +0200

    FD-749 Fix linting error

--- packages/app/components/ResultsNewAudienceStats.jsx
+++ packages/app/components/ResultsNewAudienceStats.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import ResultsAbsoluteChart from '@/app/ResultsAbsoluteChart.jsx'
+import ResultsAbsoluteChart from '@/app/ResultsAbsoluteChart'
 import ResultsFallbackChart from '@/app/ResultsFallbackChart'
 
 import MarkdownText from '@/elements/MarkdownText'
