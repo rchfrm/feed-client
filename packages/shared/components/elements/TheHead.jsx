96fe0a0586650e5c8d136fd47831a65097da7b26
Dennis Dijkstra <mail@dennisdijkstra.com>
2021-10-25 09:45:46 +0200

    FD-841 Add metadata to App and Admin

--- packages/shared/components/elements/TheHead.jsx
+++ packages/shared/components/elements/TheHead.jsx
@@ -14,12 +14,13 @@ const TheHead = ({ siteUrl, metaDescription, noIndex, includeStripe }) => {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       </Favicons>
-      {/* Twitter and OG info */}
+      {/* Twitter, Facebook and OG info */}
       <meta key="meta-description" name="description" content={metaDescription} />
       <meta key="meta-og:url" property="og:url" content={siteUrl} />
       <meta key="meta-og:image" property="og:image" content={ogImageUrl} />
       <meta key="meta-og:site_name" property="og:site_name" content="Feed" />
       <meta key="meta-og:description" property="og:description" content={metaDescription} />
+      <meta key="meta-og:type" property="og:type" content="website" />
       <meta key="meta-twitter:card" name="twitter:card" content="summary_large_image" />
       <meta key="meta-twitter:site" name="twitter:site" content="@feed_hq" />
       <meta key="meta-twitter:creator" name="twitter:creator" content="@feed_hq" />
@@ -29,6 +30,7 @@ const TheHead = ({ siteUrl, metaDescription, noIndex, includeStripe }) => {
       <meta key="meta-twitter:image" name="twitter:image" content={ogImageUrl} />
       <meta key="meta-twitter:image:width" name="twitter:image:width" content="1200px" />
       <meta key="meta-twitter:image:height" name="twitter:image:height" content="630px" />
+      <meta key="meta-fb:app_id" property="fb:app_id" content="617771208738795" />
       {/* Preconnect to external assets */}
       {includeStripe && <link rel="preconnect" href="https://js.stripe.com/v3" />}
       {noIndex && <meta name="robots" content="noindex" />}
