701958f059c8989ceca42114e4910eccb095712f
Dennis Dijkstra <dennis@tryfeed.co>
2021-10-27 10:55:42 +0200

    FD-709 Toggle posts when prioritising (#453)
    * FD-709 Deprioritize post if opted out from both campaign types
    
    * FD-709 Opt in post if prioritized and not opted in for at least one campaign type
    
    * FD-709 Usepromotion_enabled and conversions_enabled values to check if post is opted in

--- packages/app/components/PostCard.jsx
+++ packages/app/components/PostCard.jsx
@@ -38,6 +38,7 @@ const PostCard = ({
         artistId={artistId}
         priorityEnabled={post.priorityEnabled}
         updatePost={updatePost}
+        toggleCampaign={toggleCampaign}
         postIndex={postIndex}
         promotionStatus={promotionStatus}
         className="mb-2"
@@ -61,7 +62,9 @@ const PostCard = ({
             artistId={artistId}
             post={post}
             postToggleSetterType={postToggleSetterType}
+            postIndex={postIndex}
             toggleCampaign={toggleCampaign}
+            updatePost={updatePost}
             priorityEnabled={post.priorityEnabled}
             togglesClassName="py-2 px-4 mb-2 last:mb-0"
             className="mb-2"
@@ -92,6 +95,7 @@ const PostCard = ({
 PostCard.propTypes = {
   post: PropTypes.object.isRequired,
   postIndex: PropTypes.number.isRequired,
+  updatePost: PropTypes.func.isRequired,
   toggleCampaign: PropTypes.func.isRequired,
   postToggleSetterType: PropTypes.string.isRequired,
   isMissingDefaultLink: PropTypes.bool.isRequired,
