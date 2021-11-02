e8e6298974f283e5f89f3ec848fc3b94b32bc100
jay-see-jay <6587212+jay-see-jay@users.noreply.github.com>
2021-11-02 17:03:38 +0000

    Arts and Music Landing pages (#462)
    * Start work on separating out models to create sector specific landing pages
    
    * Get music and arts copy from Dato
    
    * Create arts page
    
    * Add intro video to HomePage
    
    * Pull in testimonies
    
    * Add testimonies response to all landing pages
    
    * Pull in features from Dato
    
    * Show featured article
    
    * Simplify dato query
    
    * Remove unused components
    
    * Remove commented out code

---
+++ packages/landing/graphQl/homePageQueries.js
@@ -0,0 +1,45 @@
+import { image } from '@/landing/graphQl/querySnippets'
+import { blogIntro } from '@/landing/graphQl/blogArticlesQueries'
+
+function getFilter(page) {
+  switch (page) {
+    case 'music':
+      return 'heroCopy(filter: {name: {eq: "Music"}})'
+    case 'arts':
+      return 'heroCopy(filter: {name: {eq: "Arts"}})'
+    default:
+      return 'heroCopy(filter: {setAsDefault: {eq: true}})'
+  }
+}
+
+function getQuery(page) {
+  return `
+    query {
+      ${getFilter(page)} {
+        straplineA
+        straplineB
+        description
+        ${image('mobileImage')}
+        ${image('desktopImage')}
+        testimonies {
+          id
+          handle
+          quote
+          bio
+          ${image({ imgixParams: 'q: "50", auto: format, w: "600", h: "600", fit: crop', sizes: '10vw' })}
+        }
+        features {
+          id
+          header
+          copy
+          ${image({ imgixParams: 'q: "75", auto: format, w: "800"', sizes: '50vw' })}
+        }
+        featuredArticles {
+          ${blogIntro()}
+        }
+      }
+    }
+  `
+}
+
+export default getQuery
