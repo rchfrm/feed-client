07d2c8a9409a15971afd596edec2258710b713ab
Dennis Dijkstra <mail@dennisdijkstra.com>
2021-11-03 21:21:02 +0100

    FD-840 Add connect button at bottom of page

---
+++ packages/app/components/PostCardConnectAccounts.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Router from 'next/router'
+
+import FacebookIcon from '@/icons/FacebookIcon'
+import InstagramIcon from '@/icons/InstagramIcon'
+
+import brandColors from '@/constants/brandColors'
+
+import * as ROUTES from '@/app/constants/routes'
+
+const PostCardConnectAccounts = () => {
+  const goToConnectAccountsPage = () => {
+    Router.push(ROUTES.CONNECT_ACCOUNTS)
+  }
+
+  return (
+    <button
+      className={[
+        'mx-auto max-w-sm mb-12',
+        'sm:max-w-none sm:mx-0 sm:mb-0',
+        'col-span-12 sm:col-span-6 lg:col-span-4',
+        'flex flex-column justify-center items-center p-12 text-center',
+      ].join(' ')}
+      onClick={goToConnectAccountsPage}
+    >
+      <FacebookIcon className="h-8 mb-6" fill={brandColors.facebook.bg} />
+      <div className="flex flex-column mb-6">
+        <span className="mb-1 text-xl font-bold underline">Connect your pages</span>
+        <span className="text-lg">to see your Facebook and Instagram posts</span>
+      </div>
+      <InstagramIcon className="h-8 mb-6" fill={brandColors.instagram.bg} />
+    </button>
+  )
+}
+
+PostCardConnectAccounts.propTypes = {
+}
+
+PostCardConnectAccounts.defaultProps = {
+}
+
+export default PostCardConnectAccounts
--- packages/app/components/PostsNoArtists.jsx
+++ packages/app/components/PostsNoArtists.jsx
@@ -1,30 +1,49 @@
 import React from 'react'
 
 import PostCardDummy from '@/app/PostCardDummy'
-import PostCardFacebookConnect from '@/app/PostCardFacebookConnect'
+import PostCardConnectAccounts from '@/app/PostCardConnectAccounts'
+import ConnectFacebookButton from '@/app/ConnectFacebookButton'
 
 import { dummyPosts } from '@/app/helpers/postsHelpers'
 
 const PostsNoArtists = () => {
+  const [errors, setErrors] = React.useState([])
+
   return (
-    <ul
-      className={[
-        'sm:grid',
-        'grid-cols-12',
-        'row-gap-10',
-        'col-gap-6',
-        'grid-flow-row-dense',
-      ].join(' ')}
-    >
-      {dummyPosts.map((post, index) => {
-        return (
-          <>
-            {index === 1 && <PostCardFacebookConnect key={index + dummyPosts.length} />}
-            <PostCardDummy post={post} key={index} />
-          </>
-        )
-      })}
-    </ul>
+    <>
+      <ul
+        className={[
+          'sm:grid',
+          'grid-cols-12',
+          'row-gap-10',
+          'col-gap-6',
+          'grid-flow-row-dense',
+          'mb-30',
+        ].join(' ')}
+      >
+        {dummyPosts.map((post, index) => {
+          return (
+            <>
+              {index === 1 && <PostCardConnectAccounts key={index + dummyPosts.length} />}
+              <PostCardDummy post={post} key={index} />
+            </>
+          )
+        })}
+      </ul>
+      <div className="grid grid-cols-12">
+        <ConnectFacebookButton
+          errors={errors}
+          setErrors={setErrors}
+          buttonText="Connect your Facebook &amp; Instagram"
+          trackComponentName="PostsNoArtists"
+          className={[
+            'col-span-12 sm:col-span-6 sm:col-start-4',
+            'lg:col-span-4 lg:col-start-5',
+            'sm:max-w-none mx-auto max-w-sm',
+          ].join(' ')}
+        />
+      </div>
+    </>
   )
 }
 
