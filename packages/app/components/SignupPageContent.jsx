7b90b45220114bc4d72ed0506a252827bc5187f4
Dennis Dijkstra <mail@dennisdijkstra.com>
2021-12-09 15:43:25 +0100

    FD-901 Sign up page layout changes

--- packages/app/components/SignupPageContent.jsx
+++ packages/app/components/SignupPageContent.jsx
@@ -5,9 +5,9 @@ import SignupEmailForm from '@/app/SignupEmailForm'
 
 import Error from '@/elements/Error'
 import MarkdownText from '@/elements/MarkdownText'
+import TickCircleIcon from '@/icons/TickCircleIcon'
 
 import copy from '@/app/copy/LoginPageCopy'
-
 
 const SignupPageContent = () => {
   const { authError, setAuthError } = React.useContext(AuthContext)
@@ -20,19 +20,33 @@ const SignupPageContent = () => {
   }, [setAuthError])
 
   return (
-    <div className="flex mx-auto pt-20 max-w-4xl">
-      <div className="flex-1 pt-10 border-solid border-r border-grey-3">
-        <div className="mr-16">
-          <MarkdownText markdown="You are this close to never having to set-up an ad campaign again!" />
-          <MarkdownText markdown="Already have an account? Log in here" />
-        </div>
-      </div>
-      <div className="flex-1 pt-10">
-        <div className="ml-16">
+    <div className="flex flex-column sm:flex-row mx-auto sm:pt-8 max-w-4xl">
+      <div className="flex-1 pb-5 sm:pb-0 pt-10 sm:order-2">
+        <div className="sm:ml-16">
           <Error error={authError} />
-          <MarkdownText className="font-bold text-xl" markdown="Enter a password to create your account" />
+          <h1 className="text-2xl">Enter a password to create your account</h1>
           <MarkdownText className={['small--text'].join(' ')} markdown={copy.tcText('next')} />
           <SignupEmailForm />
+        </div>
+      </div>
+      <div className="flex-1 sm:order-1 pt-15 sm:pt-10 border-solid border-t sm:border-t-0 sm:border-r border-grey-3">
+        <div className="sm:mr-16">
+          <MarkdownText className="mb-10 text-lg" markdown={copy.signupTeaser} />
+          <ul className="mb-10 pl-10">
+            <li className="flex mb-4">
+              <TickCircleIcon className="w-6 h-6 mr-6" />
+              Really intelligent algorithm
+            </li>
+            <li className="flex mb-4">
+              <TickCircleIcon className="w-6 h-6 mr-6" />
+              Stop/start ads whenever you like
+            </li>
+            <li className="flex mb-4">
+              <TickCircleIcon className="w-6 h-6 mr-6" />
+              Only pay 10% of your daily budget
+            </li>
+          </ul>
+          <MarkdownText markdown={copy.loginReminder} />
         </div>
       </div>
     </div>
@@ -43,7 +57,6 @@ SignupPageContent.propTypes = {
 }
 
 SignupPageContent.defaultProps = {
-  showEmailSignup: false,
 }
 
 export default SignupPageContent
--- packages/app/copy/LoginPageCopy.js
+++ packages/app/copy/LoginPageCopy.js
@@ -4,6 +4,10 @@ import * as ROUTES from '@/app/constants/routes'
 export default {
   tcText: (cta) => `By clicking ${cta}, you agree to our [Terms of Service](https://archform.ltd/terms) and [Privacy Policy](https://archform.ltd/privacy).`,
 
+  signupTitle: '# Enter a password to create your account',
+
+  signupTeaser: 'You are this close to never having to set-up an ad campaign again!',
+
   signupReminder: `Don't have an account? [Sign up here](${ROUTES.SIGN_UP})`,
 
   loginReminder: `Already have an account? [Log in here](${ROUTES.LOGIN})`,
