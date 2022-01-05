a9fa58b3cc21d609a21ce887ecfe62884780a5d3
Dennis Dijkstra <mail@dennisdijkstra.com>
2022-01-05 15:21:00 +0100

    FD-937 Trigger sidepanel on click

---
+++ packages/app/components/PostsFiltersOptions.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+
+
+const PostsFiltersOptions = () => {
+  return (
+    <div>
+      <p>Sidepanel</p>
+    </div>
+  )
+}
+
+PostsFiltersOptions.propTypes = {
+}
+
+PostsFiltersOptions.defaultProps = {
+}
+
+export default PostsFiltersOptions
