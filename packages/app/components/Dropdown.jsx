3d77a7178b1b83b0d621072584d7363cbde1fd08
Dennis Dijkstra <mail@dennisdijkstra.com>
2022-01-24 21:52:30 +0100

    FD-904 Create dropdown component

---
+++ packages/app/components/Dropdown.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { capitalise } from '@/helpers/utils'
+
+const Dropdown = ({ title, items, handleItemClick, className }) => {
+  const [isOpen, setIsOpen] = React.useState(false)
+  const dropdownRef = React.useRef(null)
+
+  const close = ({ target }) => {
+    if (!dropdownRef.current) return
+    // Don't close if clicking on dropdown button or items
+    if (dropdownRef.current.contains(target)) return
+
+    setIsOpen(false)
+  }
+
+  const onClick = (item) => {
+    handleItemClick(item)
+    setIsOpen(false)
+  }
+
+  // Listen to clicks outside dropdown to handle close
+  React.useEffect(() => {
+    if (isOpen) {
+      window.addEventListener('click', close)
+      return
+    }
+    window.removeEventListener('click', close)
+
+    return () => {
+      window.removeEventListener('click', close)
+    }
+  }, [isOpen])
+
+  return (
+    <div className="relative" ref={dropdownRef}>
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className={className}
+      >
+        {title}
+      </button>
+      {isOpen && (
+        <ul
+          className={[
+            'absolute p-2',
+            'border-2 border-solid border-black',
+            'bg-white rounded-dialogue',
+          ].join(' ')}
+        >
+          {items.map((item) => (
+            <li key={item}>
+              <button
+                onClick={() => onClick(item)}
+                className="font-semibold"
+              >
+                {capitalise(item)}
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  )
+}
+
+Dropdown.propTypes = {
+  title: PropTypes.string.isRequired,
+  items: PropTypes.array.isRequired,
+  handleItemClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
+}
+
+Dropdown.defaultProps = {
+  className: null,
+}
+
+export default Dropdown
