25ea49a36b66892da30edd13efcd040b4ec78160
Dennis Dijkstra <mail@dennisdijkstra.com>
2022-03-04 16:23:12 +0000

    FD-903 Adjust facebook button text based on screen size

--- packages/app/components/NoArtistsConnectAccountsBlock.jsx
+++ packages/app/components/NoArtistsConnectAccountsBlock.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import useBreakpointTest from '@/hooks/useBreakpointTest'
+
 import ConnectFacebookButton from '@/app/ConnectFacebookButton'
 import MarkdownText from '@/elements/MarkdownText'
 import copy from '@/app/copy/PostsPageCopy'
 
 const NoArtistsConnectAccountsBlock = ({ className, page }) => {
+  const isDesktopLayout = useBreakpointTest('xxs')
+
   return (
     <div className={className}>
       <MarkdownText markdown={copy.connectWithFacebookBlock(page)} />
       <ConnectFacebookButton
-        buttonText="Continue with Facebook"
+        buttonText={isDesktopLayout ? 'Continue with Facebook' : 'Connect'}
         trackComponentName="NoArtistsConnectAccountsBlock"
         className="w-full text-sm"
       />
