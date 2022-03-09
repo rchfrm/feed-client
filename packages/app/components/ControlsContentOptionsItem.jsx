4f6b5fdd0054becdf7d8c1bbde6905f57dd249f5
Dennis Dijkstra <mail@dennisdijkstra.com>
2022-03-09 15:06:31 +0000

    FD-989 Create options item component

---
+++ packages/app/components/ControlsContentOptionsItem.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ControlsContentOptionsItem = ({
+  option,
+  isActive,
+  isLast,
+  goToSpecificSetting,
+}) => {
+  const { title, description, key } = option
+
+  return (
+    <a
+      role="button"
+      className={[
+        'flex items-center no-underline',
+        'p-4',
+        !isLast && !isActive ? 'border-solid border-grey-1 border-b-2' : null,
+        isActive ? 'bg-insta' : null,
+      ].join(' ')}
+      onClick={() => goToSpecificSetting(key)}
+    >
+      {/* TITLE */}
+      <div className={[
+        'relative w-8 h-8',
+        'flex justify-center items-center mr-4',
+        'flex-shrink-0',
+        'rounded-full',
+        'border border-solid border-black bg-white',
+        // isActive && 'bg-green',
+      ].join(' ')}
+      >
+        {isActive && <span className="w-4 h-4 block rounded-full bg-green" />}
+      </div>
+      <div>
+        <p className="font-bold mb-2">{title}</p>
+        <p className="mb-0">{description}</p>
+      </div>
+    </a>
+  )
+}
+
+ControlsContentOptionsItem.propTypes = {
+  key: PropTypes.string.isRequired,
+  option: PropTypes.object.isRequired,
+  isActive: PropTypes.bool.isRequired,
+  isLast: PropTypes.bool.isRequired,
+  goToSpecificSetting: PropTypes.func.isRequired,
+}
+
+ControlsContentOptionsItem.defaultProps = {
+}
+
+export default ControlsContentOptionsItem
