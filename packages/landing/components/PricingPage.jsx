107455d3e9559a00a649d686f9829101a17113d6
jay-see-jay <6587212+jay-see-jay@users.noreply.github.com>
2022-03-17 14:44:07 +0000

    FD-1115 Remove existing pricing page code

--- packages/landing/components/PricingPage.jsx
+++ packages/landing/components/PricingPage.jsx
@@ -1,40 +1,11 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
-import BannerBase from '@/landing/elements/BannerBase'
-import BasicTextPage from '@/landing/BasicTextPage'
-
-
-import PricingPageTiers from '@/landing/PricingPageTiers'
-import PricingPageDescription from '@/landing/PricingPageDescription'
-import PrimaryCTA from '@/landing/PrimaryCTA'
-
-const PricingPage = ({ pageData }) => {
-  const { promoBanner, pricingCopy } = pageData
+const PricingPage = () => {
   return (
     <>
-      {/* Promotion */}
-      {promoBanner && (
-        <BannerBase copy={promoBanner} />
-      )}
-
-      <BasicTextPage
-        pageData={{ title: 'Pricing' }}
-        endContent={<PricingPageTiers pageData={pageData} />}
-        isFullWidth
-      />
-
-      <PrimaryCTA />
-
-      {pricingCopy && (
-        <PricingPageDescription copy={pricingCopy} />
-      )}
+      <h1>Pricing</h1>
     </>
   )
-}
-
-PricingPage.propTypes = {
-  pageData: PropTypes.object.isRequired,
 }
 
 export default PricingPage
