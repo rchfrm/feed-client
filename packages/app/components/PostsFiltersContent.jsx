dfd4649b397c0ed58af6eef2a0a80bd8d1aa03b4
Dennis Dijkstra <mail@dennisdijkstra.com>
2022-03-23 16:13:30 +0000

    FD-1068 Undo posts filter change

--- packages/app/components/PostsFiltersContent.jsx
+++ packages/app/components/PostsFiltersContent.jsx
@@ -53,7 +53,7 @@ const PostsFiltersContent = ({
   React.useEffect(() => {
     setSidePanelButton(CLOSE_BUTTON)
     // eslint-disable-next-line
-  }, [hasMultipleFiltersApplied, setSidePanelButton])
+  }, [hasMultipleFiltersApplied, CLOSE_BUTTON, setSidePanelButton])
 
   return (
     <div>
