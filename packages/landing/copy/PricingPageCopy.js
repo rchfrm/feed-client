7944dd61fbfd14b5737342959f505cdba678ec1d
jay-see-jay <6587212+jay-see-jay@users.noreply.github.com>
2022-03-24 15:07:00 +0000

    FD-1115 Use markdown instead of html
    Co-authored-by: Dennis Dijkstra <mail@dennisdijkstra.com>

--- packages/landing/copy/PricingPageCopy.js
+++ packages/landing/copy/PricingPageCopy.js
@@ -2,7 +2,7 @@
 
 export default {
   title: `We charge 10% of your budget. <strong>That's it.</strong>`,
-  bullets: '* <strong>No commitments</strong>: <em>start and stop whenever you like</em>'
-    + '\n * <strong>No mimimum budget</strong>: <em>well, there is but it’s just £2 a day</em>',
+  bullets: `* **No commitments**: *start and stop whenever you like* 
+* **No mimimum budget**: *well, there is but it's just £2 a day*`,
   budgetInput: 'Enter your daily budget to see a breakdown of your spend over a month.',
 }
