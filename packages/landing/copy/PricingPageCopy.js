7d141a54ecc3434de77251d2170e506239860441
Dennis Dijkstra <mail@dennisdijkstra.com>
2022-03-25 10:00:45 +0000

    Fix pricing page typo

--- packages/landing/copy/PricingPageCopy.js
+++ packages/landing/copy/PricingPageCopy.js
@@ -3,6 +3,6 @@
 export default {
   title: `We charge 10% of your budget. <strong>That's it.</strong>`,
   bullets: `* **No commitments**: *start and stop whenever you like* 
-* **No mimimum budget**: *well, there is but it's just £2 a day*`,
+* **No minimum budget**: *well, there is but it's just £2 a day*`,
   budgetInput: 'Enter your daily budget to see a breakdown of your spend over a month.',
 }
