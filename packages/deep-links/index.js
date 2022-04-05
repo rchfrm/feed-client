6915c8ddabded061746f45f2bcc125787006abb6
jay-see-jay <6587212+jay-see-jay@users.noreply.github.com>
2022-04-05 15:19:58 +0100

    FD-1120 Deep Links (#524)
    * FD-1120 Create deep-links workspace
    
    * FD-1120 Add workspace config files
    
    * FD-1120 Create index page
    
    * FD-1120 Get dynamic route using server side props
    
    * FD-1120 Draft redirect
    
    * FD-1120 Remove unnecessary config files
    
    * FD-1120 Redirect based on platform
    
    * FD-1120 Remove unnecessary code and move to /instagram path
    
    * FD-1120 Absolute imports
    
    * FD-1120 Redirect other URLs to tryfeed.co
    
    * FD-1120 Update deploy scripts
    
    * FD-1120 Use express instead of next
    
    * FD-1120 Specify node v12
    
    * FD-1120 Update package.json scripts
    
    * FD-1120 Undo unnecessary changes to config files and deploy script
    
    * FD-1120 Remove console log
    
    * FD-1120 Log, returns and status code variable
    
    * FD-1120 Update README

---
+++ packages/deep-links/index.js
@@ -0,0 +1,31 @@
+const functions = require('firebase-functions')
+const express = require('express')
+const favicon = require('serve-favicon')
+const path = require('path')
+const UserAgent = require('./helpers/userAgent')
+
+const app = express()
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+
+const HTTP_STATUS_PERMANENT_REDIRECT = 308
+
+app.get('/instagram/:handle', (req, res) => {
+  const { params: { handle }, headers } = req
+  const userAgent = UserAgent(headers['user-agent'])
+
+  if (userAgent.isIOS) {
+    return res.redirect(HTTP_STATUS_PERMANENT_REDIRECT, `instagram://user?username=${handle}`)
+  }
+
+  if (userAgent.isAndroid) {
+    return res.redirect(HTTP_STATUS_PERMANENT_REDIRECT, `intent://www.instagram.com/${handle}#Intent;package=com.instagram.android;scheme=https;end`)
+  }
+
+  return res.redirect(HTTP_STATUS_PERMANENT_REDIRECT, `https://instagram.com/${handle}`)
+})
+
+app.get('*', (req, res) => {
+  return res.redirect(HTTP_STATUS_PERMANENT_REDIRECT, 'https://tryfeed.co')
+})
+
+exports.instagramRedirect = functions.https.onRequest(app)
