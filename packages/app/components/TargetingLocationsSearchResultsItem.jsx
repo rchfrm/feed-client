a6852cd4a69dcf36f226017efe4ede42bdcd1088
Dennis Dijkstra <mail@dennisdijkstra.com>
2022-04-18 15:20:19 +0200

    FD-1060 Add location to checkbox list

--- packages/app/components/TargetingLocationsSearchResultsItem.jsx
+++ packages/app/components/TargetingLocationsSearchResultsItem.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import { TargetingContext } from '@/app/contexts/TargetingContext'
+
+import * as targetingHelpers from '@/app/helpers/targetingHelpers'
+
 const TargetingLocationsSearchResultsItem = ({ location, setSavedLocation }) => {
   const {
     name,
@@ -9,14 +13,36 @@ const TargetingLocationsSearchResultsItem = ({ location, setSavedLocation }) => 
     country_code: countryCode,
   } = location
 
+  const {
+    targetingState,
+    popularLocations,
+    setLocationOptions,
+  } = React.useContext(TargetingContext)
+
+  const isCity = type === 'city'
+  const isCountry = type === 'country'
+
   const saveCustomLocation = async () => {
+    const formattedLocation = {
+      ...location,
+      code: location.key,
+    }
+
+    const currentLocations = {
+      cities: [...targetingState.cities, ...(isCity ? [formattedLocation] : [])],
+      countries: [...targetingState.countries, ...(isCountry ? [formattedLocation] : [])],
+    }
+
+    const updatedLocationOptions = targetingHelpers.formatPopularLocations(currentLocations, popularLocations)
+
+    setLocationOptions(updatedLocationOptions)
     setSavedLocation(location)
   }
 
   return (
     <li className="mb-3 pl-12">
       <button onClick={saveCustomLocation} className="align-left">
-        <span className="font-bold underline">{name}</span>, {type === 'city' ? `${region}, ` : null} {countryCode}
+        <span className="font-bold underline">{name}</span>, {isCity ? `${region}, ` : null} {countryCode}
       </button>
     </li>
   )
