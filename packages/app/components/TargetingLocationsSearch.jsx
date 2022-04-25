20dd2dc5ac52d0883125a31a2560c4397ee0729b
Dennis Dijkstra <mail@dennisdijkstra.com>
2022-04-25 14:33:30 +0200

    FD-1060 Custom locations (#536)
    * FD-1060 Create targeting locations search components
    
    * FD-1060 Add location to checkbox list
    
    * FD-1060 Refactor search component
    
    * FD-1060 Update targeting state when adding a custom location
    
    * FD-1060 Fix proptypes, rename state variable and wrap input with form tag
    
    * FD-1060 Tweak placeholder text
    
    * FD-1060 Check if location already exists
    
    * FD-1060 Create search success component
    
    * FD-1060 Tweak formatting and reset error on change
    
    * FD-1060 Remove useless async keyword
    
    * FD-1060 Remove confusing warning about only targeting Instagram
    
    * FD-1060 Check if city belongs to already selected country
    
    * FD-1060 Fix typo
    
    Co-authored-by: jay-see-jay <6587212+jay-see-jay@users.noreply.github.com>

---
+++ packages/app/components/TargetingLocationsSearch.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import TargetingLocationsSearchResultsItem from '@/app/TargetingLocationsSearchResultsItem'
+import TargetingLocationsSearchSuccess from '@/app/TargetingLocationsSearchSuccess'
+
+import Search from '@/elements/Search'
+
+import { getGeoLocations } from '@/app/helpers/targetingHelpers'
+
+const TargetingLocationsSearch = () => {
+  const [location, setLocation] = React.useState(null)
+  const { name } = location || {}
+
+  return (
+    !location ? (
+      <>
+        <p className="text-lg">Search for another location</p>
+        <Search
+          name="location"
+          onChange={getGeoLocations}
+          onClick={setLocation}
+          listItem={TargetingLocationsSearchResultsItem}
+          placeholder="Start typing to search..."
+          className="mb-4"
+        />
+      </>
+    ) : (
+      <TargetingLocationsSearchSuccess
+        name={name}
+        setLocation={setLocation}
+      />
+    )
+  )
+}
+
+TargetingLocationsSearch.propTypes = {
+}
+
+TargetingLocationsSearch.defaultProps = {
+}
+
+export default TargetingLocationsSearch
