bc57583932ae95a29037a6a9b6a8e1c7c0f34f3f
Dennis Dijkstra <mail@dennisdijkstra.com>
2022-04-29 15:48:32 +0200

    FD-1102 Dont show top performing sales post if objective is not sales

--- packages/app/components/ResultsPostsStats.jsx
+++ packages/app/components/ResultsPostsStats.jsx
@@ -11,6 +11,7 @@ import { postResultsConfig } from '@/app/helpers/resultsHelpers'
 
 const getControlsStoreState = (state) => ({
   isSpendingPaused: state.isSpendingPaused,
+  optimizationPreferences: state.optimizationPreferences,
 })
 
 const ResultsPostsStats = ({
@@ -21,25 +22,31 @@ const ResultsPostsStats = ({
     return post.type === x.type
   })).filter(Boolean)
 
-  const { isSpendingPaused } = useControlsStore(getControlsStoreState)
+  const { isSpendingPaused, optimizationPreferences } = useControlsStore(getControlsStoreState)
   const isDesktopLayout = useBreakpointTest('sm')
+  const { objective } = optimizationPreferences
+  const hasSalesObjective = objective === 'sales'
 
   return (
     sortedPosts.length ? (
-      sortedPosts.map((post, index) => (
-        <ResultsPostStats
-          key={post.type}
-          post={post}
-          config={postResultsConfig[index]}
-          className={[
-            'col-span-12',
-            className,
-            'flex flex-col sm:items-center',
-            isDesktopLayout ? 'order-4' : `order-${index + 1}`,
-            'mb-6 sm:mb-0',
-          ].join(' ')}
-        />
-      ))
+      sortedPosts.map((post, index) => {
+        if (post.type === 'conversions' && !hasSalesObjective) return
+
+        return (
+          <ResultsPostStats
+            key={post.type}
+            post={post}
+            config={postResultsConfig[index]}
+            className={[
+              'col-span-12',
+              className,
+              'flex flex-col sm:items-center',
+              isDesktopLayout ? 'order-4' : `order-${index + 1}`,
+              'mb-6 sm:mb-0',
+            ].join(' ')}
+          />
+        )
+      })
     ) : (
       <ResultsPostsNoData isSpendingPaused={isSpendingPaused} />
     )
