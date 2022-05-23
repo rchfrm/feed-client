cf2b7778de0ce9f32280c15a5ebc1d0ca5a33f4e
Dennis Dijkstra <mail@dennisdijkstra.com>
2022-05-23 20:03:31 +0200

    Fix posts not showing up

--- packages/app/helpers/postsHelpers.js
+++ packages/app/helpers/postsHelpers.js
@@ -317,9 +317,9 @@ const getAdPreviewLinks = (post) => {
   return sortedAds.reduce((result, ad) => {
     return {
       ...result,
-      ...(ad.adset_identifier.includes('engage') && ad.preview_shareable_link ? { engage: ad.preview_shareable_link } : null),
-      ...(ad.adset_identifier.includes('traffic') && ad.preview_shareable_link ? { nurture: ad.preview_shareable_link } : null),
-      ...(ad.adset_identifier.includes('conversions') && ad.preview_shareable_link ? { sales: ad.preview_shareable_link } : null),
+      ...(ad.adset_identifier?.includes('engage') && ad.preview_shareable_link ? { engage: ad.preview_shareable_link } : null),
+      ...(ad.adset_identifier?.includes('traffic') && ad.preview_shareable_link ? { nurture: ad.preview_shareable_link } : null),
+      ...(ad.adset_identifier?.includes('conversions') && ad.preview_shareable_link ? { sales: ad.preview_shareable_link } : null),
     }
   }, {})
 }
