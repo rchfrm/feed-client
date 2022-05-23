04f37256e2672bb0c5ae3f46c08566e681f54d63
jay-see-jay <6587212+jay-see-jay@users.noreply.github.com>
2022-05-23 16:47:50 +0100

    FD-1202 Layout service fee:

--- packages/landing/components/PricingTierServiceFee.jsx
+++ packages/landing/components/PricingTierServiceFee.jsx
@@ -2,7 +2,21 @@ import PropTypes from 'prop-types'
 
 export default function PricingTierServiceFee({ percentage }) {
   return (
-    <p>{percentage}</p>
+    <div
+      className={[
+        'flex',
+        'items-center',
+        percentage === 0 && 'text-grey-3',
+      ].join(' ')}
+    >
+      <p className="h2">{percentage * 100}</p>
+      <p
+        className={['h4', 'pr-1'].join(' ')}
+      >
+        %
+      </p>
+      <p>service fee</p>
+    </div>
   )
 }
 
