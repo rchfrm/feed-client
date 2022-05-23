585f981079aa59c74cc4051a4740c755302a6008
Dennis Dijkstra <mail@dennisdijkstra.com>
2022-05-23 22:13:48 +0200

    FD-1206 Tweak container width

--- packages/app/components/GetStartedPaymentMethod.jsx
+++ packages/app/components/GetStartedPaymentMethod.jsx
@@ -87,7 +87,7 @@ const GetStartedPaymentMethod = () => {
       <h3 className="w-full mb-8 xs:mb-4 font-medium text-xl">{copy.paymentMethodSubtitle}</h3>
       <MarkdownText className="hidden xs:block sm:w-2/3 text-grey-3 italic" markdown={copy.paymentMethodDescription} />
       <Error error={error} />
-      <div className="w-full sm:w-1/2 mx-auto">
+      <div className="w-full sm:w-1/2 lg:w-1/3 mx-auto">
         {defaultPaymentMethod ? (
           <>
             <p className="mb-4 font-bold text-center">Your current default card:</p>
