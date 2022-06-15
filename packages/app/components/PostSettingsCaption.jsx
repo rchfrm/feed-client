5609c33f8bad1bed6a2bebc71dd0bf938f5fb3d6
Dennis Dijkstra <mail@dennisdijkstra.com>
2022-06-15 21:56:41 +0200

    FD-943 Finish caption component

--- packages/app/components/PostSettingsCaption.jsx
+++ packages/app/components/PostSettingsCaption.jsx
@@ -9,7 +9,6 @@ import PostCardEditCaptionMessage from '@/app/PostCardEditCaptionMessage'
 import CheckboxInput from '@/elements/CheckboxInput'
 import Button from '@/elements/Button'
 import Error from '@/elements/Error'
-import CaptionText from '@/elements/CaptionText'
 
 import { getPostAdMessages, updatePostCaption, resetPostCaption } from '@/app/helpers/postsHelpers'
 
@@ -20,10 +19,9 @@ const PostSettingsAdMessage = ({
   const { id: postId } = post
 
   const [adMessages, setAdMessages] = React.useState([])
-  const [currentAdMessage, setCurrentAdMessage] = React.useState('')
+  const [currentAdMessage, setCurrentAdMessage] = React.useState({})
   const [caption, setCaption] = React.useState('')
   const [savedCaption, setSavedCaption] = React.useState('')
-  const [useEditMode, setUseEditMode] = React.useState(false)
 
   const [isDefaultAdMessage, setIsDefaultAdMessage] = React.useState(true)
   const [shouldShowSaveButton, setShouldShowSaveButton] = React.useState(false)
@@ -37,6 +35,7 @@ const PostSettingsAdMessage = ({
     if (!isMounted) return
 
     if (error) {
+      setError(error)
       return
     }
 
@@ -46,11 +45,11 @@ const PostSettingsAdMessage = ({
   const handleChange = () => {
     setIsDefaultAdMessage(!isDefaultAdMessage)
 
+    // On check set caption equal to original post message
     if (!isDefaultAdMessage) {
-      setCaption(null)
+      setCaption(post?.message)
     } else {
-      setUseEditMode(true)
-      setCaption(currentAdMessage.message)
+      setCaption(currentAdMessage.message || '')
     }
   }
 
@@ -75,12 +74,7 @@ const PostSettingsAdMessage = ({
     setCurrentAdMessage(postAdMessage)
     setSavedCaption(postAdMessage?.message)
     setShouldShowSaveButton(false)
-    setUseEditMode(false)
   }
-
-  React.useEffect(() => {
-    console.log(caption)
-  }, [caption])
 
   // Watch for ad message changes and show save button if there has been a change
   React.useEffect(() => {
@@ -96,9 +90,13 @@ const PostSettingsAdMessage = ({
     const adMessage = adMessages?.find((callToAction) => callToAction.campaignType === campaignType) || {}
 
     setCurrentAdMessage(adMessage)
-    setCaption(adMessage?.message)
-    setSavedCaption(adMessage?.message)
-  }, [campaignType, adMessages])
+    setCaption(adMessage?.message || '')
+    setSavedCaption(adMessage?.message || '')
+
+    if (adMessage?.message && adMessage?.message !== post?.message) {
+      setIsDefaultAdMessage(false)
+    }
+  }, [campaignType, adMessages, post?.message])
 
   return (
     <div className="mb-10">
@@ -128,19 +126,10 @@ const PostSettingsAdMessage = ({
         <div
           className="bg-grey-1 p-4 rounded-dialogue"
         >
-          {useEditMode ? (
-            <PostCardEditCaptionMessage
-              message={caption}
-              setMessage={setCaption}
-            />
-          ) : (
-            <CaptionText
-              caption={caption}
-              className={[
-                'mb-0',
-              ].join(' ')}
-            />
-          )}
+          <PostCardEditCaptionMessage
+            message={caption || post.message}
+            setMessage={setCaption}
+          />
         </div>
       )}
       <Error error={error} />
