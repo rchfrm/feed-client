ca460b146936ab964aa9ddd855f4b048b58deecf
Dennis Dijkstra <mail@dennisdijkstra.com>
2022-06-24 16:20:37 +0200

    Undo changes

--- packages/app/pages/posts/[id].jsx
+++ packages/app/pages/posts/[id].jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
+import { useRouter } from 'next/router'
+
 import PostLoader from '@/app/PostLoader'
 
 import BasePage from '@/app/BasePage'
 import testPageReady from '@/hoc/testPageReady'
 
-const Page = ({ id }) => {
+const Page = () => {
+  const router = useRouter()
+  const { id } = router.query
+
   return (
     <BasePage
       headerConfig={{ text: '' }}
@@ -16,14 +21,6 @@ const Page = ({ id }) => {
       <PostLoader postId={id} />
     </BasePage>
   )
-}
-
-export const getServerSideProps = async (context) => {
-  const { query: { id } } = context
-
-  return { props:
-    { id },
-  }
 }
 
 export default testPageReady('app')(Page)
