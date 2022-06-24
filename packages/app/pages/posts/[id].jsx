9959e471ca4be673e5ec3c074d5190d798167fbd
Dennis Dijkstra <mail@dennisdijkstra.com>
2022-06-24 16:00:37 +0200

    Use getServerSideProps on single post page

--- packages/app/pages/posts/[id].jsx
+++ packages/app/pages/posts/[id].jsx
@@ -1,16 +1,11 @@
 import React from 'react'
 
-import { useRouter } from 'next/router'
-
 import PostLoader from '@/app/PostLoader'
 
 import BasePage from '@/app/BasePage'
 import testPageReady from '@/hoc/testPageReady'
 
-const Page = () => {
-  const router = useRouter()
-  const { id } = router.query
-
+const Page = ({ id }) => {
   return (
     <BasePage
       headerConfig={{ text: '' }}
@@ -21,6 +16,14 @@ const Page = () => {
       <PostLoader postId={id} />
     </BasePage>
   )
+}
+
+export const getServerSideProps = async (context) => {
+  const { query: { id } } = context
+
+  return { props:
+    { id },
+  }
 }
 
 export default testPageReady('app')(Page)
