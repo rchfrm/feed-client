4224471ad4660df60d7d12c29f0c2258e44e72a9
Dennis Dijkstra <mail@dennisdijkstra.com>
2022-07-05 17:44:28 +0200

    FD-1277 Create reusable pricing plans header component

--- packages/landing/components/PricingPlans.jsx
+++ packages/landing/components/PricingPlans.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+
+import useBreakpointTest from '@/landing/hooks/useBreakpointTest'
+
 import ManagedPlan from '@/landing/ManagedPlan'
+import PricingPlansHeader from '@/PricingPlansHeader'
 import PricingPlansWrapper from '@/PricingPlansWrapper'
-import PricingPeriodToggle from '@/PricingPeriodToggle'
-import PricingCurrencySelect from '@/PricingCurrencySelect'
 import PricingPlan from '@/landing/PricingPlan'
+
 import MarkdownText from '@/elements/MarkdownText'
+
 import { pricingCopy } from '@/landing/copy/PricingPageCopy'
 import { pricingPlans } from '@/constants/pricing'
 
@@ -18,6 +22,8 @@ export default function PricingPlans() {
   const [showAnnualPricing, setShowAnnualPricing] = React.useState(false)
   const [currency, setCurrency] = React.useState('GBP')
   const [maxSpend, setMaxSpend] = React.useState(monthlyCost[currency] * maxSpendMultiple)
+  const isDesktop = useBreakpointTest('sm')
+
   React.useEffect(() => {
     setMaxSpend(monthlyCost[currency] * maxSpendMultiple)
   }, [currency, maxSpendMultiple, monthlyCost])
@@ -27,32 +33,19 @@ export default function PricingPlans() {
         'col-span-12',
       ].join(' ')}
     >
-      <div
-        className={[
-          'flex flex-column xs:flex-row sm:items-center xs:justify-between',
-          "after:content-[''] after:flex-1",
-          'mb-5',
-        ].join(' ')}
-      >
-        <div className="flex flex-1 mb-2 xs:mb-0">
-          <PricingCurrencySelect
-            currency={currency}
-            setCurrency={setCurrency}
-            className="xs:ml-2 w-[75px]"
-          />
-        </div>
-        <PricingPeriodToggle
-          showAnnualPricing={showAnnualPricing}
-          setShowAnnualPricing={setShowAnnualPricing}
-          className="flex items-center"
-          buttonPillClassName="bg-insta border-insta"
-        />
-      </div>
+      <PricingPlansHeader
+        currency={currency}
+        setCurrency={setCurrency}
+        showAnnualPricing={showAnnualPricing}
+        setShowAnnualPricing={setShowAnnualPricing}
+        buttonPillClassName="bg-insta border-insta"
+      />
       <PricingPlansWrapper
         plans={pricingPlans}
         showAnnualPricing={showAnnualPricing}
         currency={currency}
         pricingPlanComponent={PricingPlan}
+        isDesktop={isDesktop}
       />
       <ManagedPlan currency={currency} />
       <MarkdownText markdown={twoThousandPlus(currency, maxSpend)} className="text-center mb-10" />
