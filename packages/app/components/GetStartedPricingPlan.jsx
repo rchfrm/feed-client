51c46690418cfc49ff409dd1508337aa2982bdda
Dennis Dijkstra <mail@dennisdijkstra.com>
2022-07-11 10:30:11 +0200

    FD-1257 Split up components

--- packages/app/components/GetStartedPricingPlan.jsx
+++ packages/app/components/GetStartedPricingPlan.jsx
@@ -5,17 +5,13 @@ import useBreakpointTest from '@/hooks/useBreakpointTest'
 
 import GetStartedPricingPlanMonthlyCost from '@/app/GetStartedPricingPlanMonthlyCost'
 import GetStartedPricingPlanServiceFee from '@/app/GetStartedPricingPlanServiceFee'
+import GetStartedPricingPlanRecommendation from '@/app/GetStartedPricingPlanRecommendation'
+import GetStartedPricingPlanSelectButton from '@/app/GetStartedPricingPlanSelectButton'
+import GetStartedPricingPlanReadMoreButton from '@/app/GetStartedPricingPlanReadMoreButton'
 
-import Button from '@/elements/Button'
 import MarkdownText from '@/elements/MarkdownText'
-import ArrowAltIcon from '@/icons/ArrowAltIcon'
-import PlusCircleIcon from '@/icons/PlusCircleIcon'
-import StarCircleIcon from '@/icons/StarCircleIcon'
-import InsightsCircleIcon from '@/icons/InsightsCircleIcon'
-import LightbulbIcon from '@/icons/LightbulbIcon'
 
 import { capitalise } from '@/helpers/utils'
-import brandColors from '@/constants/brandColors'
 
 const GetStartedPricingPlan = ({
   plan,
@@ -32,14 +28,6 @@ const GetStartedPricingPlan = ({
     serviceFeePercentage,
   } = plan
 
-  const title = capitalise(name)
-  const icons = {
-    basic: PlusCircleIcon,
-    growth: InsightsCircleIcon,
-    pro: StarCircleIcon,
-  }
-
-  const Icon = icons[name]
   const isDesktop = useBreakpointTest('sm')
 
   return (
@@ -55,19 +43,7 @@ const GetStartedPricingPlan = ({
       ].join(' ')}
     >
       {isRecommended && (
-        <div className={[
-          'absolute left-0',
-          isDesktop ? 'left-0 -top-9' : 'bottom-0 right-0',
-          'flex items-center justify-center',
-          'w-full h-8 p-3',
-          'outline outline-[5px] outline-solid outline-insta',
-          'rounded-b-dialogue sm:rounded-t-dialogue sm:rounded-b-none bg-insta',
-          'text-xs text-white italic',
-        ].join(' ')}
-        >
-          <LightbulbIcon className="h-4 w-4 mr-1" fill={brandColors.white} />
-          Recommended based on your settings
-        </div>
+        <GetStartedPricingPlanRecommendation />
       )}
       <h2 className="mb-0 sm:mb-5 text-2xl sm:text-3xl">{capitalise(name)}</h2>
       <MarkdownText
@@ -98,42 +74,14 @@ const GetStartedPricingPlan = ({
           plan={plan}
         />
       </div>
-      <button
-        className={[
-          'sm:w-full flex justify-between',
-          'px-0 sm:px-4 py-2 sm:mb-5',
-          'sm:border-2 sm:border-green sm:border-solid sm:rounded-dialogue',
-          'font-bold',
-        ].join(' ')}
-        onClick={() => setSelectedPricingPlan(name)}
-      >
-        <span className="hidden sm:block">Select {title}</span>
-        <ArrowAltIcon
-          direction="right"
-          className="w-6 h-6 sm:w-auto sm:h-auto"
-        />
-      </button>
-      <div className={[
-        'absolute -bottom-6 sm:bottom-auto sm:relative',
-        'transform -translate-x-1/2 left-1/2',
-        'flex items-center font-normal',
-        'whitespace-nowrap sm:whitespace-normal',
-        'text-xs sm:text-base',
-      ].join(' ')}
-      >
-        <Icon className="w-4 h-4 inline-block mr-1" />
-        <div>
-          <Button
-            version="text"
-            onClick={() => handleSidePanel(plan)}
-            className="h-4 inline-block mr-1"
-            trackComponentName="GetStartedPricingPlan"
-          >
-            Read more
-          </Button>
-          about the {title} tier
-        </div>
-      </div>
+      <GetStartedPricingPlanSelectButton
+        setSelectedPricingPlan={setSelectedPricingPlan}
+        plan={plan}
+      />
+      <GetStartedPricingPlanReadMoreButton
+        plan={plan}
+        handleSidePanel={handleSidePanel}
+      />
     </div>
   )
 }
