cab2ea328a5ef88fe08fdd224360c46f81b284ec
Dennis Dijkstra <mail@dennisdijkstra.com>
2022-08-01 14:03:40 +0200

    FD-1311 Add pricing plan sidepanel to objective step (#586)

--- packages/app/components/GetStartedObjectiveButtonFooter.jsx
+++ packages/app/components/GetStartedObjectiveButtonFooter.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import useOpenPricingPlanReadMoreSidePanel from '@/app/hooks/useOpenPricingPlanReadMoreSidePanel'
+
+import { ArtistContext } from '@/app/contexts/ArtistContext'
+
 import MarkdownText from '@/elements/MarkdownText'
 
 import InformationIcon from '@/icons/InformationIcon'
@@ -8,19 +12,32 @@ import StarCircleIcon from '@/icons/StarCircleIcon'
 import InsightsCircleIcon from '@/icons/InsightsCircleIcon'
 import LockIcon from '@/icons/LockIcon'
 
+import { pricingPlans } from '@/constants/pricing'
 import brandColors from '@/constants/brandColors'
+
 import copy from '@/app/copy/getStartedCopy'
 
 const GetStartedObjectiveButtonFooter = ({
   plan,
   isDisabled,
 }) => {
+  const { artist } = React.useContext(ArtistContext)
+  const { currency: artistCurrency } = artist
+
   const icons = {
     basic: InformationIcon,
     growth: InsightsCircleIcon,
     pro: StarCircleIcon,
   }
   const Icon = icons[plan]
+
+  const openPricingPlanReadMoreSidePanel = useOpenPricingPlanReadMoreSidePanel()
+
+  const openReadMoreSidePanel = () => {
+    const pricingPlan = pricingPlans.find(({ name }) => name === plan)
+
+    openPricingPlanReadMoreSidePanel(pricingPlan, artistCurrency || 'GBP')
+  }
 
   return (
     isDisabled ? (
@@ -32,10 +49,13 @@ const GetStartedObjectiveButtonFooter = ({
         <MarkdownText markdown={copy.objectiveDisabledFooter(plan)} className="mb-0 text-xs mt-[2px]" />
       </div>
     ) : (
-      <div className="flex justify-center items-center">
+      <button
+        className="flex justify-center items-center"
+        onClick={openReadMoreSidePanel}
+      >
         <Icon className="w-4 h-4 mr-1" fill={brandColors.greyDark} />
         <MarkdownText markdown={copy.objectivePlanFooter(plan)} className="mb-0 text-xs text-grey-3" />
-      </div>
+      </button>
     )
   )
 }
