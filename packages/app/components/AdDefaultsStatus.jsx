2c10efd47fdae1138d934352548035b587253624
Dennis Dijkstra <mail@dennisdijkstra.com>
2022-09-20 16:40:16 +0200

    FD-1370 Handle loading state

--- packages/app/components/AdDefaultsStatus.jsx
+++ packages/app/components/AdDefaultsStatus.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import useAsyncEffect from 'use-async-effect'
 
+import { SidePanelContext } from '@/contexts/SidePanelContext'
+
 import ToggleSwitch from '@/elements/ToggleSwitch'
 import Error from '@/elements/Error'
 
@@ -21,27 +23,34 @@ const AdDefaultsStatus = ({
   const [postType, setPostType] = React.useState('')
   const [shouldUpdatePostStatus, setShouldUpdatePostStatus] = React.useState(false)
   const [shouldShowPostStatusConfirmation, setShouldShowPostStatusConfirmation] = React.useState(false)
+  const [isLoading, setIsLoading] = React.useState(false)
   const [error, setError] = React.useState(null)
 
   const postTypes = ['post', 'story', 'reels']
+  const { setSidePanelLoading } = React.useContext(SidePanelContext)
 
   useAsyncEffect(async (isMounted) => {
     if (!shouldUpdatePostStatus) {
       return
     }
 
+    setIsLoading(true)
+
     const { res: { success } } = await batchTogglePromotionEnabled(artistId, postType, pendingDefaultPostStatus)
     if (!isMounted() || !success) {
+      setIsLoading(false)
       return
     }
 
     const { res: artist, error } = await updateDefaultPromotionStatus(artistId, postType, pendingDefaultPostStatus)
     if (!isMounted()) {
+      setIsLoading(false)
       return
     }
 
     if (error) {
       setError(error)
+      setIsLoading(false)
       return
     }
 
@@ -60,6 +69,7 @@ const AdDefaultsStatus = ({
         defaultPromotionEnabled: newDefaultPostStatus,
       },
     })
+    setIsLoading(false)
   }, [shouldUpdatePostStatus])
 
   // Call this on toggle switch change
@@ -75,17 +85,22 @@ const AdDefaultsStatus = ({
     setShouldShowPostStatusConfirmation(true)
   }, [pendingDefaultPostStatus])
 
+  React.useEffect(() => {
+    setSidePanelLoading(isLoading)
+  }, [isLoading, setSidePanelLoading])
+
   return (
     <div>
       <Error error={error} />
       <div className="w-1/2 xs:w-1/3 sm:w-1/4 md:w-1/3 lg:w-1/4">
-        {postTypes.map((postType) => (
-          <div className="flex justify-between items-center mb-3" key={postType}>
-            <p className="mr-2 mb-0">{capitalise(postType)}</p>
+        {postTypes.map((type) => (
+          <div className="flex justify-between items-center mb-3" key={type}>
+            <p className="mr-2 mb-0">{capitalise(type)}</p>
             <ToggleSwitch
-              name={postType}
-              state={defaultPostStatus[postType]}
+              name={type}
+              state={defaultPostStatus[type]}
               onChange={updateGlobalStatus}
+              isLoading={isLoading && type === postType}
             />
           </div>
         ))}
