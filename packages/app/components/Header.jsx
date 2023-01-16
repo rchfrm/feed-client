9a0caf1297550d4e718b40b9740a81ce7b444750
Dennis Dijkstra <mail@dennisdijkstra.com>
2023-01-16 10:19:45 +0100

    FD-1530 Update header component

--- packages/app/components/Header.jsx
+++ packages/app/components/Header.jsx
@@ -1,41 +1,40 @@
 import React from 'react'
-import Router, { useRouter } from 'next/router'
+import { useRouter } from 'next/router'
 import PeekElement from 'react-peek-element'
-import useBrowserStore from '@/hooks/useBrowserStore'
 import useLoggedInTest from '@/app/hooks/useLoggedInTest'
 import useNotificationsStore from '@/app/stores/notificationsStore'
 import { InterfaceContext } from '@/contexts/InterfaceContext'
 import { ArtistContext } from '@/app/contexts/ArtistContext'
-import FeedLogo from '@/icons/FeedLogo'
-import SubNavMenuButton from '@/app/SubNavMenuButton'
-import SubNavProfileButton from '@/app/SubNavProfileButton'
-import SubNav from '@/app/SubNav'
+import { UserContext } from '@/app/contexts/UserContext'
+import LogoButton from '@/app/LogoButton'
+import HeaderMenu from '@/app/HeaderMenu'
+import HeaderMenuButton from '@/app/HeaderMenuButton'
+import HeaderProfileButton from '@/app/HeaderProfileButton'
+import ProfilesList from '@/app/ProfilesList'
 import * as ROUTES from '@/app/constants/routes'
 
-const getTotalActiveNotifications = (state) => state.totalActiveNotifications
+const getNotificationsStoreState = (state) => ({
+  totalActiveNotifications: state.totalActiveNotifications,
+  artistsWithNotifications: state.artistsWithNotifications,
+})
 
 const Header = () => {
-  const { width: windowWidth } = useBrowserStore()
+  const [shouldShowMore, setShouldShowMore] = React.useState(false)
 
-  const totalNotificationsUnread = useNotificationsStore(getTotalActiveNotifications)
   const isLoggedIn = useLoggedInTest()
+  const { user } = React.useContext(UserContext)
   const { pathname } = useRouter()
-  const { subNavOpen, toggleSubNav } = React.useContext(InterfaceContext)
+  const isGetStartedPage = pathname === ROUTES.GET_STARTED
+
+  const { totalActiveNotifications, artistsWithNotifications } = useNotificationsStore(getNotificationsStoreState)
+  const { isMenuOpen, toggleMenu } = React.useContext(InterfaceContext)
   const { artistId, artistLoading } = React.useContext(ArtistContext)
 
   React.useEffect(() => {
-    toggleSubNav(false)
-  }, [artistId, artistLoading, toggleSubNav])
+    toggleMenu(false)
+  }, [artistId, artistLoading, toggleMenu])
 
-  const goHome = () => {
-    if (pathname === ROUTES.HOME) {
-      return
-    }
-
-    Router.push(ROUTES.HOME)
-  }
-
-  if (artistLoading) {
+  if (! isLoggedIn || user.is_email_verification_needed || isGetStartedPage || artistLoading) {
     return null
   }
 
@@ -43,41 +42,40 @@ const Header = () => {
     <PeekElement
       usePlaceHolder
       config={{
-        childProps: { style: { zIndex: 14, transform: subNavOpen ? 'none' : null }, className: 'peek-element' },
+        childProps: { style: { zIndex: 22, transform: isMenuOpen ? 'none' : null }, className: 'peek-element' },
       }}
     >
       <header className={[
         'relative md:hidden top-0 left-0 right-0',
         '-mx-6 sm:-mx-8 px-4 py-1 h-15',
         'flex justify-between items-center',
-        'bg-black z-[12]',
+        'bg-black',
       ].join(' ')}
       >
         <div className="flex items-center">
-          <a
-            onClick={goHome}
-            role="button"
-            title="home"
-            className="mr-4"
-          >
-            <FeedLogo className="w-8" id="header" />
-          </a>
-          <SubNavMenuButton
-            toggleSubNav={toggleSubNav}
-            navOpen={subNavOpen}
+          <LogoButton className="w-8 mr-3" id="header" />
+          <HeaderMenuButton
+            toggle={toggleMenu}
           />
         </div>
-        <SubNavProfileButton
-          toggleSubNav={toggleSubNav}
-          isSubNavOpen={subNavOpen}
-          hasNotifications={!! totalNotificationsUnread}
-          className={['flex flex-col justify-center w-8 h-8 pointer-events-none'].join(' ')}
+        <HeaderProfileButton
+          hasNotifications={!! totalActiveNotifications}
+          shouldShowMore={shouldShowMore}
+          setShouldShowMore={setShouldShowMore}
         />
+        {shouldShowMore && (
+          <ProfilesList
+            artistsWithNotifications={artistsWithNotifications}
+            shouldShowMore={shouldShowMore}
+            setShouldShowMore={setShouldShowMore}
+            className="top-20 right-6"
+            hasConnectMore
+          />
+        )}
       </header>
-      <SubNav
-        isOpen={subNavOpen && isLoggedIn}
-        toggle={toggleSubNav}
-        windowWidth={windowWidth}
+      <HeaderMenu
+        isOpen={isMenuOpen && isLoggedIn}
+        toggle={toggleMenu}
       />
     </PeekElement>
   )
