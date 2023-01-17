4316eaafd61d8dcede7bd12ed12af51443370d61
Dennis Dijkstra <mail@dennisdijkstra.com>
2023-01-17 10:09:53 +0100

    FD-1554 Create new button component

---
+++ packages/shared/components/elements/ButtonNew.jsx
@@ -0,0 +1,117 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Spinner from '@/elements/Spinner'
+import { track } from '@/helpers/trackingHelpers'
+import { getStringFromChildrenProp } from '@/helpers/utils'
+import brandColors from '@/constants/brandColors'
+
+const Button = ({
+  type,
+  version,
+  size,
+  children,
+  onClick,
+  isLoading,
+  isDisabled,
+  componentName,
+  className,
+}) => {
+  const isTextButton = version === 'text'
+
+  const classes = {
+    size: {
+      small: 'h-8 p-2 text-[13px]',
+      medium: 'h-12 p-3 text-[13px]',
+      large: 'h-12 w-auto p-3.5',
+    },
+    version: {
+      primary: `
+        bg-green
+        hover:bg-green-light
+        active:border-2 active:border-green-dark active:border-dashed
+        disabled:bg-grey
+      `,
+      secondary: `
+        border-green border-solid border-2
+        hover:bg-green-bg-light
+        active:border-4 active::bg-green-bg-light
+        disabled:border-grey-dark disabled:bg-offwhite
+      `,
+      tertiary: `
+        bg-green-bg-light
+        hover:bg-green-bg-dark
+        active:border-2 active:border-green-dark active:border-dashed
+        disabled:bg-grey-light
+      `,
+      text: '',
+    },
+  }
+
+  const onButtonClick = (e) => {
+    const buttonText = getStringFromChildrenProp(children)
+
+    track('button_click', {
+      buttonText,
+      componentName,
+    })
+
+    onClick(e)
+  }
+
+  return (
+    <button
+      type={type}
+      onClick={onButtonClick}
+      className={[
+        'relative',
+        'rounded-dialogue',
+        isTextButton ? 'inline' : [classes.size[size], 'flex justify-center items-center'].join(' '),
+        classes.version[version],
+        isDisabled ? 'cursor-not-allowed' : null,
+        className,
+      ].join(' ')}
+      disabled={isDisabled}
+    >
+      {isLoading && (
+        <Spinner
+          width={size === 'small' ? 15 : 20}
+          fill={brandColors.black}
+          className="absolute left-1/2 -translate-x-1/2"
+        />
+      )}
+      <span
+        className={[
+          'flex items-center',
+          isDisabled ? 'opacity-50' : null,
+          isLoading ? 'invisible' : null,
+          isTextButton ? 'underline' : 'font-bold',
+        ].join(' ')}
+      >
+        {children}
+      </span>
+    </button>
+  )
+}
+
+Button.propTypes = {
+  type: PropTypes.string,
+  version: PropTypes.string,
+  size: PropTypes.string,
+  onClick: PropTypes.func.isRequired,
+  isDisabled: PropTypes.bool,
+  isLoading: PropTypes.bool,
+  className: PropTypes.string,
+  componentName: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+}
+
+Button.defaultProps = {
+  type: 'button',
+  version: 'primary',
+  size: 'medium',
+  isDisabled: false,
+  isLoading: false,
+  className: '',
+}
+
+export default Button
