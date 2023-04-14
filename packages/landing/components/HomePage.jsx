9075b9cdc427433a2bff2d9c6d38421fd35d3b98
jay-see-jay <6587212+jay-see-jay@users.noreply.github.com>
2023-04-14 10:54:52 +0100

    FD-1682 Move copy to copy folder

--- packages/landing/components/HomePage.jsx
+++ packages/landing/components/HomePage.jsx
@@ -6,38 +6,7 @@ import MarkdownText from '@/elements/MarkdownText'
 import Features from '@/landing/Features'
 import TryFeed from '@/landing/TryFeed'
 import PricingPlans from '@/landing/PricingPlans'
-
-const copy = {
-  header: 'Add real people to your audience',
-  description: 'Feed automates **Meta** campaigns: taking care of audience creation, campaign set up and continuous optimisation.\n\nThe platform maximises your budget to reach real, engaged people. No bots.',
-  features: [
-    {
-      id: 'feature-1',
-      header: 'Get started in minutes',
-      copy: 'Once you connect Feed to your ad account, you can start running campaigns in minutes.\n\nNo need to manually set up audiences or campaigns.',
-    },
-    {
-      id: 'feature-2',
-      header: 'Unique insights',
-      copy: 'Feed provides unique insight into your audience. Breakdown follower growth by age, gender, city and country.\n\nSo you can hone in on what works best.',
-    },
-    {
-      id: 'feature-3',
-      header: 'Clear, shareable reports',
-      copy: 'Our reports show the impact of your campaigns in a format and language that\'s clear and simple.\n\nAllowing you to share them with the whole team.',
-    },
-    {
-      id: 'feature-4',
-      header: 'Promote organic posts',
-      copy: 'The algorithm analyses existing social media posts to identify what has potential as an ad.\n\nThis means there\'s no need to create new content or get additional sign off.',
-    },
-    {
-      id: 'feature-5',
-      header: 'Always-on testing process',
-      copy: 'Creatives are continuously tested against each other to identify the best performers. Those that don\'t perform are swapped for the next creative in the queue every 48 hours.\n\nYou can decide the order or let Feed optimise for you.',
-    },
-  ],
-}
+import copy from '@/landing/copy/LandingPageCopy'
 
 export default function HomePage({ pageData }) {
   const {
