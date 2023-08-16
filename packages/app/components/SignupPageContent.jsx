d428c9274c934032be0c3467f759fa9322a21b12
jay-see-jay <6587212+jay-see-jay@users.noreply.github.com>
2023-08-16 18:45:07 +0100

    Remove ability to sign-up

--- packages/app/components/SignupPageContent.jsx
+++ packages/app/components/SignupPageContent.jsx
@@ -1,17 +1,9 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-
 import { AuthContext } from '@/contexts/AuthContext'
-
-import SignupEmailForm from '@/app/SignupEmailForm'
-import SignupPageTestimonial from '@/app/SignupPageTestimonial'
-
 import Error from '@/elements/Error'
-import MarkdownText from '@/elements/MarkdownText'
+import Anchor from '@/landing/elements/Anchor'
 
-import copy from '@/app/copy/LoginPageCopy'
-
-const SignupPageContent = ({ email, isValidReferralCode, testimony }) => {
+const SignupPageContent = () => {
   const { authError, setAuthError } = React.useContext(AuthContext)
 
   // Clear auth error when leaving page
@@ -22,35 +14,17 @@ const SignupPageContent = ({ email, isValidReferralCode, testimony }) => {
   }, [setAuthError])
 
   return (
-    <div className="flex mx-auto sm:pt-4 max-w-4xl">
-      <div className="flex-1 pb-5 pt-16">
+    <div className="flex m-auto max-w-4xl">
+      <div className="flex-1">
         <div className="sm:mr-6 md:mr-12">
           <Error error={authError} />
-          <h2 className="mb-2 text-2xl">Create account</h2>
-          <MarkdownText className={['small--text'].join(' ')} markdown={copy.tcText('clicking next')} />
-          <SignupEmailForm
-            initialEmail={email}
-            isValidReferralCode={isValidReferralCode}
-          />
+          <h2 className="mb-8 text-2xl">Request access</h2>
+          <p>We are not currently accepting new sign ups to Feed.</p>
+          <p>If you would like to try the platform, please email <Anchor href="mailto:help@tryfeed.co">help@tryfeed.co</Anchor>.</p>
         </div>
-      </div>
-      <div className="hidden sm:flex flex-1 justify-center ml-6 md:ml-12 pt-15">
-        <SignupPageTestimonial
-          testimony={testimony}
-        />
       </div>
     </div>
   )
-}
-
-SignupPageContent.propTypes = {
-  email: PropTypes.string,
-  isValidReferralCode: PropTypes.func.isRequired,
-  testimony: PropTypes.object.isRequired,
-}
-
-SignupPageContent.defaultProps = {
-  email: '',
 }
 
 export default SignupPageContent
